76aef1ca1c982f3dcf734b9d7605ee37d0016764
changhwan <chang5hustle@gmail.com>
2023-05-23 22:41:21 +0900

    feat(RemoteProductDataSource): 기존 페이지네이션 로직 유지할수있는 서버통신 로직 작성(콜백으로 리펙터링)

--- app/src/main/java/woowacourse/shopping/data/datasource/product/remote/RemoteProductDataSource.kt
+++ app/src/main/java/woowacourse/shopping/data/datasource/product/remote/RemoteProductDataSource.kt
@@ -8,6 +8,7 @@ import woowacourse.shopping.data.datasource.product.ProductDataSource
 import woowacourse.shopping.data.model.DataProduct
 import woowacourse.shopping.data.remote.OkHttpModule
 import java.io.IOException
+import java.lang.Integer.min
 
 class RemoteProductDataSource() : ProductDataSource.Remote {
 
@@ -26,7 +27,22 @@ class RemoteProductDataSource() : ProductDataSource.Remote {
             override fun onFailure(call: Call, e: IOException) {}
 
             override fun onResponse(call: Call, response: Response) {
+                val productCache = OkHttpModule.gson.fromJson(
+                    response.body?.string(),
+                    Array<DataProduct>::class.java
+                ).toList()
+                onReceived(getDataProductsFromCache(size, lastId, productCache))
             }
         })
+    }
+
+    private fun getDataProductsFromCache(
+        size: Int,
+        lastId: Int,
+        allProducts: List<DataProduct>
+    ): List<DataProduct> {
+        if (lastId == -1) return allProducts.subList(0, min(allProducts.size, size))
+        val startIndex = allProducts.indexOfFirst { it.id == lastId }
+        return allProducts.subList(startIndex, min(allProducts.size, startIndex + size))
     }
 }
