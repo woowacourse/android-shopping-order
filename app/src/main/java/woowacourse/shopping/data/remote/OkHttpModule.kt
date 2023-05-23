5ead546d4799d7a2618464cab7cf10d6b6ee8693
changhwan <chang5hustle@gmail.com>
2023-05-23 21:48:04 +0900

    feat(OkHttpModule): okhttp 관련 기초세팅

---
+++ app/src/main/java/woowacourse/shopping/data/remote/OkHttpModule.kt
@@ -0,0 +1,40 @@
+package woowacourse.shopping.data.remote
+
+import com.google.gson.Gson
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+object OkHttpModule {
+    var BASE_URL = "http://15.164.103.138:8080/"
+
+    private val encodedUserInfo =
+        Base64.getEncoder().encodeToString("a@a.com:1234".toByteArray(Charsets.UTF_8))
+
+    private val authorizationInterceptor: Interceptor =
+        Interceptor { chain ->
+            with(chain) {
+                proceed(
+                    request()
+                        .newBuilder()
+                        .addHeader("Authorization", encodedUserInfo)
+                        .build()
+                )
+            }
+        }
+
+    val shoppingOkHttpClient: OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .addInterceptor(authorizationInterceptor)
+            .build()
+
+    val gson = Gson()
+
+    fun getOkHttpRequest(url: String): Request =
+        Request.Builder().url(url).build()
+}
