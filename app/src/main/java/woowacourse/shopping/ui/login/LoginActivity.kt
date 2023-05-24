109a3485721a08a2f5ec6446adbc1233c3e1cda5
SeHun <81347125+s9hn@users.noreply.github.com>
2023-05-24 15:54:47 +0900

    feat : 서버선택 뷰 기능 구현

--- app/src/main/java/woowacourse/shopping/ui/login/LoginActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/login/LoginActivity.kt
@@ -1,5 +1,7 @@
 package woowacourse.shopping.ui.login
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.shopping.databinding.ActivityLoginBinding
@@ -10,5 +12,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    companion object {
+        private const val SERVER_KEY = "server_key"
+        fun getIntent(context: Context, serverKey: Int): Intent =
+            Intent(context, LoginActivity::class.java).apply {
+                putExtra(SERVER_KEY, serverKey)
+            }
     }
 }
--- app/src/main/java/woowacourse/shopping/ui/selectserver/SelectServerActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/selectserver/SelectServerActivity.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping.ui.selectserver
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.shopping.databinding.ActivitySelectServerBinding
+import woowacourse.shopping.ui.login.LoginActivity
 
 class SelectServerActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySelectServerBinding
@@ -10,5 +11,31 @@ class SelectServerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySelectServerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setClickEventOnHongsil()
+        setClickEventOnMatthew()
+    }
+
+    private fun setClickEventOnHongsil() {
+        binding.btnSelectServerHongsil.setOnClickListener {
+            navigateToLogin(HONGSIL_SERVER)
+        }
+    }
+
+    private fun setClickEventOnMatthew() {
+        binding.btnSelectServerMatthew.setOnClickListener {
+            navigateToLogin(MATTHEW_SERVER)
+        }
+    }
+
+    private fun navigateToLogin(serverKey: Int) {
+        val intent = LoginActivity.getIntent(this, serverKey)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        private const val HONGSIL_SERVER = 0
+        private const val MATTHEW_SERVER = 1
     }
 }
