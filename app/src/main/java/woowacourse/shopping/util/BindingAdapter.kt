e10c36b309a401a8068016bc996a96e9b97bab4a
changhwan <chang5hustle@gmail.com>
2023-05-24 22:19:30 +0900

    feat(*): ThrottleFirstOnClickListener 바인딩 어댑터 설정 및 baksetProduct add버튼 적용

--- app/src/main/java/woowacourse/shopping/util/BindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/util/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package woowacourse.shopping.util
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -9,4 +10,19 @@ fun ImageView.setImage(imgUrl: String) {
     Glide.with(context)
         .load(imgUrl)
         .into(this)
+}
+
+@BindingAdapter("app:setThrottleFirstOnClick")
+inline fun View.setBindingThrottleFirstOnClickListener(
+    crossinline block: (View) -> Unit
+) {
+    val delay: Long = 50L
+    var previousClickedTime = 0L
+    setOnClickListener { view ->
+        val clickedTime = System.currentTimeMillis()
+        if (clickedTime - previousClickedTime >= delay) {
+            block(view)
+            previousClickedTime = clickedTime
+        }
+    }
 }
