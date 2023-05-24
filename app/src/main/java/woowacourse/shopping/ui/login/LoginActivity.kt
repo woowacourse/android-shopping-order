4ff587715ba8f2cb0b756b1dd4a5739fe1f883dd
SeHun <81347125+s9hn@users.noreply.github.com>
2023-05-24 15:46:32 +0900

    feat : 로그인 뷰 디자인 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package woowacourse.shopping.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.shopping.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
