0873988c031f0c9aa011596eaaeaaa0e764187c8
whk06061 <gju06061@naver.com>
2023-05-25 17:31:35 +0900

    feat: 다른 서버 주소 추가

--- app/src/main/java/woowacourse/shopping/user/Server.kt
+++ app/src/main/java/woowacourse/shopping/user/Server.kt
@@ -2,7 +2,7 @@ package woowacourse.shopping.user
 
 enum class Server(val url: String, val token: String) {
     Deetoo(
-        "http://ec2-13-209-67-35.ap-northeast-2.compute.amazonaws.com:8080/",
+        "http://ec2-15-164-212-191.ap-northeast-2.compute.amazonaws.com:8080/",
         "YUBhLmNvbToxMjM0",
     ),
     Roise("http://ec2-13-209-67-35.ap-northeast-2.compute.amazonaws.com:8080/", "YUBhLmNvbToxMjM0"),
