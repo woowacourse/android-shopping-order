0a6d54dd14c254fde0da1e10f7d966b43810d0a6
krrong <khk9664@naver.com>
2023-05-25 13:16:51 +0900

    refactor(MainActivity): Base Url 수정

--- app/src/main/java/woowacourse/shopping/data/ServerBaseUrl.kt
+++
@@ -1,5 +0,0 @@
-package woowacourse.shopping.data
-
-internal const val BASE_URL_POI = "http://3.39.194.150:8080"
-internal const val BASE_URL_TORI = "http://13.209.68.194:8080"
-internal const val BASE_URL_JENNA = "http://43.201.105.220:8080"
--- app/src/main/java/woowacourse/shopping/presentation/view/main/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/view/main/MainActivity.kt
@@ -19,28 +19,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun setButtonClick() {
         binding.btMainTori.setOnClickListener {
-            val intent = ProductListActivity.createIntent(this, BASE_URL_TORI, TOKEN_TORI)
+            val intent = ProductListActivity.createIntent(this, BASE_URL_TORI, TOKEN)
             startActivity(intent)
         }
 
         binding.btMainJenna.setOnClickListener {
-            val intent = ProductListActivity.createIntent(this, BASE_URL_JENNA, TOKEN_JENNA)
+            val intent = ProductListActivity.createIntent(this, BASE_URL_JENNA, TOKEN)
             startActivity(intent)
         }
 
         binding.btMainPoi.setOnClickListener {
-            val intent = ProductListActivity.createIntent(this, BASE_URL_POI, TOKEN_POI)
+            val intent = ProductListActivity.createIntent(this, BASE_URL_POI, TOKEN)
             startActivity(intent)
         }
     }
 
     companion object {
         private const val BASE_URL_TORI = "http://13.209.68.194:8080"
-        private const val BASE_URL_JENNA = "http://43.201.105.220:8080"
+        private const val BASE_URL_JENNA = "http://3.34.178.40:8080"
         private const val BASE_URL_POI = "http://3.39.194.150:8080"
 
-        private const val TOKEN_TORI = "YUBhLmNvbToxMjM0"
-        private const val TOKEN_JENNA = "YUBhLmNvbToxMjM0"
-        private const val TOKEN_POI = "a2FuZ3NqOTY2NUBnbWFpbC5jb206MTIzNA=="
+        private const val TOKEN = "a2FuZ3NqOTY2NUBnbWFpbC5jb206MTIzNA=="
     }
 }
