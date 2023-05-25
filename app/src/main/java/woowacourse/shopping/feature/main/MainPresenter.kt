5c3889e1658558db80774950e84457a1aba3bd77
mendel <rhthrhrl0@naver.com>
2023-05-25 17:33:03 +0900

    fix: MainPresenter의 showDetail메소드가 백그라운드 스레드에서 수행되던 버그 수정

--- app/src/main/java/woowacourse/shopping/feature/main/MainPresenter.kt
+++ app/src/main/java/woowacourse/shopping/feature/main/MainPresenter.kt
@@ -90,8 +90,7 @@ class MainPresenter(
     private fun showDetail(productUiModel: ProductUiModel) {
         val recentProduct = _recentProducts.value?.firstOrNull()
         recentProductRepository.addRecentProduct(productUiModel.toDomain())
-        _mainScreenEvent.value =
-            MainScreenEvent.ShowProductDetailScreen(productUiModel, recentProduct)
+        _mainScreenEvent.postValue(MainScreenEvent.ShowProductDetailScreen(productUiModel, recentProduct))
     }
 
     override fun showRecentProductDetail(productId: Long) {
