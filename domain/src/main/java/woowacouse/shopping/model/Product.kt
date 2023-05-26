e9a25c36b1b6c674d419f5a63c1d7621df73dc80
inseonyun <yuninseon@naver.com>
2023-05-26 20:54:42 +0900

    feat(Product): 상품 모델 구현

--- domain/src/main/java/woowacouse/shopping/model/Product.kt
+++ domain/src/main/java/woowacouse/shopping/model/Product.kt
@@ -4,5 +4,13 @@ data class Product(
     val id: Long,
     val title: String,
     val price: Int,
-    var count: Int = 0,
-)
+    val count: Int = 0,
+) {
+    fun increaseCount(): Product = copy(count = (count + 1).coerceAtMost(MAX_COUNT))
+    fun decreaseCount(): Product = copy(count = (count - 1).coerceAtLeast(MIN_COUNT))
+
+    companion object {
+        private const val MIN_COUNT = 0
+        private const val MAX_COUNT = 99
+    }
+}
---
+++ domain/src/test/java/woowacouse/shopping/model/ProductTest.kt
@@ -0,0 +1,58 @@
+package woowacouse.shopping.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ProductTest {
+    @Test
+    fun `상품 개수를 하나 추가한다`() {
+        // given
+        val product = Product(1L, "피자", 12_000, 0)
+
+        // when
+        val actual = product.increaseCount()
+
+        // then
+        val expected = 1
+        assertEquals(expected, actual.count)
+    }
+
+    @Test
+    fun `상품 개수가 99개일 때 추가 해도 개수가 증가하지 않는다`() {
+        // given
+        val product = Product(1L, "피자", 12_000, 99)
+
+        // when
+        val actual = product.increaseCount()
+
+        // then
+        val expected = 99
+        assertEquals(expected, actual.count)
+    }
+
+    @Test
+    fun `상품 개수를 하나 감소 할 수 있다`() {
+        // given
+        val product = Product(1L, "피자", 12_000, 2)
+
+        // when
+        val actual = product.decreaseCount()
+
+        // then
+        val expected = 1
+        assertEquals(expected, actual.count)
+    }
+
+    @Test
+    fun `상품 개수가 0개일 때 개수를 빼도 개수가 음수가 되지 않는다`() {
+        // given
+        val product = Product(1L, "피자", 12_000, 0)
+
+        // when
+        val actual = product.decreaseCount()
+
+        // then
+        val expected = 0
+        assertEquals(expected, actual.count)
+    }
+}
