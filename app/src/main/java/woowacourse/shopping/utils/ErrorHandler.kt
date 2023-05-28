c2bc870d314d687732ca01111914d401ff6bbb01
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-29 02:09:01 +0900

    refactor : print 함수 제거

--- app/src/main/java/woowacourse/shopping/utils/ErrorHandler.kt
+++ app/src/main/java/woowacourse/shopping/utils/ErrorHandler.kt
@@ -1,9 +1,11 @@
 package woowacourse.shopping.utils
 
+import android.util.Log
+
 object ErrorHandler {
     fun printError(throwable: Throwable) {
         throwable.printStackTrace()
-        print(throwable.message)
-        println(throwable.stackTraceToString())
+        Log.d("Order Error", "${throwable.message}")
+        Log.d("Order Error", throwable.stackTraceToString())
     }
 }
