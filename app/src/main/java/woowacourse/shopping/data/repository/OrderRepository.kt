a013a0d1fd621d81d1eeb717f42e5b44cbfb02e5
boogieWoogie <kimjinuk1999@gmail.com>
2023-05-29 21:32:24 +0900

    feat(data/repository): repository딴에서 주문을 추가하는 기능 구현

--- app/src/main/java/woowacourse/shopping/data/repository/OrderRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/repository/OrderRepository.kt
@@ -5,6 +5,13 @@ import woowacourse.shopping.ui.model.OrderRecord
 // todo: 위치가 어디에?
 interface OrderRepository {
 
+    fun addOrder(
+        basketIds: List<Int>,
+        usingPoint: Int,
+        totalPrice: Int,
+        onAdded: (orderId: Int) -> Unit,
+    )
+
     fun getOrderRecord(
         orderId: Int,
         onReceived: (orderRecord: OrderRecord) -> Unit,
--- app/src/main/java/woowacourse/shopping/data/repository/OrderRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/repository/OrderRepositoryImpl.kt
@@ -2,11 +2,29 @@ package woowacourse.shopping.data.repository
 
 import woowacourse.shopping.data.datasource.OrderRemoteDataSource
 import woowacourse.shopping.data.mapper.toOrderRecord
+import woowacourse.shopping.data.model.OrderRequest
 import woowacourse.shopping.ui.model.OrderRecord
 
 class OrderRepositoryImpl(
     private val orderRemoteDataSource: OrderRemoteDataSource,
 ) : OrderRepository {
+
+    override fun addOrder(
+        basketIds: List<Int>,
+        usingPoint: Int,
+        totalPrice: Int,
+        onAdded: (orderId: Int) -> Unit,
+    ) {
+        val orderRequest = OrderRequest(
+            basketIds = basketIds.map(Int::toLong),
+            usingPoint = usingPoint.toLong(),
+            totalPrice = totalPrice.toLong()
+        )
+
+        orderRemoteDataSource.addOrder(orderRequest) { orderId ->
+            onAdded(orderId.toInt())
+        }
+    }
 
     override fun getOrderRecord(
         orderId: Int,
