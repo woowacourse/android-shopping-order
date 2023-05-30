abc2be0f50a875ec471dcbd5e72e1647776244de
krrong <khk9664@naver.com>
2023-05-30 22:09:46 +0900

    feat(CartProduct): 도메인 CartProduct 구현

---
+++ domain/src/main/java/com/example/domain/cart/CartProduct.kt
@@ -0,0 +1,26 @@
+package com.example.domain.cart
+
+import com.example.domain.Product
+
+data class CartProduct(
+    val id: Long,
+    val product: Product,
+    val count: Int,
+    val checked: Boolean,
+) {
+    fun updateChecked(isChecked: Boolean): CartProduct {
+        return copy(checked = isChecked)
+    }
+
+    fun updateCount(count: Int): CartProduct {
+        return copy(count = count)
+    }
+
+    fun increaseCount(): CartProduct {
+        return copy(count = this.count + 1)
+    }
+
+    fun decreaseCount(): CartProduct {
+        return copy(count = this.count - 1)
+    }
+}
