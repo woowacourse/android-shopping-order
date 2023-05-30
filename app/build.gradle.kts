8b7cc29999b370bb6a8f99ee98e9bd650950b32f
tmdgh1592 <tmdgh1592@naver.com>
2023-05-30 15:09:46 +0900

    chore: Retrofit 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:mockwebserver:4.11.0")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
