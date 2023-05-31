5ac360b0775e061488c3a5e3c75dae05fe67839d
changhwan <chang5hustle@gmail.com>
2023-05-31 17:55:19 +0900

    feat(PaymentConfirmContract): PaymentConfirmContract presenter 기초세팅

---
+++ app/src/main/java/woowacourse/shopping/ui/paymentconfirm/PaymentConfirmContract.kt
@@ -0,0 +1,9 @@
+package woowacourse.shopping.ui.paymentconfirm
+
+interface PaymentConfirmContract {
+    interface View
+
+    interface Presenter {
+        val view: View
+    }
+}
---
+++ app/src/main/java/woowacourse/shopping/ui/paymentconfirm/PaymentConfirmPresenter.kt
@@ -0,0 +1,5 @@
+package woowacourse.shopping.ui.paymentconfirm
+
+class PaymentConfirmPresenter(
+    override val view: PaymentConfirmContract.View
+) : PaymentConfirmContract.Presenter
