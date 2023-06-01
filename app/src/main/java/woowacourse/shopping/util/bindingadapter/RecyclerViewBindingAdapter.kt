cc52965ef16fd86cce633e1f1d334950c32de2c4
tmdgh1592 <tmdgh1592@naver.com>
2023-06-01 16:03:43 +0900

    refactor: RecyclerViewBindingAdapter의 setAdapter 메서드의 인자를 ConcatAdapter에서 RecyclerViewAdapter로 변경

--- app/src/main/java/woowacourse/shopping/util/bindingadapter/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/util/bindingadapter/RecyclerViewBindingAdapter.kt
@@ -1,12 +1,13 @@
 package woowacourse.shopping.util.bindingadapter
 
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 @BindingAdapter("bind:adapter", "bind:onAdapted", requireAll = false)
-fun RecyclerView.setAdapter(adapter: ConcatAdapter, onAdapted: () -> Unit) {
+fun RecyclerView.setAdapter(adapter: Adapter<ViewHolder>, onAdapted: () -> Unit) {
     this.adapter = adapter
     onAdapted.invoke()
 }
