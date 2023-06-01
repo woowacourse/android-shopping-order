46014de4201acb6ddc8b8aeb0c1da315c50d33f4
hyunji1203 <hj27720@gmail.com>
2023-06-01 21:50:10 +0900

    feat: 주문, 주문 상세 화면 xml 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/order/OrderActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.ui.order
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.shopping.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
