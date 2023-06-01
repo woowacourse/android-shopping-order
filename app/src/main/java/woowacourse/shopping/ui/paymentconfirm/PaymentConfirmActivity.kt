386fc585a39ee8077f53447e07879cc9eab2d58e
changhwan <chang5hustle@gmail.com>
2023-06-01 12:03:21 +0900

    feat(PaymentConfirmActivity): 화면 이동시 전달받은데이터 Presneter에서 관리하도록 로직 작성

--- app/src/main/java/woowacourse/shopping/ui/paymentconfirm/PaymentConfirmActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/paymentconfirm/PaymentConfirmActivity.kt
@@ -9,22 +9,38 @@ import woowacourse.shopping.R
 import woowacourse.shopping.data.datasource.userpointdata.remote.RemoteUserPointInfoDataSource
 import woowacourse.shopping.data.repository.PointRepository
 import woowacourse.shopping.databinding.ActivityPaymentConfirmBinding
+import woowacourse.shopping.ui.mapper.toDomain
 import woowacourse.shopping.ui.model.UiBasketProduct
 import woowacourse.shopping.ui.model.UiUserPointInfo
+import woowacourse.shopping.util.getParcelableArrayListExtraCompat
+import woowacourse.shopping.util.intentDataNullProcess
 
 class PaymentConfirmActivity : AppCompatActivity(), PaymentConfirmContract.View {
 
     private lateinit var binding: ActivityPaymentConfirmBinding
     private lateinit var presenter: PaymentConfirmContract.Presenter
+    private lateinit var currentOrderBasketProducts: List<UiBasketProduct>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_payment_confirm)
+        if (!initExtraData()) return
         initPresenter()
     }
 
+    private fun initExtraData(): Boolean {
+        currentOrderBasketProducts =
+            intent.getParcelableArrayListExtraCompat<UiBasketProduct>(CURRENT_ORDER_BASKET_PRODUCTS)
+                ?.toList() ?: return intentDataNullProcess(CURRENT_ORDER_BASKET_PRODUCTS)
+        return true
+    }
+
     private fun initPresenter() {
-        presenter = PaymentConfirmPresenter(this, PointRepository(RemoteUserPointInfoDataSource()))
+        presenter = PaymentConfirmPresenter(
+            this,
+            PointRepository(RemoteUserPointInfoDataSource()),
+            currentOrderBasketProducts.map { it.toDomain() }
+        )
     }
 
     override fun updateUserPointInfo(userPointInfo: UiUserPointInfo) {
--- app/src/main/java/woowacourse/shopping/ui/paymentconfirm/PaymentConfirmPresenter.kt
+++ app/src/main/java/woowacourse/shopping/ui/paymentconfirm/PaymentConfirmPresenter.kt
@@ -1,14 +1,18 @@
 package woowacourse.shopping.ui.paymentconfirm
 
+import woowacourse.shopping.domain.Basket
+import woowacourse.shopping.domain.BasketProduct
 import woowacourse.shopping.domain.UserPointInfo
 import woowacourse.shopping.domain.repository.PointRepository
 import woowacourse.shopping.ui.mapper.toUi
 
 class PaymentConfirmPresenter(
     override val view: PaymentConfirmContract.View,
-    private val pointRepository: PointRepository
+    private val pointRepository: PointRepository,
+    currentOrderBasketProducts: List<BasketProduct>
 ) : PaymentConfirmContract.Presenter {
     private lateinit var userPointInfo: UserPointInfo
+    private val currentOrderBasket = Basket(currentOrderBasketProducts)
 
     init {
         fetchUserPointInfo()
