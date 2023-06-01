d9e975a1a3fa849de981ea4f48ce21a95390d0f6
boogieWoogie <kimjinuk1999@gmail.com>
2023-06-01 10:55:20 +0900

    test(OrderHistoryPresenterTest): 테스트 코드 작성

---
+++ app/src/test/java/woowacourse/shopping/ui/OrderHistoryPresenterTest.kt
@@ -0,0 +1,47 @@
+package woowacourse.shopping.ui
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+import woowacourse.shopping.data.repository.OrderRepository
+import woowacourse.shopping.ui.model.Order
+import woowacourse.shopping.ui.orderhistory.OrderHistoryContract
+import woowacourse.shopping.ui.orderhistory.OrderHistoryPresenter
+
+class OrderHistoryPresenterTest {
+
+    private lateinit var presenter: OrderHistoryContract.Presenter
+    private lateinit var view: OrderHistoryContract.View
+    private lateinit var repository: OrderRepository
+
+    @Before
+    fun setUp() {
+        view = mockk(relaxed = true)
+        repository = mockk(relaxed = true)
+        presenter = OrderHistoryPresenter(
+            view = view,
+            repository = repository
+        )
+    }
+
+    @Test
+    fun `주문 목록들을 받아온 후 뷰를 초기화한다`() {
+        // given
+        val orders = OrderFixture.createOrders()
+        val slotInitView = slot<(orders: List<Order>) -> Unit>()
+        every {
+            repository.getOrders(onReceived = capture(slotInitView))
+        } answers {
+            slotInitView.captured.invoke(orders)
+        }
+
+        // when 저장소로부터 주문 목록을 받아온다.
+        presenter.getOrders()
+
+        // then 뷰가 초기화된다
+        verify { view.initView(orders) }
+    }
+}
