6deb88509f19686d9cd191b3d15908d73cda4012
tmdgh1592 <tmdgh1592@naver.com>
2023-06-03 00:57:30 +0900

    refactor: 코드 포맷팅

--- app/src/main/java/woowacourse/shopping/data/dto/mapper/PaymentMapper.kt
+++ app/src/main/java/woowacourse/shopping/data/dto/mapper/PaymentMapper.kt
@@ -3,8 +3,8 @@ package woowacourse.shopping.data.dto.mapper
 import woowacourse.shopping.data.dto.PaymentRequest
 import woowacourse.shopping.data.dto.PaymentResponse
 import woowacourse.shopping.domain.model.Payment
-import woowacourse.shopping.domain.model.discount.Point
 import woowacourse.shopping.domain.model.Price
+import woowacourse.shopping.domain.model.discount.Point
 
 fun PaymentResponse.toPayment(): Payment = Payment(
     originalPrice = Price(originalPayment),
