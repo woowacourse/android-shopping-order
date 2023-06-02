e64bfc274277d32a4b1ddc51eb2d594064b58a9c
Gloria <ippnsj@gmail.com>
2023-06-02 18:17:23 +0900

    feat: 주문 완료 / 주문 상세 페이지 레이아웃 작성

---
+++ app/src/main/java/woowacourse/shopping/ui/orderdetail/OrderDetailActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.ui.orderdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import woowacourse.shopping.R
+
+class OrderDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_detail)
+    }
+}
