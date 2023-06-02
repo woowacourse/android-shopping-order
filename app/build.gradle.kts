6637bb7a853e7f59aa9253e2db2fa4896fc3331a
tmdgh1592 <tmdgh1592@naver.com>
2023-06-03 00:41:52 +0900

    chore: Gson 라이브러리 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,9 +75,8 @@ dependencies {
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
     // kotlinx-serializable
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 }
