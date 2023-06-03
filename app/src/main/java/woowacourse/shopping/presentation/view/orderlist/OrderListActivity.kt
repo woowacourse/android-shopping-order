e6ba9c15b8cb01155751ccce4de36377a546f8db
krrong <khk9664@naver.com>
2023-06-03 12:50:43 +0900

    feat(OrderListActivity): 주문 목록 화면 추가

---
+++ app/src/main/java/woowacourse/shopping/presentation/view/orderlist/OrderListActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.presentation.view.orderlist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.shopping.R
+
+class OrderListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_list)
+    }
+}
