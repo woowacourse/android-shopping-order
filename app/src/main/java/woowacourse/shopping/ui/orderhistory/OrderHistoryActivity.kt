756578be6a1abc78fab42bce9a55a118ea61a447
Gloria <ippnsj@gmail.com>
2023-06-03 16:09:59 +0900

    feat: 주문 목록 페이지 레이아웃 작성

---
+++ app/src/main/java/woowacourse/shopping/ui/orderhistory/OrderHistoryActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.ui.orderhistory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import woowacourse.shopping.R
+
+class OrderHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_history)
+    }
+}
