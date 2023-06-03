69e74d8311e1f10a716b936dacb1f8be23c99edb
hyemdooly <hyemdooly@gmail.com>
2023-06-04 01:12:38 +0900

    feat: OrderHistoryPresenter 테스트 작성

---
+++ app/src/test/java/woowacourse/shopping/OrderHistoryPresenterTest.kt
@@ -0,0 +1,64 @@
+package woowacourse.shopping
+
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+import woowacourse.shopping.domain.model.OrderCartItemsDTO
+import woowacourse.shopping.domain.model.OrderDTO
+import woowacourse.shopping.domain.model.OrdersDTO
+import woowacourse.shopping.domain.model.Price
+import woowacourse.shopping.domain.model.Product
+import woowacourse.shopping.domain.model.ProductWithQuantity
+import woowacourse.shopping.domain.repository.OrderRepository
+import woowacourse.shopping.view.orderhistory.OrderHistoryContract
+import woowacourse.shopping.view.orderhistory.OrderHistoryPresenter
+
+class OrderHistoryPresenterTest {
+    private lateinit var view: OrderHistoryContract.View
+    private lateinit var presenter: OrderHistoryContract.Presenter
+    private lateinit var orderRepository: OrderRepository
+
+    @Before
+    fun setUp() {
+        view = mockk(relaxed = true)
+        orderRepository = object : OrderRepository {
+            override fun getAll(callback: (OrdersDTO) -> Unit) {
+                callback(
+                    OrdersDTO(
+                        listOf(
+                            OrderDTO(
+                                1,
+                                "2023-02-03 11:11:00",
+                                listOf(ProductWithQuantity(Product(1, "현미밥", Price(10000), ""), 1)),
+                                10000,
+                            ),
+                        ),
+                    ),
+                )
+            }
+
+            override fun getOrder(id: Int, callback: (OrderDTO) -> Unit) {
+                // val orderId: Int, val orderedDateTime: String, val products: List<ProductWithQuantity>, val totalPrice: Int
+            }
+
+            override fun order(cartProducts: OrderCartItemsDTO, callback: (Int?) -> Unit) {
+            }
+        }
+        presenter = OrderHistoryPresenter(
+            view,
+            orderRepository,
+        )
+    }
+
+    @Test
+    fun 주문_정보를_띄울_수_있다() {
+        // given
+
+        // when
+        presenter.fetchOrders()
+
+        // then
+        verify(exactly = 1) { view.showOrders(any()) }
+    }
+}
