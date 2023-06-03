cf5243d6cced05df7416393a6490acaddf24c735
hyemdooly <hyemdooly@gmail.com>
2023-06-04 01:12:02 +0900

    refactor: OrderPresenter 접근제어자 변경, 로그 삭제

---
+++ app/src/test/java/woowacourse/shopping/OrderPresenterTest.kt
@@ -0,0 +1,90 @@
+package woowacourse.shopping
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+import woowacourse.shopping.domain.model.OrderCartItemsDTO
+import woowacourse.shopping.domain.model.OrderDTO
+import woowacourse.shopping.domain.model.OrdersDTO
+import woowacourse.shopping.domain.repository.MypageRepository
+import woowacourse.shopping.domain.repository.OrderRepository
+import woowacourse.shopping.view.order.OrderContract
+import woowacourse.shopping.view.order.OrderPresenter
+
+class OrderPresenterTest {
+    private lateinit var view: OrderContract.View
+    private lateinit var presenter: OrderContract.Presenter
+    private lateinit var mypageRepository: MypageRepository
+    private lateinit var orderRepository: OrderRepository
+
+    @Before
+    fun setUp() {
+        view = mockk(relaxed = true)
+        mypageRepository = object : MypageRepository {
+            override fun getCash(callback: (Int) -> Unit) {
+                callback(40000)
+            }
+
+            override fun chargeCash(cash: Int, callback: (Int) -> Unit) {
+            }
+        }
+        orderRepository = object : OrderRepository {
+            override fun getAll(callback: (OrdersDTO) -> Unit) {
+            }
+
+            override fun getOrder(id: Int, callback: (OrderDTO) -> Unit) {
+            }
+
+            override fun order(cartProducts: OrderCartItemsDTO, callback: (Int?) -> Unit) {
+                callback(1)
+            }
+        }
+        presenter = OrderPresenter(view, OrderCartProductModelFixture.orderCartProductsModel, orderRepository, mypageRepository)
+    }
+
+    @Test
+    fun 주문_정보를_띄울_수_있다() {
+        // given
+
+        // when
+        presenter.fetchOrder()
+
+        // then
+        verify(exactly = 1) { view.showOrder(any()) }
+    }
+
+    @Test
+    fun 보유한_캐시가_총_가격_이상일_때_주문을_할_수_있다() {
+        // given
+        presenter.fetchOrder()
+
+        // when
+        presenter.order()
+
+        // then
+        verify(exactly = 1) { view.showOrderComplete(any()) }
+    }
+
+    @Test
+    fun 보유한_캐시가_총_가격_미만일_때_주문을_할_수_없다() {
+        // give
+        mypageRepository = object : MypageRepository {
+            override fun getCash(callback: (Int) -> Unit) {
+                callback(10000)
+            }
+
+            override fun chargeCash(cash: Int, callback: (Int) -> Unit) {
+            }
+        }
+        presenter = OrderPresenter(view, OrderCartProductModelFixture.orderCartProductsModel, orderRepository, mypageRepository)
+        presenter.fetchOrder()
+
+        // when
+        presenter.order()
+
+        // then
+        verify(exactly = 1) { view.showUnableToast() }
+    }
+}
