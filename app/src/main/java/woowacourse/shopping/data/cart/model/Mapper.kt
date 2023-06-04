e228dfceb34ae3cb0fb5c47577039d07bd2eeda4
Bandal <no1msh1217@gmail.com>
2023-06-05 02:04:20 +0900

    refactor(cart): 장바구니 목록을 일부만 가져오는 기능 작성

--- app/src/main/java/woowacourse/shopping/data/cart/model/Mapper.kt
+++ app/src/main/java/woowacourse/shopping/data/cart/model/Mapper.kt
@@ -1,7 +1,11 @@
 package woowacourse.shopping.data.cart.model
 
+import com.example.domain.Cart
 import com.example.domain.CartProduct
+import com.example.domain.Pagination
 import woowacourse.shopping.data.cart.model.dto.response.CartProductResponse
+import woowacourse.shopping.data.cart.model.dto.response.CartResponse
+import woowacourse.shopping.data.product.model.dto.PaginationDto
 
 fun CartProductResponse.toDomain(): CartProduct {
     return CartProduct(
@@ -12,5 +16,20 @@ fun CartProductResponse.toDomain(): CartProduct {
         productPrice = product.price,
         quantity = quantity,
         isPicked = true
+    )
+}
+
+fun CartResponse.toDomain(): Cart {
+    return Cart(
+        products = cartItems.map(CartProductResponse::toDomain)
+    )
+}
+
+fun PaginationDto.toDomain(): Pagination {
+    return Pagination(
+        total = total,
+        perPage = perPage,
+        currentPage = currentPage,
+        lastPage = lastPage
     )
 }
--- domain/src/main/java/com/example/domain/repository/CartRepository.kt
+++ domain/src/main/java/com/example/domain/repository/CartRepository.kt
@@ -1,11 +1,19 @@
 package com.example.domain.repository
 
 import com.example.domain.CartProduct
+import com.example.domain.Pagination
 
 interface CartRepository {
 
     fun getAll(
         onSuccess: (List<CartProduct>) -> Unit,
+        onFailure: () -> Unit
+    )
+
+    fun requestFetchCartProductsUnit(
+        unitSize: Int,
+        page: Int,
+        onSuccess: (List<CartProduct>, Pagination) -> Unit,
         onFailure: () -> Unit
     )
 
