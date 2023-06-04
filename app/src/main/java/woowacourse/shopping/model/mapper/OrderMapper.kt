49b8816c4b5d66e5a16e1c9d4cc6b3a2f0459305
otter <a33397397@gmail.com>
2023-06-04 17:31:12 +0900

    feat(order): 주문 내역 상세 화면 기능 추가

--- app/src/main/java/woowacourse/shopping/model/mapper/OrderMapper.kt
+++ app/src/main/java/woowacourse/shopping/model/mapper/OrderMapper.kt
@@ -1,8 +1,41 @@
 package woowacourse.shopping.model.mapper
 
+import com.example.domain.order.Order
+import com.example.domain.order.OrderProduct
 import com.example.domain.order.OrderSummary
+import woowacourse.shopping.model.ProductState
+import woowacourse.shopping.model.order.OrderProductState
+import woowacourse.shopping.model.order.OrderState
 import woowacourse.shopping.model.order.OrderSummaryResponse
 
 fun OrderSummaryResponse.toDomain(): OrderSummary {
-    return OrderSummary(id = id, finalPrice = finalPrice, products = products, orderDate = orderDate)
+    return OrderSummary(
+        id = id,
+        finalPrice = finalPrice,
+        products = products,
+        orderDate = orderDate
+    )
+}
+
+fun Order.toUi(): OrderState {
+    return OrderState(
+        id = id,
+        originalPrice = originalPrice,
+        discountPrice = discountPrice,
+        finalPrice = finalPrice,
+        orderDate = orderDate,
+        orderProducts = orderProducts.map { it.toUi() }
+    )
+}
+
+fun OrderProduct.toUi(): OrderProductState {
+    return OrderProductState(
+        id = id, quantity = quantity,
+        product = ProductState(
+            id = product.id,
+            imageUrl = product.imageUrl,
+            name = product.name,
+            price = product.price
+        )
+    )
 }
--- app/src/test/java/woowacourse/shopping/feature/order/detail/OrderDetailPresenterTest.kt
+++ app/src/test/java/woowacourse/shopping/feature/order/detail/OrderDetailPresenterTest.kt
@@ -1,13 +1,18 @@
+
 package woowacourse.shopping.feature.order.detail
 
+import com.example.domain.Product
+import com.example.domain.order.Order
+import com.example.domain.order.OrderProduct
 import com.example.domain.repository.OrderRepository
 import io.mockk.justRun
 import io.mockk.mockk
-import io.mockk.verify
 import org.junit.Before
 import org.junit.Test
+import woowacourse.shopping.model.mapper.toUi
+import woowacourse.shopping.util.AbstractTest
 
-class OrderDetailPresenterTest {
+class OrderDetailPresenterTest : AbstractTest() {
 
     private lateinit var view: OrderDetailContract.View
     private lateinit var orderRepository: OrderRepository
@@ -21,70 +26,83 @@ class OrderDetailPresenterTest {
     }
 
     @Test
-    fun `간단한 상품 명과 개수와 할인 전 가격 총합을 표시한다`() {
+    fun `상품 아이디를 이용해 상품의 정보를 가져와 화면에 표시한다`() {
+        val order: Order = createOrder()
         // given
-        val firstProductName = "피자"
-        val productsCount = 3
-        val totalPrice = 30000
-        justRun { view.setProductsSummary(firstProductName, productsCount, totalPrice) }
+        justRun { orderRepository.requestFetchOrderById(any(), any(), any()) }
+        justRun { view.setViewFixedContents(any()) }
+        justRun { view.setProductsSummary(any(), any(), any()) }
+        justRun { view.setOrderDate(any()) }
+        justRun { view.setOrderNumber(any()) }
+        justRun { view.setOrderProducts(any()) }
 
         // when
-        presenter.loadProductsSummary()
+        presenter.loadOrderInformation()
 
         // then
-        verify { view.setProductsSummary(firstProductName, productsCount, totalPrice) }
+        view.setViewFixedContents(order.toUi())
+        view.setProductsSummary(
+            firstProductName = order.orderProducts[0].product.name,
+            productsCount = order.orderProducts.count(), originalPrice = order.originalPrice
+        )
+        view.setOrderDate(order.orderDate)
+        view.setOrderNumber(order.id)
+        view.setOrderProducts(order.orderProducts)
     }
 
+//    @Test
+//    fun `상품 아이디를 받아오지 못하였다면 오류 문구를 띄우고 화면을 종료한다`() {
+//        // given
+//        presenter = OrderDetailPresenter(view, null, orderRepository)
+//
+//        // when
+//        presenter.loadOrderInformation()
+//
+//        // then
+//        view.showAccessError()
+//        view.closeOrderDetail()
+//    }
+
     @Test
-    fun `주문일시를 표시한다`() {
+    fun `상품 정보를 받아오지 못하였다면 오류 문구를 띄우고 화면을 종료한다`() {
         // given
-        val orderDate = "2023-05-26 13:30:40"
-        justRun { view.setOrderDate(orderDate) }
+        justRun { orderRepository.requestFetchOrderById(any(), any(), any()) }
+        justRun { view.showAccessError() }
+        justRun { view.closeOrderDetail() }
 
         // when
-        presenter.loadOrderDate()
+        presenter.loadOrderInformation()
 
         // then
-        verify { view.setOrderDate(orderDate) }
+        view.showAccessError()
+        view.closeOrderDetail()
     }
 
-    @Test
-    fun `주문번호를 표시한다`() {
-        // given
-        val orderId = 1L
-        justRun { view.setOrderId(orderId) }
-
-        // when
-        presenter.loadOrderId()
-
-        // then
-        verify { view.setOrderId(orderId) }
+    private fun createOrder(
+        id: Long = 1,
+        originalPrice: Int = 1_000,
+        discountPrice: Int = 0,
+        finalPrice: Int = 1_000,
+        orderDate: String = "2023-05-26 13:30:40",
+        orderProducts: List<OrderProduct> = createOrderProducts()
+    ): Order {
+        return Order(
+            id = id,
+            originalPrice = originalPrice,
+            discountPrice = discountPrice,
+            finalPrice = finalPrice,
+            orderDate = orderDate,
+            orderProducts = orderProducts
+        )
     }
 
-    @Test
-    fun `주문한 상품 목록을 표시한다`() {
-        // given
-        justRun { view.setOrderProducts(orderProducts) }
-
-        // when
-        presenter.loadOrderProducts()
-
-        // then
-        verify { view.setOrderProducts(orderProducts) }
-    }
-
-    @Test
-    fun `결제금액들을 표시한다`() {
-        // given
-        val productsSum = 30000
-        val discountPrice = 2000
-        val finalPrice = 28000
-        justRun { view.setPriceSummary(productsSum, discountPrice, finalPrice) }
-
-        // when
-        presenter.loadPriceSummary()
-
-        // then
-        verify { view.setPriceSummary(productsSum, discountPrice, finalPrice) }
+    private fun createOrderProducts(): List<OrderProduct> {
+        return listOf(
+            OrderProduct(
+                id = 1,
+                quantity = 1,
+                product = Product(id = 1, imageUrl = "", name = "aaa", price = 1_000)
+            )
+        )
     }
 }
