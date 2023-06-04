8d06aa4721b27e0822fb30f8026c6c38463453b5
re4rk <37167652+re4rk@users.noreply.github.com>
2023-06-04 20:43:30 +0900

    refactor: 레트로핏에서 사용하지 않는 메소드 제거

--- domain/src/main/java/woowacourse/shopping/utils/RetrofitUtil.kt
+++ domain/src/main/java/woowacourse/shopping/utils/RetrofitUtil.kt
@@ -2,9 +2,6 @@ package woowacourse.shopping.utils
 
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import woowacourse.shopping.data.service.RetrofitCartService
@@ -50,60 +47,5 @@ object RetrofitUtil {
                 .build()
         }
         return instance!!
-    }
-
-    fun <T> callback(callback: (Result<T>) -> Unit): Callback<T> {
-        return object : Callback<T> {
-            override fun onResponse(call: Call<T>, response: Response<T>) {
-                if (response.isSuccessful) {
-                    when (response.body()) {
-                        null -> callback(Result.failure(Throwable("response body is null")))
-                        else -> callback(Result.success(response.body()!!))
-                    }
-                } else {
-                    callback(Result.failure(Throwable(response.message())))
-                }
-            }
-
-            override fun onFailure(call: Call<T>, t: Throwable) {
-                callback(Result.failure(t))
-            }
-        }
-    }
-
-    fun <T> callbackNullable(block: (Result<T?>) -> Unit): Callback<T> {
-        return object : Callback<T> {
-            override fun onResponse(call: Call<T>, response: Response<T>) {
-                if (response.isSuccessful) {
-                    block(Result.success(response.body()))
-                } else {
-                    block(Result.failure(Throwable(response.message())))
-                }
-            }
-
-            override fun onFailure(call: Call<T>, t: Throwable) {
-                block(Result.failure(t))
-            }
-        }
-    }
-
-    fun callbackWithLocationHeader(callback: (Result<Long>) -> Unit): Callback<Unit> {
-        return object : Callback<Unit> {
-            override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
-                if (response.isSuccessful) {
-                    val orderId = response.headers()["Location"]
-                        ?.substringAfterLast("/")
-                        ?.toLong()
-                        ?: return callback(Result.failure(Throwable("location header is null")))
-                    callback(Result.success(orderId))
-                } else {
-                    callback(Result.failure(Throwable(response.message())))
-                }
-            }
-
-            override fun onFailure(call: Call<Unit>, t: Throwable) {
-                callback(Result.failure(t))
-            }
-        }
     }
 }
