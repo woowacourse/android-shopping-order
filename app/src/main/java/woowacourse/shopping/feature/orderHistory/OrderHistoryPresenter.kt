02b5efb301779304b33e23588891970eecf2e03b
pingu244 <pingu244@naver.com>
2023-06-05 02:57:43 +0900

    refactor(OrderHistoryPresenter): 마지막 페이지에 대한 오류 처리

--- app/src/main/java/woowacourse/shopping/feature/orderHistory/OrderHistoryPresenter.kt
+++ app/src/main/java/woowacourse/shopping/feature/orderHistory/OrderHistoryPresenter.kt
@@ -9,17 +9,27 @@ class OrderHistoryPresenter(
 ) : OrderHistoryContract.Presenter {
 
     private var currentPage = 1
+    private var totalPage = 0
 
     override fun loadOrderHistory() {
         orderRepository.getOrderHistory(
             currentPage, callback = {
                 it.onSuccess { orderHistoryInfo ->
                     view.addOrderHistory(orderHistoryInfo.info.map { it.toPresentation() })
+                    totalPage = orderHistoryInfo.totalPages
                     ++currentPage
                 }.onFailure { throwable ->
-                    view.showErrorMessage(throwable)
+                    if (currentPage > totalPage) {
+                        view.showErrorMessage(Throwable(LAST_PAGE_MESSAGE))
+                    } else {
+                        view.showErrorMessage(throwable)
+                    }
                 }
             }
         )
+    }
+
+    companion object {
+        private const val LAST_PAGE_MESSAGE = "마지막 페이지 입니다"
     }
 }
