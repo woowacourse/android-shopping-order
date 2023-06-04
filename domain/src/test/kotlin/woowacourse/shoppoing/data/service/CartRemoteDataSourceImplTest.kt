192a1a5026d253162f191c23a3bed57f94a6ffef
re4rk <37167652+re4rk@users.noreply.github.com>
2023-06-04 13:40:56 +0900

    refactor: 테스트 코드 잘못된 순서 설계 수정

--- domain/src/test/kotlin/woowacourse/shoppoing/data/service/CartRemoteDataSourceImplTest.kt
+++ domain/src/test/kotlin/woowacourse/shoppoing/data/service/CartRemoteDataSourceImplTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import woowacourse.shopping.data.remoteDataSourceImpl.CartRemoteDataSourceImpl
+import woowacourse.shopping.model.CartProduct
 import woowacourse.shopping.utils.RetrofitUtil
 import woowacourse.shopping.utils.mockWebServer.CartMockWeb
 
@@ -23,62 +24,72 @@ class CartRemoteDataSourceImplTest {
     fun `장바구니 목록을 가져온다`() {
         // when
         var lock = true
+        var cartProducts: List<CartProduct> = emptyList()
         remoteCartDataSource.getAll { result ->
-            result.onSuccess { cartProducts ->
-                // then
-                assertThat(cartProducts).hasSize(7)
-                for (i in 0 until 7) {
-                    assertThat(cartProducts[i].id).isEqualTo(i)
-                    assertThat(cartProducts[i].product.name).isEqualTo("치킨$i")
-                    assertThat(cartProducts[i].product.price).isEqualTo(10000)
-                    assertThat(cartProducts[i].product.imageUrl).isEqualTo(
-                        "https://search4.kakaocdn.net/argon/656x0_80_wr/KjhZ1Chrw9p"
-                    )
-                    assertThat(cartProducts[i].quantity).isEqualTo(i + 1)
-                }
-            }.onFailure { e -> throw e }
+            result.onSuccess { cartProducts = it }
+                .onFailure { e -> throw e }
             lock = false
         }
         while (lock) { Thread.sleep(100) }
+
+        // then
+        assertThat(cartProducts).hasSize(7)
+        cartProducts.forEachIndexed { i, it ->
+            assertThat(it.id).isEqualTo(i)
+            assertThat(it.quantity).isEqualTo(i + 1)
+            assertThat(it.product.name).isEqualTo("치킨$i")
+            assertThat(it.product.price).isEqualTo(10000)
+            assertThat(it.product.imageUrl).isEqualTo(
+                "https://search4.kakaocdn.net/argon/656x0_80_wr/KjhZ1Chrw9p"
+            )
+        }
     }
 
     @Test
     fun `장바구니에 상품을 추가한다`() {
         // when
         var lock = true
-
+        var count: Int? = null
         remoteCartDataSource.postItem(1) { result ->
-            // then
-            result.onSuccess { assertThat(it).isEqualTo(1) }
+            result.onSuccess { count = 1 }
                 .onFailure { e -> throw e }
             lock = false
         }
         while (lock) { Thread.sleep(100) }
+
+        // then
+        assertThat(count).isEqualTo(1)
     }
 
     @Test
     fun `장바구니의 상품 개수를 변경한다`() {
         // when
         var lock = true
+        var count: Int? = null
         remoteCartDataSource.patchItemQuantity(1, 10) { result ->
-            // then
-            result.onSuccess { assertThat(it).isEqualTo(10) }
+            result.onSuccess { count = 10 }
                 .onFailure { e -> throw e }
             lock = false
         }
         while (lock) { Thread.sleep(100) }
+
+        // then
+        assertThat(count).isEqualTo(10)
     }
 
     @Test
     fun `장바구니에서 상품을 제거한다`() {
         // when
         var lock = true
+        var count: Int? = null
         remoteCartDataSource.deleteItem(1) { result ->
-            // then
-            result.onSuccess { assertThat(it).isEqualTo(0) }
+            result.onSuccess { count = 0 }
                 .onFailure { e -> throw e }
             lock = false
         }
         while (lock) { Thread.sleep(100) }
+
+        // then
+        assertThat(count).isEqualTo(0)
     }
 }
--- domain/src/test/kotlin/woowacourse/shoppoing/data/service/ProductRemoteDataSourceImplTest.kt
+++ domain/src/test/kotlin/woowacourse/shoppoing/data/service/ProductRemoteDataSourceImplTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import woowacourse.shopping.data.remoteDataSourceImpl.ProductRemoteDataSourceImpl
+import woowacourse.shopping.model.Product
 import woowacourse.shopping.utils.RetrofitUtil
 import woowacourse.shopping.utils.mockWebServer.MockWeb
 
@@ -21,41 +22,51 @@ class ProductRemoteDataSourceImplTest {
 
     @Test
     fun `상품 목록을 가져온다`() {
-        // when
+        // given
+        var products: List<Product> = emptyList()
         var lock = true
-        remoteProductRepository.getAll {
-            // then
-            it.onSuccess { products ->
-                assertThat(products).hasSize(100)
-                for (i in 0..99) {
-                    assertThat(products[i].id).isEqualTo(i)
-                    assertThat(products[i].name).isEqualTo("치킨$i")
-                    assertThat(products[i].price).isEqualTo(10000)
-                    assertThat(products[i].imageUrl).isEqualTo(
-                        "https://search4.kakaocdn.net/argon/656x0_80_wr/KjhZ1Chrw9p"
-                    )
-                }
-            }.onFailure { e -> throw e }
+
+        // when
+        remoteProductRepository.getAll { result ->
+            result.onSuccess { products = it }
+                .onFailure { e -> throw e }
             lock = false
         }
         while (lock) { Thread.sleep(100) }
+
+        // then
+        assertThat(products).hasSize(100)
+        products.forEachIndexed { i, it ->
+            assertThat(it.id).isEqualTo(i)
+            assertThat(it.name).isEqualTo("치킨$i")
+            assertThat(it.price).isEqualTo(10000)
+            assertThat(it.imageUrl).isEqualTo(
+                "https://search4.kakaocdn.net/argon/656x0_80_wr/KjhZ1Chrw9p"
+            )
+        }
     }
 
     @Test
     fun `ID로 상품을 가져온다`() {
-        // when
+        // given
+        var product: Product? = null
         var lock = true
+
+        // when
         remoteProductRepository.findById(1) {
-            it.onSuccess { product ->
-                assertThat(product.id).isEqualTo(1)
-                assertThat(product.name).isEqualTo("치킨1")
-                assertThat(product.price).isEqualTo(10000)
-                assertThat(product.imageUrl).isEqualTo(
-                    "https://search4.kakaocdn.net/argon/656x0_80_wr/KjhZ1Chrw9p"
-                )
-            }.onFailure { e -> throw e }
+            it.onSuccess { product = it }
+                .onFailure { e -> throw e }
             lock = false
         }
         while (lock) { Thread.sleep(100) }
+
+        // then
+        assertThat(product).isNotNull
+        assertThat(product!!.id).isEqualTo(1)
+        assertThat(product!!.name).isEqualTo("치킨1")
+        assertThat(product!!.price).isEqualTo(10000)
+        assertThat(product!!.imageUrl).isEqualTo(
+            "https://search4.kakaocdn.net/argon/656x0_80_wr/KjhZ1Chrw9p"
+        )
     }
 }
