36eb79284462864f705b79ec3718c806f248393a
boogieWoogie <kimjinuk1999@gmail.com>
2023-06-05 13:39:01 +0900

    refactor(data/reponse): SerializedName으로 모든 필드의 이름 명확히 명시하기

--- app/src/main/java/woowacourse/shopping/data/datasource/response/BasketProductEntity.kt
+++ app/src/main/java/woowacourse/shopping/data/datasource/response/BasketProductEntity.kt
@@ -3,8 +3,10 @@ package woowacourse.shopping.data.datasource.response
 import com.google.gson.annotations.SerializedName
 
 data class BasketProductEntity(
+    @SerializedName("id")
     val id: Int,
     @SerializedName("quantity")
     val count: Int,
+    @SerializedName("product")
     val product: ProductEntity,
 )
--- app/src/main/java/woowacourse/shopping/data/datasource/response/UserEntity.kt
+++ app/src/main/java/woowacourse/shopping/data/datasource/response/UserEntity.kt
@@ -1,7 +1,12 @@
 package woowacourse.shopping.data.datasource.response
 
+import com.google.gson.annotations.SerializedName
+
 data class UserEntity(
+    @SerializedName("email")
     val email: String,
+    @SerializedName("point")
     val point: Int,
+    @SerializedName("earnRate")
     val accumulationRate: Int,
 )
