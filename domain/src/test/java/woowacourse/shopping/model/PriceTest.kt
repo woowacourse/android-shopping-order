a9dd682afacc789d8ef719c5a383fb9a9e0de0e9
vrexpert <vrexpert2302@gmail.com>
2023-06-06 16:02:27 +0900

    Test(PriceTest): 가격 예외 처리 case 추가

--- domain/src/test/java/woowacourse/shopping/model/PriceTest.kt
+++ domain/src/test/java/woowacourse/shopping/model/PriceTest.kt
@@ -1,12 +1,21 @@
 package woowacourse.shopping.model
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class PriceTest {
 
-    @Test
-    fun `가격이 0원 이상이어야 한다`() {
-        assertThrows<IllegalArgumentException> { Price(-100) }
+    @ValueSource(ints = [-1, -10, -100])
+    @ParameterizedTest
+    fun `가격은 음수일 수 없다`(value: Int) {
+        assertThrows<IllegalArgumentException> { Price(value) }
+    }
+
+    @ValueSource(ints = [0, 1, 10])
+    @ParameterizedTest
+    fun `가격은 0원 이상이어야 한다`(value: Int) {
+        assertDoesNotThrow { Price(value) }
     }
 }
