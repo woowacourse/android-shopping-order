b1e6f234a5f6af69880ebbf1b4d17c137279a3f3
vrexpert <vrexpert2302@gmail.com>
2023-06-06 20:39:45 +0900

    test(CartPresenterTest): 장바구니 목록 하나의 선택 상태를 변경 테스트

--- app/src/test/java/woowacourse/shopping/presentation/cart/CartPresenterTest.kt
+++ app/src/test/java/woowacourse/shopping/presentation/cart/CartPresenterTest.kt
@@ -95,8 +95,9 @@ class CartPresenterTest {
     fun `장바구니 아이템의 상품 개수를 증가시킨다`() {
         // given : 장바구니 아이템의 상품 개수를 증가시킬 수 있는 상태다.
 
-        val expected = CartProductFixture.getCheckableCartProductModels(quantity = 2, 1, 2, 3, 4) +
-            CartProductFixture.getCheckableCartProductModel(5, 3)
+        val expected =
+            CartProductFixture.getCheckableCartProductModels(quantity = 2, 1, 2, 3, 4) +
+                CartProductFixture.getCheckableCartProductModel(5, 3)
 
         every { view.showCartProductModels(expected) } just runs
 
@@ -141,5 +142,34 @@ class CartPresenterTest {
 
         // then : 장바구니 아이템 상품 개수가 감소되어 화면에 노출된다.
         verify { view.showCartProductModels(expected) }
+    }
+
+    @Test
+    fun `장바구니 아이템의 선택을 해제한다`() {
+        // given : 장바구니 아이템의 선택 상태를 변경할 수 있는 상태이다.
+        val selectedExpectedProduct =
+            CartProductFixture.getCartProducts(2, 1, 2, 3, 4, 6, 7, 8, 9, 10)
+        val expectedCount = selectedExpectedProduct.sumOf { it.quantity }
+        val expectedPrice = selectedExpectedProduct.sumOf { it.getTotalPrice() }
+
+        every { view.showAllCheckBoxIsChecked(false) } just runs
+        every { view.showTotalCount(expectedCount) } just runs
+        every { view.showTotalPrice(expectedPrice) } just runs
+
+        presenter.loadCarts()
+
+        // when : 장바구니 아이템 선택 상태 변경 요청을 보낸다.
+        presenter.changeProductSelected(
+            productModel = CartProductFixture.getProductModel(5),
+        )
+
+        // then : 장바구니 아이템 선택 상태가 변경되어 노출된다.
+        verify { view.showAllCheckBoxIsChecked(false) }
+
+        // and : 선택된 상품 전체 가격을 업데이트 한다.
+        verify { view.showTotalCount(expectedCount) }
+
+        // and : 선택된 상품 전체 개수를 업데이트 한다.
+        verify { view.showTotalPrice(expectedPrice) }
     }
 }
