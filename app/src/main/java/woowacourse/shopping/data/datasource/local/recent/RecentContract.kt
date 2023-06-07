0e19d531ce250b72d3ee4cf3f0f9513c8b4725f7
EmilyCh0 <emily9803@ewhain.net>
2023-06-08 04:37:48 +0900

    remove: 사용하지 않는 파일 및 코드 제거

--- app/src/main/java/woowacourse/shopping/data/datasource/local/recent/RecentContract.kt
+++ app/src/main/java/woowacourse/shopping/data/datasource/local/recent/RecentContract.kt
@@ -5,11 +5,6 @@ object RecentContract {
     const val TABLE_COLUMN_RECENT_PRODUCT_ID = "recent_product_id"
     const val TABLE_COLUMN_DATE_TIME = "recent_date_time"
 
-    fun allColumn() = listOf(
-        TABLE_COLUMN_RECENT_PRODUCT_ID,
-        TABLE_COLUMN_DATE_TIME
-    )
-
     fun createSQL(): String {
         return "CREATE TABLE $TABLE_NAME(" +
             "  $TABLE_COLUMN_RECENT_PRODUCT_ID int," +
