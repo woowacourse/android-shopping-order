c5164ebc7bef666fa760f4578f90bd52a0468b7f
changhwan <chang5hustle@gmail.com>
2023-06-08 11:49:28 +0900

    refactor(RetrofitCallbackHelper): callback 간소화(RemoteOrderDataSource수정)

--- app/src/main/java/woowacourse/shopping/data/datasource/order/remote/RemoteOrderDataSource.kt
+++ app/src/main/java/woowacourse/shopping/data/datasource/order/remote/RemoteOrderDataSource.kt
@@ -1,10 +1,8 @@
 package woowacourse.shopping.data.datasource.order.remote
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import woowacourse.shopping.data.datasource.order.OrderDataSource
 import woowacourse.shopping.data.httpclient.RetrofitModule
+import woowacourse.shopping.data.httpclient.getRetrofitCallback
 import woowacourse.shopping.data.httpclient.mapper.toData
 import woowacourse.shopping.data.httpclient.request.AddOrderRequest
 import woowacourse.shopping.data.httpclient.response.order.Individualorder.IndividualOrderResponse
@@ -25,8 +23,9 @@ class RemoteOrderDataSource : OrderDataSource.Remote {
             totalPrice = orderTotalPrice
         )
         RetrofitModule.orderService.addOrder(addOrderRequest).enqueue(
-            object : Callback<Unit> {
-                override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
+            getRetrofitCallback(
+                failureLogTag = this::class.java.name,
+                onResponse = { _, response ->
                     if (response.isSuccessful) {
                         val orderId = response.headers()["Location"]?.split("/")?.last()?.toInt()
                         orderId?.let { onReceived(Result.success(it)) }
@@ -43,47 +42,31 @@ class RemoteOrderDataSource : OrderDataSource.Remote {
                         }
                     }
                 }
-
-                override fun onFailure(call: Call<Unit>, t: Throwable) {}
-            }
+            )
         )
     }
 
     override fun getIndividualOrderInfo(orderId: Int, onReceived: (DataOrder) -> Unit) {
         RetrofitModule.orderService.getIndividualOrderInfo(orderId).enqueue(
-            object : Callback<IndividualOrderResponse> {
-                override fun onResponse(
-                    call: Call<IndividualOrderResponse>,
-                    response: Response<IndividualOrderResponse>
-                ) {
+            getRetrofitCallback<IndividualOrderResponse>(
+                failureLogTag = this::class.java.name,
+                onResponse = { _, response ->
                     val orderInfo = response.body()?.toData()
-
-                    orderInfo?.let {
-                        onReceived(it)
-                    }
+                    orderInfo?.let { onReceived(it) }
                 }
-
-                override fun onFailure(call: Call<IndividualOrderResponse>, t: Throwable) {}
-            }
+            )
         )
     }
 
     override fun getOrdersInfo(onReceived: (List<DataOrder>) -> Unit) {
         RetrofitModule.orderService.getOrdersInfo().enqueue(
-            object : Callback<List<IndividualOrderResponse>> {
-                override fun onResponse(
-                    call: Call<List<IndividualOrderResponse>>,
-                    response: Response<List<IndividualOrderResponse>>
-                ) {
+            getRetrofitCallback<List<IndividualOrderResponse>>(
+                failureLogTag = this::class.java.name,
+                onResponse = { _, response ->
                     val ordersInfo = response.body()?.map { it.toData() }
-
-                    ordersInfo?.let {
-                        onReceived(it)
-                    }
+                    ordersInfo?.let { onReceived(it) }
                 }
-
-                override fun onFailure(call: Call<List<IndividualOrderResponse>>, t: Throwable) {}
-            }
+            )
         )
     }
 }
