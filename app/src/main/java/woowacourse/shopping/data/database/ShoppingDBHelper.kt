d039ce1acf8efbcfcb2546f4649727c9d7e79b33
Sujin Kim <sujinkim919@gmail.com>
2023-06-08 15:13:48 +0900

    refactor: 사용하지 않는 파일 삭제

--- app/src/main/java/woowacourse/shopping/data/database/ShoppingDBHelper.kt
+++ app/src/main/java/woowacourse/shopping/data/database/ShoppingDBHelper.kt
@@ -3,25 +3,20 @@ package woowacourse.shopping.data.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import woowacourse.shopping.data.database.cart.CartConstant
-import woowacourse.shopping.data.database.product.ProductConstant
 import woowacourse.shopping.data.database.recentProduct.RecentProductConstant
 
-class ShoppingDBHelper(context: Context) :
-    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class ShoppingDBHelper(
+    context: Context,
+) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     init {
         onCreate(writableDatabase)
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL(CartConstant.getCreateTableQuery())
-        db?.execSQL(ProductConstant.getCreateTableQuery())
         db?.execSQL(RecentProductConstant.getCreateTableQuery())
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db?.execSQL(CartConstant.getUpdateTableQuery())
-        db?.execSQL(ProductConstant.getUpdateTableQuery())
         db?.execSQL(RecentProductConstant.getUpdateTableQuery())
         onCreate(db)
     }
