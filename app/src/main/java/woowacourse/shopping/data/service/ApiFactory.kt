d94f79d1297d0bd1792bc9d1bfdc4640362ba29a
Namyunsuk <yunsuk0328@naver.com>
2024-05-28 22:40:43 +0900

    feat: ProductService 구현

---
+++ app/src/main/java/woowacourse/shopping/data/service/ApiFactory.kt
@@ -0,0 +1,18 @@
+package woowacourse.shopping.data.service
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import woowacourse.shopping.BuildConfig
+
+object ApiFactory {
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    val productService = retrofit.create(ProductService::class.java)
+}
