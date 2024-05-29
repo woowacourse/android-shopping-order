7379e6d57183a90f6e81cf955fea5c2dafcf2a57
sh1mj1 <7wlgns@gmail.com>
2024-05-29 14:54:53 +0900

    feat: BasicAuthInterceptor 생성

--- app/src/main/java/woowacourse/shopping/remote/RetrofitService.kt
+++ app/src/main/java/woowacourse/shopping/remote/RetrofitService.kt
@@ -1,12 +1,19 @@
 package woowacourse.shopping.remote
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitService {
+    private val okHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(BasicAuthInterceptor("hxeyexn", "password"))
+            .build()
+
     val retrofitService: Retrofit =
         Retrofit.Builder()
             .baseUrl("http://54.180.95.212:8080")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 }
