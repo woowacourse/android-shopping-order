c776fd6e609644d133eda12e8bda4fa7b15acfc2
hxeyexn <kck85075@kumoh.ac.kr>
2024-05-29 21:17:29 +0900

    fix: 상품 상세 이미지 로드

--- app/src/main/java/woowacourse/shopping/ui/setImageViewResource.kt
+++ app/src/main/java/woowacourse/shopping/ui/setImageViewResource.kt
@@ -3,7 +3,6 @@ package woowacourse.shopping.ui
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import woowacourse.shopping.R
 
 @BindingAdapter("imageUrl")
 fun setImageViewResource(
@@ -12,7 +11,6 @@ fun setImageViewResource(
 ) {
     Glide.with(imageView.context)
         .load(resUrl)
-        .placeholder(R.drawable.product1)
         .fallback(android.R.drawable.ic_menu_report_image)
         .error(android.R.drawable.ic_menu_report_image)
         .thumbnail()
