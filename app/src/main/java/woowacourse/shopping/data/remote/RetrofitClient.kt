7b6d7ee8627f315d468133a9803e0928707d88b4
aprilgom <aprilgom@gmail.com>
2024-05-29 19:40:59 +0900

    feat: 인증 기능 구현

--- app/src/main/java/woowacourse/shopping/data/remote/RetrofitClient.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/RetrofitClient.kt
@@ -1,12 +1,23 @@
 package woowacourse.shopping.data.remote
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "http://54.180.95.212:8080"
+    private val httpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(
+                AuthenticationInterceptor(
+                    "aprilgom",
+                    "password",
+                ),
+            ).build()
     val retrofitApi: RetrofitService =
         Retrofit.Builder()
+            .client(httpClient)
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
