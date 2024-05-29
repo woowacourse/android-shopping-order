743a2e7fb7bce3ce42163c00856ed6386531b059
songpink <jejeje04@naver.com>
2024-05-29 16:41:16 +0900

    불필요한 dao 삭제

--- app/src/main/java/woowacourse/shopping/data/cart/model/CartData.kt
+++
@@ -1,6 +0,0 @@
-package woowacourse.shopping.data.cart.model
-
-data class CartData(
-    val id: Long,
-    val count: Int,
-)
--- app/src/main/java/woowacourse/shopping/remote/service/CartService.kt
+++ app/src/main/java/woowacourse/shopping/remote/service/CartService.kt
@@ -1,35 +1,35 @@
 package woowacourse.shopping.remote.service
 
-
-import kotlinx.serialization.Serializable
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
-import retrofit2.http.DELETE
-import retrofit2.http.PATCH
 import woowacourse.shopping.remote.dto.request.CartItemRequest
-import woowacourse.shopping.remote.dto.response.ProductsResponse
+import woowacourse.shopping.remote.dto.response.CartItemsResponse
 
 interface CartService {
     @GET("cart-items")
     fun fetchCartItems(
         @Query("page") page: Int,
         @Query("size") size: Int,
-    ): Call<ProductsResponse>
-
-    @GET("cart-items/counts")
-    fun fetchCartItemsCount(@Path("id") id: Long): Call<ProductsResponse>
+    ): Call<CartItemsResponse>
 
     @POST("cart-items")
-    fun createCartItems(@Body body: CartItemRequest): Call<Unit>
+    fun createCartItems(
+        @Body body: CartItemRequest,
+    ): Call<Unit>
 
     @DELETE("cart-items/{id}")
-    fun deleteCartItem(@Path("id") id: Long): Call<ProductsResponse>
+    fun deleteCartItem(
+        @Path("id") id: Long,
+    ): Call<Unit>
 
     @PATCH("cart-items/{id}")
-    fun patchCartItem(@Path("id") id: Long): Call<ProductsResponse>
+    fun patchCartItem(
+        @Path("id") id: Long,
+    ): Call<Unit>
 }
-
