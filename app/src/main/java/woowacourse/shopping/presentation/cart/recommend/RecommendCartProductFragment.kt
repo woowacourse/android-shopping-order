0d4fac01101b572ddec8f5b242a0d763043379b7
murjune <sis9221302@gmail.com>
2024-05-29 21:55:54 +0900

    feat: 추천 상품, 전체 선택 ui

---
+++ app/src/main/java/woowacourse/shopping/presentation/cart/recommend/RecommendCartProductFragment.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping.presentation.cart.recommend
+
+import android.os.Bundle
+import android.view.View
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.FragmentRecommendCartProductBinding
+import woowacourse.shopping.presentation.base.BindingFragment
+
+class RecommendCartProductFragment :
+    BindingFragment<FragmentRecommendCartProductBinding>(R.layout.fragment_recommend_cart_product) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
