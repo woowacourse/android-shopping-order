5ff4f6810ff6b719e45cd19ea0bd995bdbbbdc69
hxeyexn <kck85075@kumoh.ac.kr>
2024-05-29 16:13:01 +0900

    chore: http logging 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,4 +91,6 @@ dependencies {
     // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
