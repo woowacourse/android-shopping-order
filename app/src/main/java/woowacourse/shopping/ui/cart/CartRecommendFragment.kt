a38cabcefbc113602174594464edf5b157e3fba0
kimhyemin <kimhm0728@naver.com>
2024-05-30 17:07:01 +0900

    feat: 추천 상품 리스트 기능 구현

--- app/src/main/java/woowacourse/shopping/ui/cart/CartRecommendFragment.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartRecommendFragment.kt
@@ -5,19 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import woowacourse.shopping.R
 import woowacourse.shopping.databinding.FragmentCartRecommendBinding
+import woowacourse.shopping.ui.detail.ProductDetailActivity
+import woowacourse.shopping.ui.products.adapter.ProductsAdapter
 
 class CartRecommendFragment(val viewModel: CartViewModel) : Fragment() {
-    private lateinit var binding: FragmentCartRecommendBinding
+    private var _binding: FragmentCartRecommendBinding? = null
+    private val binding get() = _binding!!
+
+    private val adapter by lazy {
+        ProductsAdapter(
+            onClickProductItem = { navigateToProductDetailView(it) },
+            onClickRecentProductItem = { },
+            onIncreaseProductQuantity = { viewModel.increaseQuantity(it) },
+            onDecreaseProductQuantity = { viewModel.decreaseQuantity(it) },
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        binding = FragmentCartRecommendBinding.inflate(inflater, container, false)
-        val listView = binding.rvRecommendProduct
-        return inflater.inflate(R.layout.fragment_cart_recommend, container, false)
+        _binding = FragmentCartRecommendBinding.inflate(inflater, container, false)
+        initializeView()
+
+        return binding.root
+    }
+
+    private fun initializeView() {
+        binding.rvRecommendProduct.itemAnimator = null
+        binding.rvRecommendProduct.adapter = adapter
+        viewModel.recommendProductUiModels.observe(this) {
+            adapter.updateProducts(it)
+        }
+    }
+
+    private fun navigateToProductDetailView(productId: Int) {
+        val intent = ProductDetailActivity.newIntent(requireContext(), productId)
+        startActivity(intent)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
