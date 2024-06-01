f0c8eaea6767d5a0be2d200d8e812042c8db1a0b
JunJangE <wnswkd486@gmail.com>
2024-06-01 12:28:27 +0900

    chore: 필요없는 파일 및 코드 제거

--- app/src/main/java/woowacourse/shopping/domain/model/Carts.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Carts.kt
@@ -3,13 +3,6 @@ package woowacourse.shopping.domain.model
 data class Carts(
     val content: List<Cart>,
     val pageable: Pageable,
-//    val totalPages: Int,
     val totalElements: Int,
-//    val sort: Sort,
-//    val first: Boolean,
     val last: Boolean,
-//    val number: Int,
-//    val numberOfElements: Int,
-//    val size: Int,
-//    val empty: Boolean,
 )
--- app/src/main/java/woowacourse/shopping/domain/model/Products.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Products.kt
@@ -3,13 +3,4 @@ package woowacourse.shopping.domain.model
 data class Products(
     val content: List<Product>,
     val pageable: Pageable,
-//    val last: Boolean,
-//    val totalPages: Int,
-//    val totalElements: Int,
-//    val sort: Sort,
-//    val first: Boolean,
-//    val number: Int,
-//    val numberOfElements: Int,
-//    val size: Int,
-//    val empty: Boolean,
 )
