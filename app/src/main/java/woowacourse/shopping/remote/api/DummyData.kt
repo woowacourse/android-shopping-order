961a8edd060c7b30a3459b1c1f51b89a5008df43
JunJangE <wnswkd486@gmail.com>
2024-06-02 02:35:32 +0900

    test: ProductDetailViewModelTest

--- app/src/main/java/woowacourse/shopping/remote/api/DummyData.kt
+++ app/src/main/java/woowacourse/shopping/remote/api/DummyData.kt
@@ -1,12 +1,20 @@
 package woowacourse.shopping.remote.api
 
+import woowacourse.shopping.data.mapper.toDomain
+import woowacourse.shopping.data.model.remote.CartDto
 import woowacourse.shopping.data.model.remote.ProductDto
+import woowacourse.shopping.domain.model.Cart
+import woowacourse.shopping.domain.model.Carts
+import woowacourse.shopping.domain.model.Pageable
+import woowacourse.shopping.domain.model.Products
+import woowacourse.shopping.domain.model.Sort
 
 object DummyData {
     const val STUB_IMAGE_URL_A =
         "https://i.namu.wiki/i/VnSgJ92KZ4dSRF2_x3LAYiE-zafxvNochXYrt6QD88DNtVziOxYUVKploFydbFNY7rcmOBUEra42XObzSuBwww.webp"
     val STUB_PRODUCT_A =
         ProductDto(id = 1L, name = "홍차", price = 10000, category = "", imageUrl = STUB_IMAGE_URL_A)
+    val STUB_CART_A = CartDto(id = Cart.EMPTY_CART_ID, quantity = 0, productDto = STUB_PRODUCT_A)
 
     const val STUB_IMAGE_URL_B =
         "https://img.danawa.com/prod_img/500000/451/474/img/3474451_1.jpg?_v=20210323160420"
@@ -18,26 +26,13 @@ object DummyData {
             category = "",
             imageUrl = STUB_IMAGE_URL_B,
         )
+    val STUB_CART_B = CartDto(id = 2, quantity = 0, productDto = STUB_PRODUCT_B)
 
     const val STUB_IMAGE_URL_C =
         "https://i.namu.wiki/i/fhsBMFdIgnB_D4KHQpaG0n2yk5X26rVpfsYeoIaJxwb3gLbQDJ9C7rgVQEZWKfhUwE0bR_2yT0Y1FCOwkDePJg.webp"
     val STUB_PRODUCT_C =
         ProductDto(id = 3, name = "신라면", price = 15000, category = "", imageUrl = STUB_IMAGE_URL_C)
-
-    val CART_PRODUCTS =
-        mutableListOf(
-            STUB_PRODUCT_B.copy(id = 0),
-            STUB_PRODUCT_B.copy(id = 1),
-            STUB_PRODUCT_C.copy(id = 2),
-            STUB_PRODUCT_A.copy(id = 3),
-            STUB_PRODUCT_B.copy(id = 4),
-            STUB_PRODUCT_C.copy(id = 5),
-            STUB_PRODUCT_A.copy(id = 6),
-            STUB_PRODUCT_B.copy(id = 7),
-            STUB_PRODUCT_C.copy(id = 8),
-            STUB_PRODUCT_A.copy(id = 9),
-            STUB_PRODUCT_B.copy(id = 10),
-        )
+    val STUB_CART_C = CartDto(id = 3, quantity = 0, productDto = STUB_PRODUCT_C)
 
     val PRODUCT_LIST =
         mutableListOf(
@@ -94,5 +89,34 @@ object DummyData {
                 id = (index + 1).toLong(),
                 name = "$index ${product.name}",
             )
-        }.toMutableList()
+        }
+
+    private val sort = Sort(sorted = false, unSorted = true, empty = true)
+    val pageable =
+        Pageable(sort, pageNumber = 0, pageSize = 20, offset = 0, paged = true, unPaged = false)
+
+    val PRODUCTS =
+        Products(
+            content = PRODUCT_LIST.map { it.toDomain() },
+            pageable = pageable,
+        )
+
+    val CARTS =
+        Carts(
+            totalElements = 0,
+            last = false,
+            pageable = pageable,
+            content = emptyList(),
+        )
+
+    val CARTS_PULL =
+        Carts(
+            totalElements = PRODUCT_LIST.size,
+            last = false,
+            pageable = pageable,
+            content =
+                PRODUCT_LIST.mapIndexed { index, productDto ->
+                    Cart(id = index, product = productDto.toDomain())
+                },
+        )
 }
