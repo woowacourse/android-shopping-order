d7f44bb17f8ba6b4b26da0fb85fee9748b5a0dc8
songpink <jejeje04@naver.com>
2024-06-02 17:42:05 +0900

    refactor: 클래스 파일을 분리

--- app/src/main/java/woowacourse/shopping/data/cart/model/CartItemData.kt
+++ app/src/main/java/woowacourse/shopping/data/cart/model/CartItemData.kt
@@ -2,14 +2,6 @@ package woowacourse.shopping.data.cart.model
 
 import woowacourse.shopping.domain.entity.Product
 
-data class CartPageData(
-    val cartItems: List<CartItemData>,
-    val pageNumber: Int,
-    val totalPageSize: Int,
-    val pageSize: Int,
-    val totalProductSize: Int,
-)
-
 data class CartItemData(
     val cartId: Long,
     val count: Int,
