1310147ca5622ef2cd9afb301706aed6d55ff6d6
ii2001 <iivks2001@gmail.com>
2024-06-02 16:39:21 +0900

    :recycle: 비동기 처리화

--- app/src/main/java/woowacourse/shopping/data/repository/real/OrderRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/repository/real/OrderRepositoryImpl.kt
@@ -1,12 +1,15 @@
 package woowacourse.shopping.data.repository.real
 
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import woowacourse.shopping.data.remote.source.OrderDataSourceImpl
 import woowacourse.shopping.data.source.OrderDataSource
 import woowacourse.shopping.domain.repository.OrderRepository
 import woowacourse.shopping.utils.exception.LatchUtils.awaitOrThrow
 import woowacourse.shopping.utils.exception.NoSuchDataException
 import java.util.concurrent.CountDownLatch
-import kotlin.concurrent.thread
 
 class OrderRepositoryImpl(
     private val orderDataSource: OrderDataSource = OrderDataSourceImpl(),
@@ -15,18 +18,21 @@ class OrderRepositoryImpl(
         val latch = CountDownLatch(1)
         var exception: Exception? = null
 
-        thread {
-            try {
-                val response = orderDataSource.orderItems(ids = ids).execute()
+        orderDataSource.orderItems(ids = ids).enqueue(object : Callback<Unit> {
+            override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
                 if (!response.isSuccessful) {
                     exception = NoSuchDataException()
                 }
-            } catch (e: Exception) {
-                exception = e
-            } finally {
+                Log.d("OrderRepositoryImpl", "orderShoppingCart: ${response.code()}")
                 latch.countDown()
             }
-        }
+
+            override fun onFailure(call: Call<Unit>, t: Throwable) {
+                exception = t as? Exception ?: Exception(t)
+                latch.countDown()
+            }
+        })
+
         latch.awaitOrThrow(exception)
     }
 }
