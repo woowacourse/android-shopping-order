16ce06555daa7682c0e9806b507bfdc4507a3d13
김진우 <jinwoo3661@naver.com>
2024-06-03 03:37:59 +0900

    refactor: 최근 본 상품 테스트 추가

--- app/src/androidTest/java/woowacourse/shopping/data/view/RecommendFragmentTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/data/view/RecommendFragmentTest.kt
@@ -6,6 +6,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
@@ -15,8 +16,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.shopping.R
 import woowacourse.shopping.TestFixture
-import woowacourse.shopping.data.db.cartItem.CartItemDao
-import woowacourse.shopping.data.db.cartItem.CartItemDatabase
+import woowacourse.shopping.data.db.recently.RecentlyProductDao
+import woowacourse.shopping.data.db.recently.RecentlyProductDatabase
 import woowacourse.shopping.view.MainActivity
 import woowacourse.shopping.view.recommend.RecommendFragment
 import kotlin.concurrent.thread
@@ -25,17 +26,17 @@ import kotlin.concurrent.thread
 class RecommendFragmentTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
-    private lateinit var database: CartItemDatabase
-    private lateinit var dao: CartItemDao
+    private lateinit var database: RecentlyProductDatabase
+    private lateinit var dao: RecentlyProductDao
     private lateinit var context: Context
 
     @Before
     fun setUp() {
         context = ApplicationProvider.getApplicationContext()
-        database = CartItemDatabase.getInstance(context)
-        dao = database.cartItemDao()
+        database = RecentlyProductDatabase.getInstance(context)
+        dao = database.recentlyProductDao()
         thread {
-            dao.saveCartItem(TestFixture.makeCartItemEntity())
+            dao.addRecentlyProduct(TestFixture.makeRecentlyProductEntity())
         }.join()
 
         activityRule.scenario.onActivity { activity ->
@@ -61,6 +62,18 @@ class RecommendFragmentTest {
     @Test
     fun `구매_버튼을_보여준다`() {
         onView(withId(R.id.btn_order))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `추천_상품_제목을_보여준다`() {
+        onView(withId(R.id.tv_subject))
+            .check(matches(withText("이런 상품은 어떠세요?")))
+    }
+
+    @Test
+    fun `최근_본_상품이_있다면_같은_카테고리의_추천_상품을_보여준다`() {
+        onView(withId(R.id.rv_recommend))
             .check(matches(isDisplayed()))
     }
 }
