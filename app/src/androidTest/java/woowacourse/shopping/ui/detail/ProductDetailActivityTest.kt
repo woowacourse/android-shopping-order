d2a564a0bc4588d754641871c2c848f9f8e544bf
kimhyemin <kimhm0728@naver.com>
2024-06-03 22:22:34 +0900

    test: 통과하지 않는 테스트 수정

--- app/src/androidTest/java/woowacourse/shopping/ui/detail/ProductDetailActivityTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/ui/detail/ProductDetailActivityTest.kt
@@ -5,8 +5,10 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.Visibility
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -28,13 +30,13 @@ class ProductDetailActivityTest {
     @Test
     fun `상품_제목이_보인다`() {
         onView(withId(R.id.tv_product_detail_title))
-            .check(matches(isDisplayed()))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 
     @Test
     fun `상품_가격이_보인다`() {
         onView(withId(R.id.tv_product_detail_price))
-            .check(matches(isDisplayed()))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 
     @Test
