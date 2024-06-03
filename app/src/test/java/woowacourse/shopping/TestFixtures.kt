ae57af479ee3cfcaecc9f28e43ead90b3f7775f0
kimhyemin <kimhm0728@naver.com>
2024-06-03 15:52:22 +0900

    test: ProductDetailViewModel 테스트

--- app/src/test/java/woowacourse/shopping/TestFixtures.kt
+++ app/src/test/java/woowacourse/shopping/TestFixtures.kt
@@ -3,8 +3,8 @@ package woowacourse.shopping
 import woowacourse.shopping.domain.model.CartItem
 import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.model.Quantity
-import woowacourse.shopping.domain.repository.CartRepository
-import woowacourse.shopping.ui.products.adapter.type.ProductUiModel
+import woowacourse.shopping.domain.model.RecentProduct
+import java.time.LocalDateTime
 
 val imageUrl = "https://www.naver.com/"
 val name = "올리브"
@@ -17,6 +17,8 @@ fun products(size: Int): List<Product> {
     return List(size) { product(it) }
 }
 
+fun recentProduct(productId: Int) = RecentProduct(0, product(productId), LocalDateTime.now())
+
 fun cartItem(
     id: Int,
     quantity: Quantity = Quantity(),
@@ -27,28 +29,28 @@ fun cartItem(
 fun cartItems(size: Int): List<CartItem> {
     return List(size) { cartItem(it) }
 }
-
-fun convertProductUiModel(
-    cartItems: List<CartItem>,
-    products: List<Product>,
-): List<ProductUiModel> {
-    return products.map { product ->
-        val cartItem = cartItems.firstOrNull { it.productId == product.id }
-        if (cartItem == null) {
-            ProductUiModel.from(product)
-        } else {
-            ProductUiModel.from(product, cartItem.quantity)
-        }
-    }
-}
-
-fun convertProductUiModel(
-    productEntities: List<Product>,
-    cartRepository: CartRepository,
-): List<ProductUiModel> {
-    return productEntities.map { product ->
-        runCatching { cartRepository.find(product.id) }
-            .map { ProductUiModel.from(product, it.quantity) }
-            .getOrElse { ProductUiModel.from(product) }
-    }
-}
+//
+// fun convertProductUiModel(
+//    cartItems: List<CartItem>,
+//    products: List<Product>,
+// ): List<ProductUiModel> {
+//    return products.map { product ->
+//        val cartItem = cartItems.firstOrNull { it.productId == product.id }
+//        if (cartItem == null) {
+//            ProductUiModel.from(product)
+//        } else {
+//            ProductUiModel.from(product, cartItem.quantity)
+//        }
+//    }
+// }
+//
+// fun convertProductUiModel(
+//    productEntities: List<Product>,
+//    cartRepository: CartRepository,
+// ): List<ProductUiModel> {
+//    return productEntities.map { product ->
+//        runCatching { cartRepository.find(product.id) }
+//            .map { ProductUiModel.from(product, it.quantity) }
+//            .getOrElse { ProductUiModel.from(product) }
+//    }
+// }
