b599598c4a4d7cb9d43cefdef4700888b24273b4
gaeun <geun5744@gmail.com>
2024-06-03 22:14:56 +0900

    refactor: 공통으로 사용되는 BindingAdapter util 패키지로 이동

--- app/src/main/java/woowacourse/shopping/ui/products/ProductContentsActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/products/ProductContentsActivity.kt
@@ -102,13 +102,3 @@ class ProductContentsActivity : AppCompatActivity() {
 }
 
 fun List<ProductUiModel>.isLoading() = this + LoadingUiModel + LoadingUiModel
-
-@BindingAdapter("imageUrl")
-fun ImageView.bindUrlToImage(imageUrl: String?) {
-    urlToImage(context, imageUrl)
-}
-
-@BindingAdapter("isVisible")
-fun View.setIsVisible(isVisible: Boolean) {
-    visibility = if (isVisible) View.VISIBLE else View.GONE
-}
