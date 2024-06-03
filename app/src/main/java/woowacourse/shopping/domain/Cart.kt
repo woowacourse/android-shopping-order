ecbb0ab867dc6104700854f2f070a42abd4d4e64
JoYehyun <jo990714@naver.com>
2024-06-04 02:29:19 +0900

    refactor: 도메인 모델 수정

--- app/src/main/java/woowacourse/shopping/domain/Cart.kt
+++ app/src/main/java/woowacourse/shopping/domain/Cart.kt
@@ -1,23 +1,7 @@
 package woowacourse.shopping.domain
 
 data class Cart(
-    val cartId: Long = -1L,
+    val cartId: Long,
     val product: Product,
-    val quantity: Int = DEFAULT_PURCHASE_COUNT,
-) {
-    fun toShoppingProduct(): ProductListItem.ShoppingProductItem =
-        ProductListItem.ShoppingProductItem(
-            id = this.product.id,
-            cartId = this.cartId,
-            name = this.product.name,
-            imgUrl = this.product.imgUrl,
-            price = this.product.price,
-            quantity = this.quantity,
-            category = this.product.category,
-            isChecked = false,
-        )
-
-    companion object {
-        const val DEFAULT_PURCHASE_COUNT = 1
-    }
-}
+    val quantity: Int,
+)
--- app/src/main/java/woowacourse/shopping/domain/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/Product.kt
@@ -6,16 +6,4 @@ data class Product(
     val imgUrl: String,
     val price: Long,
     val category: String,
-) {
-    fun toInitialShoppingItem() =
-        ProductListItem.ShoppingProductItem(
-            cartId = -1,
-            id = id,
-            name = name,
-            imgUrl = imgUrl,
-            price = price,
-            quantity = 0,
-            category = category,
-            isChecked = false,
-        )
-}
+)
