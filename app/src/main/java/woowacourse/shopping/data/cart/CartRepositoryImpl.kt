18a974a78e9ec4f7b1fa851d9a25cb42b86bff0e
JoYehyun <jo990714@naver.com>
2024-06-04 18:22:04 +0900

    refactor : 장바구니 datasource 추상화 및 네트워크 관련 로직 이동

--- app/src/main/java/woowacourse/shopping/data/cart/CartRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/cart/CartRepositoryImpl.kt
@@ -1,171 +1,52 @@
 package woowacourse.shopping.data.cart
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import woowacourse.shopping.data.NetworkResult
+import woowacourse.shopping.data.cart.remote.CartDataSource
 import woowacourse.shopping.data.cart.remote.RemoteCartDataSource
-import woowacourse.shopping.data.dto.response.CartQuantityResponse
-import woowacourse.shopping.data.dto.response.CartResponse
 import woowacourse.shopping.domain.Cart
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.domain.repository.LocalCartDataSource
 
 class CartRepositoryImpl(
-    private val remoteCartDataSource: RemoteCartDataSource = RemoteCartDataSource(),
+    private val remoteCartDataSource: CartDataSource = RemoteCartDataSource(),
     private val localCartDataSource: LocalCartDataSource,
 ) : CartRepository {
     override fun load(
         startPage: Int,
         pageSize: Int,
-        onSuccess: (List<Cart>, Int) -> Unit,
-        onFailure: () -> Unit,
+        callBack: (NetworkResult<List<Cart>>) -> Unit,
     ) {
-        remoteCartDataSource.load(startPage, pageSize).enqueue(
-            object : Callback<CartResponse> {
-                override fun onResponse(
-                    call: Call<CartResponse>,
-                    response: Response<CartResponse>,
-                ) {
-                    if (response.isSuccessful) {
-                        val cartData =
-                            response.body() ?: run {
-                                onFailure()
-                                return
-                            }
-                        val carts = cartData.cartDto.map { it.toCart() }
-                        onSuccess(carts, cartData.totalPages)
-                    }
-                }
-
-                override fun onFailure(
-                    call: Call<CartResponse>,
-                    t: Throwable,
-                ) {
-                    onFailure()
-                }
-            },
-        )
+        remoteCartDataSource.getCartItems(startPage, pageSize, callBack)
     }
 
     override fun saveNewCartItem(
         productId: Long,
         incrementAmount: Int,
-        onSuccess: (cartId: Long, newQuantity: Int) -> Unit,
-        onFailure: () -> Unit,
+        callBack: (NetworkResult<Long>) -> Unit,
     ) {
-        remoteCartDataSource.save(productId, incrementAmount).enqueue(
-            object : Callback<Unit> {
-                override fun onResponse(
-                    call: Call<Unit>,
-                    response: Response<Unit>,
-                ) {
-                    if (response.isSuccessful) {
-                        val location = response.headers()["Location"]
-                        if (location != null) {
-                            val segments = location.split("/")
-                            val cartId = segments.last().toLong()
-                            onSuccess(cartId, incrementAmount)
-                        }
-                    }
-                }
-
-                override fun onFailure(
-                    call: Call<Unit>,
-                    t: Throwable,
-                ) {
-                    onFailure()
-                }
-            },
-        )
+        remoteCartDataSource.saveCartItem(productId, incrementAmount, callBack)
     }
 
     override fun updateCartItemQuantity(
         cartId: Long,
         newQuantity: Int,
-        onSuccess: (Long, Int) -> Unit,
-        onFailure: () -> Unit,
+        callBack: (NetworkResult<Unit>) -> Unit,
     ) {
         if (newQuantity == 0) {
-            deleteCartItem(cartId, onSuccess, onFailure)
+            deleteCartItem(cartId, callBack)
         } else {
-            updateQuantity(cartId, newQuantity, onSuccess, onFailure)
+            remoteCartDataSource.updateCartItemQuantity(cartId.toInt(), newQuantity, callBack)
         }
-    }
-
-    private fun updateQuantity(
-        cartId: Long,
-        newQuantity: Int,
-        onSuccess: (Long, Int) -> Unit,
-        onFailure: () -> Unit,
-    ) {
-        remoteCartDataSource.update(cartId, newQuantity).enqueue(
-            object : Callback<Unit> {
-                override fun onResponse(
-                    call: Call<Unit>,
-                    response: Response<Unit>,
-                ) {
-                    if (response.isSuccessful) onSuccess(cartId, newQuantity)
-                }
-
-                override fun onFailure(
-                    call: Call<Unit>,
-                    t: Throwable,
-                ) {
-                    onFailure()
-                }
-            },
-        )
     }
 
     override fun deleteCartItem(
         cartId: Long,
-        onSuccess: (Long, Int) -> Unit,
-        onFailure: () -> Unit,
+        callBack: (NetworkResult<Unit>) -> Unit,
     ) {
-        remoteCartDataSource.delete(cartId).enqueue(
-            object : Callback<Unit> {
-                override fun onResponse(
-                    call: Call<Unit>,
-                    response: Response<Unit>,
-                ) {
-                    if (response.isSuccessful) {
-                        onSuccess(cartId, 0)
-                    }
-                }
-
-                override fun onFailure(
-                    call: Call<Unit>,
-                    t: Throwable,
-                ) {
-                    onFailure()
-                }
-            },
-        )
+        remoteCartDataSource.deleteCartItem(cartId.toInt(), callBack)
     }
 
-    override fun getCount(
-        onSuccess: (Int) -> Unit,
-        onFailure: () -> Unit,
-    ) {
-        remoteCartDataSource.getCount().enqueue(
-            object : Callback<CartQuantityResponse> {
-                override fun onResponse(
-                    call: Call<CartQuantityResponse>,
-                    response: Response<CartQuantityResponse>,
-                ) {
-                    if (response.isSuccessful) {
-                        val count = response.body()?.quantity ?: 0
-                        onSuccess(count)
-                    }
-                }
-
-                override fun onFailure(
-                    call: Call<CartQuantityResponse>,
-                    t: Throwable,
-                ) {
-                    onFailure()
-                }
-            },
-        )
+    override fun getCount(callBack: (NetworkResult<Int>) -> Unit) {
+        remoteCartDataSource.getTotalCount(callBack)
     }
 }
