199c841bd766ceded886867ba5756d973ea4a9a3
김진우 <jinwoo3661@naver.com>
2024-06-04 15:56:07 +0900

    feat: 쿠폰 & 쿠폰 금액 계산 도메인 구현

---
+++ app/src/main/java/woowacourse/shopping/domain/model/Coupon.kt
@@ -0,0 +1,16 @@
+package woowacourse.shopping.domain.model
+
+import woowacourse.shopping.utils.exception.ErrorEvent
+
+enum class Coupon {
+    FIXED5000,
+    BOGO,
+    FREESHIPPING,
+    MIRACLESALE;
+
+    companion object {
+        fun matchCoupon(code: String): Coupon {
+            return Coupon.entries.find { it.name == code } ?: throw ErrorEvent.LoadCouponEvent()
+        }
+    }
+}
---
+++ app/src/main/java/woowacourse/shopping/domain/model/CouponCalculator.kt
@@ -0,0 +1,45 @@
+package woowacourse.shopping.domain.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import woowacourse.shopping.view.cart.model.ShoppingCart
+
+class CouponCalculator(
+    private val shoppingCart: ShoppingCart,
+) {
+    private val _discountPrice : MutableLiveData<Int> = MutableLiveData(DEFAULT_PRICE)
+    val discountPrice: LiveData<Int> get() = _discountPrice
+
+    fun selectCoupon(coupon: Coupon){
+        when(coupon){
+            Coupon.FIXED5000 -> setCouponFIXED5000()
+            Coupon.BOGO -> setCouponBOGO()
+            Coupon.FREESHIPPING -> setCouponFREESHIPPING()
+            Coupon.MIRACLESALE -> setCouponMIRACLESALE()
+        }
+    }
+
+    private fun setCouponFIXED5000(){
+
+    }
+
+    private fun setCouponBOGO(){
+
+    }
+
+    private fun setCouponFREESHIPPING(){
+
+    }
+
+    private fun setCouponMIRACLESALE(){
+
+    }
+
+    fun matchCoupon(couponCode:String): Coupon {
+        return Coupon.matchCoupon(couponCode)
+    }
+
+    companion object {
+        const val DEFAULT_PRICE = 0
+    }
+}
