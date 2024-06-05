5298a77e581ace3b4111ecaa7618a0b56a0556dd
kimhyemin <kimhm0728@naver.com>
2024-06-05 18:44:28 +0900

    remove: 사용하지 않는 파일 삭제

--- app/src/main/java/woowacourse/shopping/data/cart/remote/Mapper.kt
+++ app/src/main/java/woowacourse/shopping/data/cart/remote/Mapper.kt
@@ -1,11 +1,8 @@
 package woowacourse.shopping.data.cart.remote
 
 import woowacourse.shopping.domain.model.CartItem
-import woowacourse.shopping.domain.model.CartPageAttribute
 import woowacourse.shopping.domain.model.Quantity
 
 fun Content.toCartItem() = CartItem(id, product.id, Quantity(quantity))
 
 fun CartResponse.toCartItems() = content.map { it.toCartItem() }
-
-fun CartResponse.toCartPage() = CartPageAttribute(totalPages, totalElements, first, last)
