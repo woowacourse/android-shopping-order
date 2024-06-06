43947eab1a9c863b9ab46d4e95ff3d5db66e1431
gaeun <geun5744@gmail.com>
2024-06-06 23:19:56 +0900

    feat: CouponRepository 구현

---
+++ app/src/main/java/woowacourse/shopping/data/repository/CouponRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package woowacourse.shopping.data.repository
+
+import woowacourse.shopping.data.datasource.CouponDataSource
+import woowacourse.shopping.data.datasource.impl.RemoteCouponDataSource
+import woowacourse.shopping.data.remote.dto.response.BuyXGetYCoupon
+import woowacourse.shopping.data.remote.dto.response.FixedDiscountCoupon
+import woowacourse.shopping.data.remote.dto.response.FreeShippingCoupon
+import woowacourse.shopping.data.remote.dto.response.PercentageDiscountCoupon
+import woowacourse.shopping.domain.model.coupon.Buy2Free1
+import woowacourse.shopping.domain.model.coupon.Coupon
+import woowacourse.shopping.domain.model.coupon.Discount5000
+import woowacourse.shopping.domain.model.coupon.FreeShipping
+import woowacourse.shopping.domain.model.coupon.MiracleCoupon
+import woowacourse.shopping.domain.repository.CouponRepository
+
+class CouponRepositoryImpl(private val dataSource:CouponDataSource = RemoteCouponDataSource()) : CouponRepository {
+    override suspend  fun getCoupons(): List<Coupon> = dataSource.getCoupons().map { it.toDomain() }
+
+    private fun woowacourse.shopping.data.remote.dto.response.Coupon.toDomain(): Coupon =
+        when (this) {
+            is BuyXGetYCoupon -> Buy2Free1(
+                id, code, description, expirationDate, discountType, buyQuantity, getQuantity
+            )
+
+            is FixedDiscountCoupon -> Discount5000(
+                id, description, discountType, expirationDate, discount, minimumAmount, code
+            )
+
+            is FreeShippingCoupon -> FreeShipping(
+                id, code, description, expirationDate, discountType, minimumAmount,
+            )
+
+            is PercentageDiscountCoupon -> MiracleCoupon(
+                id,
+                code,
+                description,
+                expirationDate,
+                discountType,
+                discount,
+                availableTime.start,
+                availableTime.end
+            )
+        }
+}
---
+++ app/src/main/java/woowacourse/shopping/domain/repository/CouponRepository.kt
@@ -0,0 +1,9 @@
+package woowacourse.shopping.domain.repository
+
+import woowacourse.shopping.domain.model.coupon.Coupon
+
+
+interface CouponRepository {
+
+    suspend fun getCoupons():List<Coupon>
+}
