b5d9f1d5ff734e9f4f6868de0daa3fa6948801cb
gaeun <geun5744@gmail.com>
2024-06-06 18:44:16 +0900

    chore: room ktx implementation 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
     // okhttp
     implementation("com.squareup.okhttp3:mockwebserver:4.12.0")
