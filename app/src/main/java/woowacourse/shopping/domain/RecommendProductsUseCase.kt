dcd1d14a97405849eaeea8e8bfe94c13680b396d
songpink <jejeje04@naver.com>
2024-06-06 17:19:03 +0900

    refactor: 함수 분리

--- app/src/main/java/woowacourse/shopping/domain/RecommendProductsUseCase.kt
+++ app/src/main/java/woowacourse/shopping/domain/RecommendProductsUseCase.kt
@@ -19,27 +19,31 @@ class RecommendProductsUseCase(
 
     suspend operator fun invoke(): List<Product> {
         val productsDeferred =
-            CoroutineScope(Dispatchers.IO).async {
-                val recentProducts = productRepository.recentProducts(1).getOrNull() ?: emptyList()
-                val firstProduct = recentProducts.firstOrNull()
-                val category = firstProduct?.category
-                var recommendProducts =
-                    // 카테고리가 없으면 전체 상품에서 추천 상품을 가져온다.
-                    if (category == null) {
-                        productRepository.products(0, PRODUCT_SIZE).getOrNull() ?: emptyList()
-                    } else {
-                        // 카테고리가 있으면 해당 카테고리의 상품에서 추천 상품을 가져온다.
-                        productRepository.products(category = category, 0, PRODUCT_SIZE).getOrNull()
-                            ?: emptyList()
-                    }
-                // 카트에 있는 상품은 추천 상품에서 제외한다.
-                recommendProducts =
-                    recommendProducts.filterNot {
-                        cartProducts.contains(it)
-                    }.take(RECOMMEND_PRODUCT_SIZE)
-                recommendProducts
-            }
+            CoroutineScope(Dispatchers.IO).async { obtainRecommendProducts() }
         return productsDeferred.await()
+    }
+
+    private suspend fun obtainRecommendProducts(): List<Product> {
+        val recentProducts =
+            productRepository.recentProducts(1).getOrNull() ?: emptyList()
+        val firstProduct = recentProducts.firstOrNull()
+        val category = firstProduct?.category
+        var recommendProducts = loadRecommendProducts(category)
+        // 카트에 있는 상품은 추천 상품에서 제외한다.
+        recommendProducts =
+            recommendProducts.filterNot { cartProducts.contains(it) }.take(RECOMMEND_PRODUCT_SIZE)
+        return recommendProducts
+    }
+
+    private fun loadRecommendProducts(category: String?): List<Product> {
+        return if (category == null) {
+            // 카테고리가 없으면 전체 상품에서 추천 상품을 가져온다.
+            productRepository.products(0, PRODUCT_SIZE).getOrNull() ?: emptyList()
+        } else {
+            // 카테고리가 있으면 해당 카테고리의 상품에서 추천 상품을 가져온다.
+            productRepository.products(category = category, 0, PRODUCT_SIZE).getOrNull()
+                ?: emptyList()
+        }
     }
 
     companion object {
