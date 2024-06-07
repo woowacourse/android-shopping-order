5dc8ef2a9c1b4e3da45d57423aba39b5bb61644d
JoYehyun <jo990714@naver.com>
2024-06-07 13:32:52 +0900

    chore: 코루틴 적용을 위한 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.databinding:databinding-runtime:8.4.0")
     implementation("androidx.activity:activity:1.8.0")
-    implementation("androidx.test.espresso:espresso-contrib:3.5.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
@@ -89,4 +88,6 @@ dependencies {
     androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("androidx.room:room-ktx:2.6.1")
 }
