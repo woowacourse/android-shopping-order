23afe905f5f4a375449df539a72b5d96fbf90c60
songpink <jejeje04@naver.com>
2024-06-07 15:29:53 +0900

    refactor: 필요 없는 코루틴 제거

--- app/src/main/java/woowacourse/shopping/data/shopping/recent/RecentProductDataSourceImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/shopping/recent/RecentProductDataSourceImpl.kt
@@ -1,8 +1,5 @@
 package woowacourse.shopping.data.shopping.recent
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import woowacourse.shopping.local.dao.RecentProductDao
 
 class RecentProductDataSourceImpl(
@@ -10,21 +7,13 @@ class RecentProductDataSourceImpl(
 ) : RecentProductDataSource {
     override suspend fun recentProducts(size: Int): Result<List<RecentProductData>> {
         return runCatching {
-            val recentProductsDeferred =
-                CoroutineScope(Dispatchers.IO).async {
-                    recentProductDao.loadProducts(size)
-                }
-            recentProductsDeferred.await().map { it.toData() }
+            recentProductDao.loadProducts(size).map { it.toData() }
         }
     }
 
     override suspend fun saveRecentProduct(product: RecentProductData): Result<Long> {
         return runCatching {
-            val idDeferred =
-                CoroutineScope(Dispatchers.IO).async {
-                    recentProductDao.saveProduct(product.toEntity())
-                }
-            idDeferred.await()
+            recentProductDao.saveProduct(product.toEntity())
         }
     }
 }
