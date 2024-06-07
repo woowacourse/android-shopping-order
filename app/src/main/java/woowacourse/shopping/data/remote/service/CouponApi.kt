c1d07e471fe758a00eab4ae35a56263ee6c6dba7
kkosang <eiwoqp77@gmail.com>
2024-06-07 17:34:26 +0900

    feat: CouponApi,DTO 정의

--- app/src/main/java/woowacourse/shopping/data/remote/service/CouponApi.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/service/CouponApi.kt
@@ -1,3 +1,12 @@
 package woowacourse.shopping.data.remote.service
 
-interface CouponApi
+import retrofit2.http.GET
+import retrofit2.http.Header
+import woowacourse.shopping.data.remote.dto.response.Coupons
+
+interface CouponApi {
+    @GET("coupons")
+    suspend fun getCoupons(
+        @Header("accept") accept: String = "*/*",
+    ): List<Coupons>
+}
