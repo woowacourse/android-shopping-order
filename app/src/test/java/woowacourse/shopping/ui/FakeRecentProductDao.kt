7ab7b52602818f7f31d7e921c765027ebf707625
Namyunsuk <yunsuk0328@naver.com>
2024-06-08 16:32:13 +0900

    refactor: 리팩토링에 따른 테스트 수정

--- app/src/test/java/woowacourse/shopping/ui/FakeRecentProductDao.kt
+++ app/src/test/java/woowacourse/shopping/ui/FakeRecentProductDao.kt
@@ -8,7 +8,7 @@ object FakeRecentProductDao : RecentProductDao {
     private var id: Long = 0
     private val recentProducts = mutableMapOf<Long, RecentProduct>()
 
-    override fun insert(recentProduct: RecentProduct): Long {
+    override suspend fun insert(recentProduct: RecentProduct): Long {
         val recentTime = LocalDateTime.now()
         val oldRecentProduct =
             recentProducts.values.find { it.productId == recentProduct.productId }
@@ -22,18 +22,18 @@ object FakeRecentProductDao : RecentProductDao {
         return oldRecentProduct.id
     }
 
-    override fun deleteAll() {
+    override suspend fun deleteAll() {
         recentProducts.clear()
     }
 
-    override fun findMostRecentProduct(): RecentProduct? {
+    override suspend fun findMostRecentProduct(): RecentProduct? {
         if (recentProducts.isEmpty()) {
             return null
         }
         return recentProducts.values.maxBy { it.recentTime }
     }
 
-    override fun findAll(): List<RecentProduct> {
+    override suspend fun findAll(): List<RecentProduct> {
         return recentProducts.values.sortedByDescending { it.recentTime }
     }
 }
