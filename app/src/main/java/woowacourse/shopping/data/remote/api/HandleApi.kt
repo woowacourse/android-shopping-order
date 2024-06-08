ebaad7374c5f965aeecfbc7d7f630cdadac9d928
gaeun <geun5744@gmail.com>
2024-06-09 00:44:35 +0900

    feat: handleApi 내부 로직 코루틴 적용

--- app/src/main/java/woowacourse/shopping/data/remote/api/HandleApi.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/api/HandleApi.kt
@@ -1,20 +1,27 @@
 package woowacourse.shopping.data.remote.api
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import retrofit2.HttpException
 import retrofit2.Response
+import woowacourse.shopping.ui.utils.exceptionHandler
 
-suspend fun <T : Any?> handleApi(execute: suspend () -> Response<T>): ApiResult<T> {
-    return try {
-        val response = execute()
-        val body = response.body()
-        if (response.isSuccessful && body != null) {
-            ApiResult.Success(body)
-        } else {
-            ApiResult.Error(code = response.code(), message = response.message())
+suspend inline fun <T : Any?> handleApi(crossinline execute: suspend () -> Response<T>): ApiResult<T> =
+    coroutineScope {
+        val deferredResult = async<ApiResult<T>>(exceptionHandler()) {
+            try {
+                val response = execute()
+                val body = response.body()
+                if (response.isSuccessful && body != null) {
+                    ApiResult.Success(body)
+                } else {
+                    ApiResult.Error(code = response.code(), message = response.message())
+                }
+            } catch (e: HttpException) {
+                ApiResult.Error(code = e.code(), message = e.message())
+            } catch (e: Throwable) {
+                ApiResult.Exception(e)
+            }
         }
-    } catch (e: HttpException) {
-        ApiResult.Error(code = e.code(), message = e.message())
-    } catch (e: Throwable) {
-        ApiResult.Exception(e)
+        return@coroutineScope deferredResult.await()
     }
-}
