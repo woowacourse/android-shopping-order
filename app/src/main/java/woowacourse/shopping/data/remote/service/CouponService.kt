50effa728400cd76752c1138f4f876480ca7bc20
gaeun <geun5744@gmail.com>
2024-06-09 00:48:03 +0900

    refactor: 변경된 dto 네이밍 적용

--- app/src/main/java/woowacourse/shopping/data/remote/service/CouponService.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/service/CouponService.kt
@@ -2,9 +2,9 @@ package woowacourse.shopping.data.remote.service
 
 import retrofit2.Response
 import retrofit2.http.GET
-import woowacourse.shopping.data.remote.dto.response.Coupon
+import woowacourse.shopping.data.remote.dto.response.CouponDto
 
 interface CouponService {
     @GET("/coupons")
-    suspend fun getCoupons(): Response<List<Coupon>>
+    suspend fun getCoupons(): Response<List<CouponDto>>
 }
