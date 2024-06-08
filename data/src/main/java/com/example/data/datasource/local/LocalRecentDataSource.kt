2eafa15c5a594ba2efbca216d6dcd34b4fd24f41
aprilgom <aprilgom@gmail.com>
2024-06-08 16:21:49 +0900

    feat: LocalRecentDataSource 인터페이스 생성

--- data/src/main/java/com/example/data/datasource/local/LocalRecentDataSource.kt
+++ data/src/main/java/com/example/data/datasource/local/LocalRecentDataSource.kt
@@ -1,3 +1,21 @@
 package com.example.data.datasource.local
 
-class LocalRecentDataSource
+import com.example.data.datasource.local.room.dao.RecentProductDao
+import com.example.domain.datasource.RecentProductDataSource
+import com.example.domain.model.Product
+import com.example.domain.model.RecentProduct
+
+class LocalRecentDataSource(private val recentProductDao: RecentProductDao) :
+    RecentProductDataSource {
+    override fun findLastOrNull(): RecentProduct? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findRecentProducts(): List<RecentProduct> {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(product: Product) {
+        TODO("Not yet implemented")
+    }
+}
