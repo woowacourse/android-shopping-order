d8a711495d0563088e839846d5d0bea956df2837
kimhyemin <kimhm0728@naver.com>
2024-06-08 17:42:00 +0900

    test: 쿠폰 적용 시간 AvailableTime 테스트

--- app/src/main/java/woowacourse/shopping/domain/model/AvailableTime.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/AvailableTime.kt
@@ -6,9 +6,17 @@ class AvailableTime(
     private val start: LocalTime,
     private val end: LocalTime,
 ) {
+    init {
+        require(start.isBefore(end)) { INVALID_AVAILABLE_TIME_RANGE_MESSAGE }
+    }
+
     fun available(time: LocalTime): Boolean {
         if (start == time) return true
         if (end == time) return true
-        return start.isAfter(time) && end.isBefore(time)
+        return start.isBefore(time) && end.isAfter(time)
+    }
+
+    companion object {
+        private const val INVALID_AVAILABLE_TIME_RANGE_MESSAGE = "시작 시간은 끝 시간보다 이전이어야 합니다."
     }
 }
