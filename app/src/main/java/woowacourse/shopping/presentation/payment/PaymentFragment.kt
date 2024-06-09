4a6c990e1de562bac3010929de31447c32573ecf
songpink <jejeje04@naver.com>
2024-06-09 21:33:13 +0900

    feat: 결제 화면에서 뒤로가기 기능 구현

--- app/src/main/java/woowacourse/shopping/presentation/payment/PaymentFragment.kt
+++ app/src/main/java/woowacourse/shopping/presentation/payment/PaymentFragment.kt
@@ -1,21 +1,30 @@
 package woowacourse.shopping.presentation.payment
 
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import woowacourse.shopping.R
+import woowacourse.shopping.data.cart.CartRepositoryInjector
 import woowacourse.shopping.data.payment.CouponRepositoryInjector
 import woowacourse.shopping.databinding.FragmentPaymentBinding
 import woowacourse.shopping.presentation.base.BindingFragment
 import woowacourse.shopping.presentation.cart.recommend.RecommendNavArgs
+import woowacourse.shopping.presentation.navigation.ShoppingNavigator
 
 class PaymentFragment : BindingFragment<FragmentPaymentBinding>(R.layout.fragment_payment) {
     private lateinit var adapter: CouponAdapter
     private val viewModel by viewModels<PaymentViewModel> {
+        val cartRepository = CartRepositoryInjector.cartRepository()
         val couponRepository = CouponRepositoryInjector.couponRepository()
-        PaymentViewModel.factory(couponRepository)
+        PaymentViewModel.factory(cartRepository, couponRepository)
     }
+    private val navigator by lazy { requireActivity() as ShoppingNavigator }
 
     override fun onViewCreated(
         view: View,
@@ -24,13 +33,39 @@ class PaymentFragment : BindingFragment<FragmentPaymentBinding>(R.layout.fragmen
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
             vm = viewModel
-            Log.d("alsong", "onViewCreated: ${viewModel.coupons.value}")
             lifecycleOwner = viewLifecycleOwner
         }
         viewModel.coupons.observe(viewLifecycleOwner) {
             adapter.setData(it)
         }
         initViews()
+        initAppBar()
+    }
+
+    private fun initAppBar() {
+        (requireActivity() as? AppCompatActivity)?.supportActionBar?.apply {
+            title = "Cart"
+            setDisplayHomeAsUpEnabled(true)
+        }
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(
+                    menu: Menu,
+                    menuInflater: MenuInflater,
+                ) {
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    if (menuItem.itemId == android.R.id.home) {
+                        navigator.popBackStack()
+                        return true
+                    }
+                    return false
+                }
+            },
+            viewLifecycleOwner,
+        )
     }
 
     private fun initViews() {
