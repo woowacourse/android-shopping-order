73979a6b062d30584df10b18e9ebd43709da0ec1
sh1mj1 <7wlgns@gmail.com>
2024-06-09 21:54:43 +0900

    refactor ShoppingProductsRepository: not-suspend 함수 제거

--- app/src/main/java/woowacourse/shopping/domain/repository/DefaultShoppingProductRepository.kt
+++ app/src/main/java/woowacourse/shopping/domain/repository/DefaultShoppingProductRepository.kt
@@ -9,28 +9,6 @@ class DefaultShoppingProductRepository(
     private val productsSource: ProductDataSource,
     private val cartSource: ShoppingCartDataSource,
 ) : ShoppingProductsRepository {
-    private fun productQuantity(productId: Long): Int {
-        return cartSource.findByProductId(productId)?.quantity ?: 0
-    }
-
-    override fun pagedProducts(page: Int): List<Product> {
-        val productsData = productsSource.findByPaged(page)
-        return productsData.map { productData ->
-            productData.toDomain(productQuantity(productData.id))
-        }
-    }
-
-    override fun allProductsUntilPage(page: Int): List<Product> {
-        val productsData = productsSource.findAllUntilPage(page)
-        return productsData.map { productData ->
-            productData.toDomain(productQuantity(productData.id))
-        }
-    }
-
-    override fun loadProduct(id: Long): Product = productsSource.findById(id).toDomain(productQuantity(id))
-
-    override fun isFinalPage(page: Int): Boolean = productsSource.isFinalPage(page)
-
     override suspend fun pagedProducts2(page: Int): Result<List<Product>> =
         productsSource.findByPaged2(page).map { productsData ->
             productsData.map { productData ->
--- app/src/main/java/woowacourse/shopping/domain/repository/ShoppingProductsRepository.kt
+++ app/src/main/java/woowacourse/shopping/domain/repository/ShoppingProductsRepository.kt
@@ -3,14 +3,6 @@ package woowacourse.shopping.domain.repository
 import woowacourse.shopping.domain.model.Product
 
 interface ShoppingProductsRepository {
-    fun pagedProducts(page: Int): List<Product>
-
-    fun allProductsUntilPage(page: Int): List<Product>
-
-    fun loadProduct(id: Long): Product
-
-    fun isFinalPage(page: Int): Boolean
-
     suspend fun pagedProducts2(page: Int): Result<List<Product>>
 
     suspend fun allProductsUntilPage2(page: Int): Result<List<Product>>
