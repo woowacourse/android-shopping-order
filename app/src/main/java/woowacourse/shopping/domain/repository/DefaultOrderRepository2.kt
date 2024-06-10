86f99f208d8268a075310a1877da49c12e6216a2
sh1mj1 <7wlgns@gmail.com>
2024-06-10 16:16:48 +0900

    feat OrderRepository2: 주문과 주문 아이템들 레포지토리
    - 점진적 리팩터링. 추후 교체해야 함

---
+++ app/src/main/java/woowacourse/shopping/domain/repository/DefaultOrderRepository2.kt
@@ -0,0 +1,65 @@
+package woowacourse.shopping.domain.repository
+
+import android.util.Log
+import woowacourse.shopping.data.model.toDomain
+import woowacourse.shopping.data.source.OrderDataSource2
+import woowacourse.shopping.data.source.ShoppingCartDataSource
+import woowacourse.shopping.domain.model.OrderItem
+import woowacourse.shopping.domain.model.Orders
+import java.time.LocalDateTime
+
+class DefaultOrderRepository2(
+    private val orderSource: OrderDataSource2,
+    private val cartSource: ShoppingCartDataSource,
+) : OrderRepository2 {
+    override suspend fun order(): Result<Unit> = orderSource.order()
+
+    override suspend fun save(orderItem: OrderItem): Result<Unit> = orderSource.saveOrderItem(orderItem)
+
+    // 추가, 감소, 제거
+    override suspend fun updateOrderItem(productId: Long, quantity: Int): Result<Unit> {
+        val cartItem = cartSource.findCartItemByProductId(productId).getOrThrow()
+
+        if (quantity == 0) {
+            // 카트 소스에서도 제거하고, orderSource 에서도 제거
+            cartSource.removeCartItem(cartItem.id).getOrThrow()
+            return orderSource.removeOrderItem(cartItem.id)
+        }
+
+        return orderSource.saveOrderItem(
+            orderItem = OrderItem(
+                cartItemId = cartItem.id,
+                quantity = quantity,
+                product = cartItem.product.toDomain(),
+            )
+        )
+    }
+
+    override suspend fun loadAllOrders(): Result<Orders> {
+        return orderSource.loadAllOrders().map { orderItems ->
+            Orders(
+                orderItems = orderItems,
+                totalPrice = orderItems.sumOf { it.product.price * it.quantity },
+                orderDateTime = LocalDateTime.now(),
+            ).also {
+                Log.d(TAG, "orderes: $it")
+            }
+        }
+    }
+
+    override suspend fun allOrderItemsQuantity(): Result<Int> = orderSource.loadAllOrders().map { orderItems ->
+        orderItems.sumOf { orderItem -> orderItem.quantity }
+    }
+
+    override suspend fun orderItemsTotalPrice(): Result<Int> = orderSource.loadAllOrders().map { orderItems ->
+        orderItems.sumOf { orderItem -> orderItem.product.price * orderItem.quantity }
+    }
+
+    override suspend fun clear(): Result<Unit> = orderSource.clear()
+
+
+    companion object {
+        private const val TAG = "OrderRepository2"
+
+    }
+}
--- app/src/main/java/woowacourse/shopping/domain/repository/OrderRepository.kt
+++ app/src/main/java/woowacourse/shopping/domain/repository/OrderRepository.kt
@@ -1,5 +1,8 @@
 package woowacourse.shopping.domain.repository
 
+import woowacourse.shopping.domain.model.OrderItem
+import woowacourse.shopping.domain.model.Orders
+
 interface OrderRepository {
     suspend fun order(cartItemIds: List<Long>): Result<Unit>
 
@@ -14,3 +17,28 @@ interface OrderRepository {
 
     suspend fun orderItemsTotalPrice(): Result<Int>
 }
+
+interface OrderRepository2 {
+    suspend fun order(): Result<Unit>
+
+    suspend fun save(
+        orderItem: OrderItem
+    ): Result<Unit>
+
+    suspend fun updateOrderItem(
+        productId: Long,
+        quantity: Int,
+    ): Result<Unit>
+
+
+    suspend fun loadAllOrders(): Result<Orders>
+
+    suspend fun allOrderItemsQuantity(): Result<Int>
+
+    suspend fun orderItemsTotalPrice(): Result<Int>
+
+    suspend fun clear(): Result<Unit>
+
+}
+
+
