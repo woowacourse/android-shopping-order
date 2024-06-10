20c567a43c104a2a27407dbd877a7756780739ca
gaeun <geun5744@gmail.com>
2024-06-10 15:05:38 +0900

    feat: 불필요한 coroutineScope 제거

--- app/src/main/java/woowacourse/shopping/data/remote/api/HandleApi.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/api/HandleApi.kt
@@ -1,27 +1,19 @@
 package woowacourse.shopping.data.remote.api
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import retrofit2.HttpException
 import retrofit2.Response
 
 suspend inline fun <T : Any?> handleApi(crossinline execute: suspend () -> Response<T>): ApiResponse<T> =
-    coroutineScope {
-        val deferredResult =
-            async<ApiResponse<T>> {
-                try {
-                    val response = execute()
-                    val body = response.body()
-                    if (response.isSuccessful && body != null) {
-                        ApiResponse.Success(body)
-                    } else {
-                        ApiResponse.Error(code = response.code(), message = response.message())
-                    }
-                } catch (e: HttpException) {
-                    ApiResponse.Error(code = e.code(), message = e.message())
-                } catch (e: Throwable) {
-                    ApiResponse.Exception(e)
-                }
-            }
-        return@coroutineScope deferredResult.await()
+    try {
+        val response = execute()
+        val body = response.body()
+        if (response.isSuccessful && body != null) {
+            ApiResponse.Success(body)
+        } else {
+            ApiResponse.Error(code = response.code(), message = response.message())
+        }
+    } catch (e: HttpException) {
+        ApiResponse.Error(code = e.code(), message = e.message())
+    } catch (e: Throwable) {
+        ApiResponse.Exception(e)
     }
