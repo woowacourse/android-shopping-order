2bf229bd2d5dcd35bbb893172c8962a99bd16014
hevton <hevton22@gmail.com>
2024-06-11 11:14:19 +0900

    test: OverrideMockKs 공부

--- app/src/test/java/woowacourse/shopping/presentation/ui/payment/PaymentViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/presentation/ui/payment/PaymentViewModelTest.kt
@@ -1,12 +1,18 @@
 package woowacourse.shopping.presentation.ui.payment
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.OverrideMockKs
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.shopping.InstantTaskExecutorExtension
@@ -31,8 +37,26 @@ class PaymentViewModelTest {
     @RelaxedMockK
     private lateinit var recentProductRepository: RecentProductRepository
 
-    @InjectMockKs
+    private val _uiState = MutableLiveData<PaymentUiState>()
+    val uiState: LiveData<PaymentUiState> get() = _uiState
+
+    @OverrideMockKs
     private lateinit var viewModel: PaymentViewModel
+
+
+
+    @BeforeEach
+    fun setUp() {
+        _uiState.value = PaymentUiState().copy(
+            cartProducts = cartProducts
+        )
+    }
+
+    @Test
+    fun `dd`() {
+        val state = viewModel.uiState.getOrAwaitValue()
+        state.cartProducts.size shouldBe 51
+    }
 
     @Test
     fun `사용할 수 있는 쿠폰을 로드한다`() =
