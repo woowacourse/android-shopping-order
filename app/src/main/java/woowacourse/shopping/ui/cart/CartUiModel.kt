18481ff8b22b0c06b76f08da31bcadc42a14aff2
Namyunsuk <yunsuk0328@naver.com>
2024-06-12 16:45:16 +0900

    refactor: 불필요한 생성자 프로퍼티 제거

--- app/src/main/java/woowacourse/shopping/ui/cart/CartUiModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartUiModel.kt
@@ -10,7 +10,6 @@ data class CartUiModel(
     val quantity: Quantity,
     val imageUrl: String,
     val isChecked: Boolean = false,
-    val isLoading: Boolean = true,
 ) {
     val totalPrice = price * quantity.value
 }
