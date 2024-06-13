bd506a7bed3bb45a91957aee9b0b591b8e05ecde
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-06-13 16:57:56 +0900

    refactor: dao suspend fun으로 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,5 +78,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
+    implementation("androidx.room:room-ktx:2.6.1")
+
     implementation("com.facebook.shimmer:shimmer:0.5.0")
 }
