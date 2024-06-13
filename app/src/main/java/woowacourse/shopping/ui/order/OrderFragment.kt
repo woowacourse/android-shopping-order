aaffa911ee69bafed20593a83d4b14e4a7e789ed
sh1mj1 <7wlgns@gmail.com>
2024-06-14 01:32:37 +0900

    feat OrderFragment: 주문 상품 로직에 대한 에러 핸들링

--- app/src/main/java/woowacourse/shopping/ui/order/OrderFragment.kt
+++ app/src/main/java/woowacourse/shopping/ui/order/OrderFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import woowacourse.shopping.R
 import woowacourse.shopping.databinding.FragmentOrderBinding
 import woowacourse.shopping.ui.FragmentNavigator
+import woowacourse.shopping.ui.order.event.OrderError
 import woowacourse.shopping.ui.order.event.OrderEvent
 
 class OrderFragment : Fragment() {
@@ -41,8 +45,17 @@ class OrderFragment : Fragment() {
 
         observeRecommendedProducts()
         observeEvent()
+        observeError()
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeRecommendedProducts()
+        observeEvent()
+        observeError()
     }
 
     private fun observeRecommendedProducts() {
@@ -59,6 +72,25 @@ class OrderFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun observeError() {
+        viewModel.error.observe(viewLifecycleOwner) { error ->
+            when (error) {
+                OrderError.LoadRecommendedProducts -> showToast(R.string.error_load_recommended_products)
+                OrderError.CalculateOrderItemsQuantity -> showToast(R.string.error_calculate_order_items_quantity)
+                OrderError.CalculateOrderItemsTotalPrice -> showToast(R.string.error_calculate_order_items_total_price)
+                OrderError.UpdateOrderItem -> showToast(R.string.error_update_order_item)
+            }
+        }
+    }
+
+    private fun showToast(@StringRes stringId: Int) {
+        Toast.makeText(
+            requireContext(),
+            stringId,
+            Toast.LENGTH_SHORT,
+        ).show()
     }
 
     companion object {
