24f52103deb5cc4ded4a6d05bc4a16be41b708f6
gaeun <geun5744@gmail.com>
2024-06-13 20:08:36 +0900

    delete : 불필요한 메서드 제거

--- app/src/main/java/woowacourse/shopping/data/remote/api/HandleApi.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/api/HandleApi.kt
@@ -27,26 +27,3 @@ suspend inline fun <reified T> handleApi(crossinline execute: suspend () -> Resp
     } catch (e: Throwable) {
         throw e
     }
-
-suspend inline fun <reified T> handleCouponApi(crossinline execute: suspend () -> Response<T>): Result<T, DataError> =
-    try {
-        val response = execute()
-        val body = response.body()
-        if (response.isSuccessful && body != null) {
-            Result.Success(body)
-        } else if (response.isSuccessful && body == null && T::class == Unit::class) {
-            Result.Success(Unit as T)
-        } else if (response.code() == 401 || response.code() == 403) {
-            Result.Error(DataError.Network.NO_INTERNET)
-        } else if (response.code() == 404) {
-            Result.Error(DataError.NotFound)
-        } else if (response.code() == 500) {
-            Result.Error(DataError.Network.SERVER)
-        } else {
-            Result.Error(DataError.UNKNOWN)
-        }
-    } catch (e: HttpException) {
-        Result.Error(DataError.Network.NO_INTERNET)
-    } catch (e: Throwable) {
-        throw e
-    }
