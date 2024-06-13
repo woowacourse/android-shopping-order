42e705d80ad46c58542d492df168490eedc12929
hevton <hevton22@gmail.com>
2024-06-13 11:25:50 +0900

    refactor: non null 연산자 삭제

--- app/src/main/java/woowacourse/shopping/data/remote/dto/mapper/CouponMapper.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/dto/mapper/CouponMapper.kt
@@ -1,5 +1,10 @@
 package woowacourse.shopping.data.remote.dto.mapper
 
+import woowacourse.shopping.data.remote.dto.mapper.CouponType.Companion.DEFAULT_BUY_QUANTITY
+import woowacourse.shopping.data.remote.dto.mapper.CouponType.Companion.DEFAULT_DISCOUNT
+import woowacourse.shopping.data.remote.dto.mapper.CouponType.Companion.DEFAULT_GET_QUANTITY
+import woowacourse.shopping.data.remote.dto.mapper.CouponType.Companion.DEFAULT_MINIMUM_AMOUNT
+import woowacourse.shopping.data.remote.dto.mapper.CouponType.Companion.DEFAULT_TIME
 import woowacourse.shopping.data.remote.dto.response.CouponResponse
 import woowacourse.shopping.domain.coupon.Bogo
 import woowacourse.shopping.domain.coupon.Coupon
@@ -10,18 +15,18 @@ import woowacourse.shopping.domain.coupon.Unknown
 
 fun CouponResponse.toDomain(): Coupon {
     return when (CouponType.fromCode(code)) {
-        CouponType.FIXED5000 -> Fixed(id, code, description, discountType, expirationDate, discount!!, minimumAmount!!)
-        CouponType.BOGO -> Bogo(id, code, description, expirationDate, buyQuantity!!, getQuantity!!, discountType)
-        CouponType.FREESHIPPING -> FreeShipping(id, code, description, expirationDate, minimumAmount!!, discountType)
+        CouponType.FIXED5000 -> Fixed(id, code, description, discountType, expirationDate, discount ?: DEFAULT_DISCOUNT, minimumAmount ?: DEFAULT_MINIMUM_AMOUNT)
+        CouponType.BOGO -> Bogo(id, code, description, expirationDate, buyQuantity ?: DEFAULT_BUY_QUANTITY, getQuantity ?: DEFAULT_GET_QUANTITY, discountType)
+        CouponType.FREESHIPPING -> FreeShipping(id, code, description, expirationDate, minimumAmount ?: DEFAULT_MINIMUM_AMOUNT, discountType)
         CouponType.MIRACLESALE ->
             MiracleSale(
                 id,
                 code,
                 description,
                 expirationDate,
-                discount!!,
-                availableTime!!.start!!,
-                availableTime.end!!,
+                discount ?: DEFAULT_DISCOUNT,
+                availableTime?.start ?: DEFAULT_TIME,
+                availableTime?.end ?: DEFAULT_TIME,
                 discountType,
             )
         CouponType.UNKNOWN -> {
@@ -39,6 +44,11 @@ enum class CouponType(val code: String) {
     ;
 
     companion object {
+        const val DEFAULT_DISCOUNT = 0
+        const val DEFAULT_MINIMUM_AMOUNT = 0
+        const val DEFAULT_BUY_QUANTITY = 0
+        const val DEFAULT_GET_QUANTITY = 0
+        const val DEFAULT_TIME = "2024-06-13"
         fun fromCode(code: String): CouponType {
             return entries.find { couponType -> couponType.code == code } ?: UNKNOWN
         }
