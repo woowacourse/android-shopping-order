fcbb175998e99309e83defb22f6e06a6bb5ce2a6
haeum808 <haeumueah@gmail.com>
2024-06-13 21:29:25 +0900

    del: 불필요한 UiState 제거

--- app/src/main/java/woowacourse/shopping/presentation/ui/shoppingcart/orderrecommend/OrderRecommendUiState.kt
+++ app/src/main/java/woowacourse/shopping/presentation/ui/shoppingcart/orderrecommend/OrderRecommendUiState.kt
@@ -2,15 +2,6 @@ package woowacourse.shopping.presentation.ui.shoppingcart.orderrecommend
 
 import woowacourse.shopping.domain.model.Cart
 
-data class OrderRecommendUiState(
-    val recommendCarts: List<Cart> = emptyList(),
-    val orderCarts: MutableMap<Int, Cart> = hashMapOf(),
-) {
-    val orderTotalPrice get() = orderCarts.values.sumOf { it.totalPrice }
-
-    val totalQuantity get() = orderCarts.values.sumOf { it.quantity }
-}
-
 sealed interface RecommendCartsUiState {
     data class Success(
         val recommendCarts: List<Cart> = emptyList(),
