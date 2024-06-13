77702dc41eabe4b7da0906559c765b3b2897ecd3
gaeun <geun5744@gmail.com>
2024-06-13 11:48:27 +0900

    feat: error -> uiText로 변환 로직 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/utils/UiText.kt
@@ -0,0 +1,13 @@
+package woowacourse.shopping.ui.utils
+
+import woowacourse.shopping.R
+import woowacourse.shopping.domain.result.DataError
+import woowacourse.shopping.domain.result.ShowError
+
+fun ShowError.toUiText(): Int = when (this) {
+    DataError.Network.REQUEST_TIMEOUT -> R.string.time_out_error
+    DataError.Network.NO_INTERNET -> R.string.no_internet
+    DataError.Network.SERVER -> R.string.server_error
+    DataError.Network.INVALID_AUTHORIZATION -> R.string.unauthorized_error
+    DataError.UNKNOWN -> R.string.unknown_error
+}
