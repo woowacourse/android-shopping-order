2fd5cee5ab2891104e80916132202133df732edd
sh1mj1 <7wlgns@gmail.com>
2024-06-14 00:05:10 +0900

    refactor OrderViewModel, OrderError: 코루틴 스코프 설정
    - before: viewModelScope 가 IO 디스패처, 메인 디스패처에서 라이브 데이터 postValue
    
    - after: viewModelScope 가 메인 디스패처, 라이브데이터를 setValue

--- app/src/main/java/woowacourse/shopping/ui/order/OrderViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/order/OrderViewModel.kt
@@ -1,21 +1,17 @@
 package woowacourse.shopping.ui.order
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import woowacourse.shopping.ShoppingApp
 import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.repository.CategoryBasedProductRecommendationRepository
 import woowacourse.shopping.domain.repository.DefaultOrderRepository
-import woowacourse.shopping.domain.repository.DefaultShoppingCartRepository
 import woowacourse.shopping.domain.repository.OrderRepository
 import woowacourse.shopping.domain.repository.ProductsRecommendationRepository
-import woowacourse.shopping.domain.repository.ShoppingCartRepository
+import woowacourse.shopping.ui.order.event.OrderError
 import woowacourse.shopping.ui.order.event.OrderEvent
 import woowacourse.shopping.ui.order.listener.OrderListener
 import woowacourse.shopping.ui.util.MutableSingleLiveData
@@ -25,7 +21,6 @@ import woowacourse.shopping.ui.util.UniversalViewModelFactory
 class OrderViewModel(
     private val orderRepository: OrderRepository,
     private val productsRecommendationRepository: ProductsRecommendationRepository,
-    private val cartRepository: ShoppingCartRepository,
 ) : ViewModel(), OrderListener {
     private val _recommendedProducts: MutableSingleLiveData<List<Product>> = MutableSingleLiveData()
     val recommendedProducts: SingleLiveData<List<Product>> get() = _recommendedProducts
@@ -39,101 +34,78 @@ class OrderViewModel(
     private val _event: MutableSingleLiveData<OrderEvent> = MutableSingleLiveData()
     val event: SingleLiveData<OrderEvent> get() = _event
 
-    override fun order() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _event.postValue(OrderEvent.CompleteOrder)
+    private val _error: MutableSingleLiveData<OrderError> = MutableSingleLiveData()
+    val error: SingleLiveData<OrderError> get() = _error
+
+    fun loadAll() {
+        viewModelScope.launch {
+            loadRecommendProducts()
+            countOrderItemsQuantity()
+            calculateOrderItemsTotalPrice()
         }
     }
 
-    fun loadAll() {
-        viewModelScope.launch(Dispatchers.IO) {
-            productsRecommendationRepository.recommendedProducts()
-                .onSuccess { recommendedProducts ->
-                    withContext(Dispatchers.Main) {
-                        _recommendedProducts.postValue(recommendedProducts)
-                    }
-                }
-                .onFailure {
-                    // TODO : handle error
-                    Log.e(TAG, "loadAll: recommendedProducts2 $it")
-                    throw it
-                }
+    private suspend fun loadRecommendProducts() {
+        productsRecommendationRepository.recommendedProducts()
+            .onSuccess {
+                _recommendedProducts.setValue(it)
+            }
+            .onFailure {
+                _error.setValue(OrderError.LoadRecommendedProducts)
+            }
+    }
 
-            orderRepository.allOrderItemsQuantity()
-                .onSuccess { allQuantity ->
-                    withContext(Dispatchers.Main) {
-                        _addedProductQuantity.postValue(allQuantity)
-                    }
-                }
-                .onFailure {
-                    // TODO : handle error
-                    Log.e(TAG, "loadAll: allOrderItemsQuantity2 $it")
-                    throw it
-                }
+    private suspend fun countOrderItemsQuantity() {
+        orderRepository.allOrderItemsQuantity()
+            .onSuccess {
+                _addedProductQuantity.value = it
+            }
+            .onFailure {
+                _error.setValue(OrderError.CalculateOrderItemsQuantity)
+            }
+    }
 
-            orderRepository.orderItemsTotalPrice()
-                .onSuccess {
-                    withContext(Dispatchers.Main) {
-                        _totalPrice.postValue(it)
-                    }
-                }
-                .onFailure {
-                    // TODO : handle error
-                    Log.e(TAG, "loadAll: orderItemsTotalPrice2 $it")
-                    throw it
-                }
-        }
+    private suspend fun calculateOrderItemsTotalPrice() {
+        orderRepository.orderItemsTotalPrice()
+            .onSuccess {
+                _totalPrice.value = it
+            }
+            .onFailure {
+                _error.setValue(OrderError.CalculateOrderItemsTotalPrice)
+            }
+    }
+
+    override fun order() {
+        _event.setValue(OrderEvent.CompleteOrder)
     }
 
     override fun onIncrease(
         productId: Long,
         quantity: Int,
     ) {
-        viewModelScope.launch(Dispatchers.IO) {
-            cartRepository.addShoppingCartProduct(productId, INCREASE_AMOUNT)
-                .onSuccess {
-                    // TODO : handle success
-                }
-                .onFailure {
-                    // TODO : handle error
-                    Log.e(TAG, "onIncrease: addShoppingCartProduct2: $it")
-                    throw it
-                }
-
-            orderRepository.updateOrderItem(productId, quantity)
-                .onSuccess {
-                    withContext(Dispatchers.Main) {
-                        updateRecommendProductsQuantity(productId, INCREASE_AMOUNT)
-                        updateTotalQuantity(productId, INCREASE_AMOUNT)
-                        _addedProductQuantity.postValue(_addedProductQuantity.value?.plus(1) ?: 1)
-                    }
-                }
-                .onFailure {
-                    // TODO : handle error
-                    Log.e(TAG, "onIncrease: saveOrderItem2: $it")
-                    throw it
-                }
+        viewModelScope.launch {
+            updateItemQuantity(productId, quantity, INCREASE_AMOUNT)
         }
+    }
+
+    private suspend fun updateItemQuantity(productId: Long, quantity: Int, changeAmount: Int) {
+        orderRepository.updateOrderItem(productId, quantity)
+            .onSuccess {
+                updateRecommendProductsQuantity(productId, changeAmount)
+                updateTotalQuantity(productId, changeAmount)
+                _addedProductQuantity.value = addedProductQuantity.value?.plus(changeAmount)
+            }
+            .onFailure {
+                _error.setValue(OrderError.UpdateOrderItem)
+            }
     }
 
     override fun onDecrease(
         productId: Long,
         quantity: Int,
     ) {
-        viewModelScope.launch(Dispatchers.IO) {
-            orderRepository.updateOrderItem(productId, quantity)
-                .onSuccess {
-                    withContext(Dispatchers.Main) {
-                        updateRecommendProductsQuantity(productId, DECREASE_AMOUNT)
-                        updateTotalQuantity(productId, DECREASE_AMOUNT)
-                        _addedProductQuantity.postValue(_addedProductQuantity.value?.minus(1) ?: 0)
-                    }
-                }
-                .onFailure {
-                    // TODO: handle error
-                    Log.e(TAG, "onDecrease: saveOrderItem2: $it")
-                    throw it
-                }
+        viewModelScope.launch {
+            updateItemQuantity(productId, quantity, DECREASE_AMOUNT)
         }
     }
 
@@ -141,23 +113,24 @@ class OrderViewModel(
         productId: Long,
         changeAmount: Int,
     ) {
-        _totalPrice.postValue(_totalPrice.value?.plus(productQuantity(productId) * changeAmount) ?: 0)
+        _totalPrice.value = totalPrice.value?.plus(productQuantity(productId) * changeAmount)
     }
 
-    private fun productQuantity(productId: Long) = _recommendedProducts.getValue()?.find { it.id == productId }?.price ?: 0
+    private fun productQuantity(productId: Long) =
+        _recommendedProducts.getValue()?.find { it.id == productId }?.price ?: 0
 
     private fun updateRecommendProductsQuantity(
         productId: Long,
         changeAmount: Int,
     ) {
-        _recommendedProducts.postValue(
-            _recommendedProducts.getValue()?.map { product ->
+        _recommendedProducts.setValue(
+            recommendedProducts.getValue().orEmpty().map { product ->
                 if (product.id == productId) {
                     product.copy(quantity = product.quantity + changeAmount)
                 } else {
                     product
                 }
-            } ?: emptyList(),
+            }
         )
     }
 
@@ -179,16 +152,11 @@ class OrderViewModel(
                     ShoppingApp.cartSource,
                     ShoppingApp.historySource,
                 ),
-            cartRepository: ShoppingCartRepository =
-                DefaultShoppingCartRepository(
-                    ShoppingApp.cartSource,
-                ),
         ): UniversalViewModelFactory {
             return UniversalViewModelFactory {
                 OrderViewModel(
                     orderRepository,
                     productRecommendationRepository,
-                    cartRepository,
                 )
             }
         }
