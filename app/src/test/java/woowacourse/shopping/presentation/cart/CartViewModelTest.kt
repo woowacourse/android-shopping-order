fe2fbc0bf54c83a0409bdf3087f2c4b2124afeb2
songpink <jejeje04@naver.com>
2024-06-14 14:17:53 +0900

    test: 테스트코드 수정

--- app/src/test/java/woowacourse/shopping/presentation/cart/CartViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/presentation/cart/CartViewModelTest.kt
@@ -1,13 +1,16 @@
 package woowacourse.shopping.presentation.cart
 
+import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.junit5.MockKExtension
+import io.mockk.every
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import woowacourse.shopping.domain.cartProduct
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.presentation.util.CoroutinesTestExtension
 import woowacourse.shopping.presentation.util.InstantTaskExecutorExtension
@@ -15,67 +18,38 @@ import woowacourse.shopping.presentation.util.getOrAwaitValue
 
 @ExperimentalCoroutinesApi
 @ExtendWith(CoroutinesTestExtension::class)
-@ExtendWith(InstantTaskExecutorExtension::class, MockKExtension::class)
+@ExtendWith(InstantTaskExecutorExtension::class)
 class CartViewModelTest {
-    @RelaxedMockK
-    lateinit var cartRepository: CartRepository
-
+    private lateinit var cartRepository: CartRepository
     private lateinit var cartViewModel: CartViewModel
-    private val uiState get() = cartViewModel.uiState.getOrAwaitValue()
 
     @BeforeEach
     fun setUp() {
+        cartRepository = mockk()
         cartViewModel = CartViewModel(cartRepository)
     }
 
     @Test
     fun `장바구니의 최초 페이지는 1이어야 한다`() {
         coVerify { cartRepository.totalCartProducts() }
-        assertThat(uiState.currentPage).isEqualTo(1)
+        assertThat(cartViewModel.uiState.getOrAwaitValue().currentPage).isEqualTo(1)
     }
 
-//    @Test
-//    @DisplayName("현재 페이지가 1일 때, 다음 페이지로 이동하면, 페이지가 2가 된다,")
-//    fun test1() {
-//        val nextPage = 2
-//        val serverNextPage = 1
-//        // given
-//        every { cartRepository.cartProducts(serverNextPage, PAGE_SIZE) } returns
-//            Result.success(
-//                listOf(
-//                    cartProduct(),
-//                ),
-//            )
-//        // when
-//        cartViewModel.moveToNextPage()
-//        // then
-//        verify(exactly = 1) { cartRepository.cartProducts(serverNextPage, PAGE_SIZE) }
-//        uiState.currentPage shouldBe nextPage
-//    }
-//
-//    @Test
-//    @DisplayName("현재 페이지가 1일 때, 다음 페이지로 이동하면, 페이지가 2가 된다")
-//    fun test2() {
-//        val nextPage = 2
-//        val serverNextPage = 1
-//        // given
-//        every { cartRepository.canLoadMoreCartProducts(0, PAGE_SIZE) } returns Result.success(true)
-//        every { cartRepository.canLoadMoreCartProducts(2, PAGE_SIZE) } returns Result.success(true)
-//        every { cartRepository.cartProducts(serverNextPage, PAGE_SIZE) } returns
-//            Result.success(
-//                listOf(
-//                    cartProduct(),
-//                ),
-//            )
-//        // when
-//        cartViewModel.moveToNextPage()
-//        // then
-//        verify(exactly = 1) { cartRepository.canLoadMoreCartProducts(0, PAGE_SIZE) }
-//        verify(exactly = 1) { cartRepository.canLoadMoreCartProducts(2, PAGE_SIZE) }
-//        uiState.currentPage shouldBe nextPage
-//    }
-//
-//    companion object {
-//        const val PAGE_SIZE = 5
-//    }
+    @Test
+    fun `현재 페이지가 1일 때, 다음 페이지로 이동하면, 페이지가 2가 된다`() =
+        runTest {
+            // given
+            every { cartRepository.canLoadMoreCartProducts(0, 5) } returns Result.success(true)
+            every { cartRepository.canLoadMoreCartProducts(2, 5) } returns Result.success(true)
+            coEvery { cartRepository.cartProducts(1, 5) } returns
+                Result.success(
+                    listOf(
+                        cartProduct(),
+                    ),
+                )
+            // when
+            cartViewModel.moveToNextPage()
+            // then
+            assertThat(cartViewModel.uiState.getOrAwaitValue().currentPage).isEqualTo(2)
+        }
 }
