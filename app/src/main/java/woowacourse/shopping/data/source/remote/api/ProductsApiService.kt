20ee4892d62100a3c5ddb06ba6ed58457a12dab1
cucumber99 <dlsguq1@gmail.com>
2025-05-27 22:08:03 +0900

    refactor: 상품 목록 API 인터페이스 수정

--- app/src/main/java/woowacourse/shopping/data/source/remote/api/ProductsApiService.kt
+++ app/src/main/java/woowacourse/shopping/data/source/remote/api/ProductsApiService.kt
@@ -1,36 +1,24 @@
 package woowacourse.shopping.data.source.remote.api
 
 import retrofit2.Call
-import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 import woowacourse.shopping.data.model.ProductResponse
+import woowacourse.shopping.data.model.ProductsResponse
 
 interface ProductsApiService {
     @GET("/products")
     fun getProducts(
         @Header("accept") accept: String = "*/*",
+        @Query("size") size: Int = 20,
         @Query("page") page: Int = 0,
-        @Query("size") size: Int = 1
-    ): Call<ProductResponse>
-
-    @POST("/products")
-    fun postProducts(
-        @Header("accept") accept: String = "*/*",
-    ): String
+    ): Call<ProductsResponse>
 
     @GET("/products/{id}")
-    fun getProductsId(
+    fun getProductById(
         @Header("accept") accept: String = "*/*",
         @Path("id") id: Int,
     ): Call<ProductResponse>
-
-    @DELETE("/products/{id}")
-    fun deleteProductsId(
-        @Header("accept") accept: String = "*/*",
-        @Path("id") id: Int,
-    ):String
 }
