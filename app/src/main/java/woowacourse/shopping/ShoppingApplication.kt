f828b60db7351754cc4bacd87eccd53d5715b8a8
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-05-27 20:00:12 +0900

    feat: 사용자 인증 정보 저장

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping
 
 import android.app.Application
+import android.content.SharedPreferences
 import woowacourse.shopping.data.database.ShoppingDatabase
 import woowacourse.shopping.data.server.DevMockServer
 import kotlin.concurrent.thread
@@ -9,10 +10,23 @@ class ShoppingApplication : Application() {
     lateinit var database: ShoppingDatabase
         private set
 
+    val sharedPreferences: SharedPreferences by lazy {
+        getSharedPreferences(USER_INFO, MODE_PRIVATE)
+    }
+
     override fun onCreate() {
         super.onCreate()
 
         thread { DevMockServer.start() }
         database = ShoppingDatabase.getInstance(applicationContext)
+
+        sharedPreferences.edit().putString(ID, "wondroid-world")
+        sharedPreferences.edit().putString(PASSWORD, "password")
+    }
+
+    companion object {
+        private const val USER_INFO = "user_info"
+        private const val ID = "id"
+        private const val PASSWORD = "password"
     }
 }
