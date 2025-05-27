33eaf4dd2576314789e77e8e99e2868a02989143
moondev03 <moondev03@gmail.com>
2025-05-27 15:35:17 +0900

    feat: ShimmerFrameLayout BindingAdapter 추가

---
+++ app/src/main/java/woowacourse/shopping/presentation/binding/ShimmerBindingAdapter.kt
@@ -0,0 +1,17 @@
+package woowacourse.shopping.presentation.binding
+
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import com.facebook.shimmer.ShimmerFrameLayout
+
+@BindingAdapter("isShow")
+fun setShimmerBindingAdapter(view: ShimmerFrameLayout, isShow: Boolean) {
+    view.isVisible = isShow
+
+    if (!isShow) {
+        view.stopShimmer()
+        return
+    }
+
+    view.startShimmer()
+}
