f787f01ffb29ae88f87bb629f5e79a34c6d6c53e
rosemin928 <rosemin928@naver.com>
2025-05-27 14:17:12 +0900

    feat: 상품 목록 불러오는 서비스 인터페이스 작성

---
+++ app/src/main/java/woowacourse/shopping/data/remote/GoodsService.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping.data.remote
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface GoodsService {
+    @GET("/products")
+    fun requestGoods(
+        @Header("accept") accept: String = "*/*",
+        @Query("page") page: Int = 0,
+        @Query("size") size: Int = 1
+    ): Call<ProductResponse>
+}
