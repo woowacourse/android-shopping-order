2bb208521b16427edc6635f5a803fd5c2f65e009
cucumber99 <dlsguq1@gmail.com>
2025-05-27 22:14:09 +0900

    feat: 상품 목록 데이터 소스 구현

---
+++ app/src/main/java/woowacourse/shopping/data/source/remote/products/ProductsRemoteDataSource.kt
@@ -0,0 +1,21 @@
+package woowacourse.shopping.data.source.remote.products
+
+import woowacourse.shopping.data.model.ProductResponse
+import woowacourse.shopping.data.model.ProductsResponse
+import woowacourse.shopping.data.source.remote.api.ProductsApiService
+import woowacourse.shopping.data.source.remote.util.enqueueResult
+
+class ProductsRemoteDataSource(
+    private val api: ProductsApiService
+) : ProductsDataSource {
+    override fun getProducts(onResult: (Result<ProductsResponse>) -> Unit) {
+        api.getProducts().enqueueResult(onResult)
+    }
+
+    override fun getProductById(
+        id: Int,
+        onResult: (Result<ProductResponse>) -> Unit,
+    ) {
+        api.getProductById(id = id).enqueueResult(onResult)
+    }
+}
