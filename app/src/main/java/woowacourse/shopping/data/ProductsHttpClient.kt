77791b40e50214786daeb77450867a1414f97734
jerry8282 <ka06196@naver.com>
2025-05-27 22:07:23 +0900

    refactor: implement postShoppingCartItem

--- app/src/main/java/woowacourse/shopping/data/ProductsHttpClient.kt
+++ app/src/main/java/woowacourse/shopping/data/ProductsHttpClient.kt
@@ -1,11 +1,17 @@
 package woowacourse.shopping.data
 
+import android.util.Log
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
+import woowacourse.shopping.data.product.dto.CartRequest
 import woowacourse.shopping.data.product.dto.ProductResponse
+import java.util.Base64
 
 class ProductsHttpClient(
     private val baseUrl: String = "http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com",
@@ -28,19 +34,28 @@ class ProductsHttpClient(
     fun getShoppingCart(
         page: Int,
         size: Int,
-    ): Response = httpGet("/cart-items?page=$page&size=$size&sort=string")
+    ): Response = httpGet("$PATH_CART_ITEMS?page=$page&size=$size&sort=string")
 
     fun postShoppingCartItem(
         id: Long,
         quantity: Int,
-    ): Response = run("/cart-items")
+    ): Response {
+        val cartRequest = CartRequest(id, quantity)
+        val jsonString: String = Json.encodeToString(cartRequest)
+
+        return httpPost(
+            PATH_CART_ITEMS,
+            jsonString.toRequestBody(MEDIA_TYPE_JSON.toMediaTypeOrNull()),
+            true,
+        )
+    }
 
     fun getProducts(
         page: Int,
         size: Int,
     ): Response = httpGet("/products?page=$page&size=$size")
 
-    fun getCartItems(): Response = run("/cart-items")
+    fun getCartItems(): Response = run(PATH_CART_ITEMS)
 
     private fun run(path: String): Response {
         val request: Request =
@@ -52,14 +67,59 @@ class ProductsHttpClient(
         return client.newCall(request).execute()
     }
 
-    private fun httpGet(path: String): Response {
+    private fun httpGet(
+        path: String,
+        needAuthorization: Boolean = false,
+    ): Response {
+        val valueToEncode = "jerry8282:password"
         val request: Request =
             Request
                 .Builder()
                 .url(baseUrl + path)
-                .get()
+                .apply {
+                    if (needAuthorization) {
+                        authorizationHeader(valueToEncode)
+                    }
+                }.get()
                 .build()
 
         return client.newCall(request).execute()
+    }
+
+    private fun Request.Builder.authorizationHeader(valueToEncode: String) {
+        header(
+            "Authorization",
+            Base64.getEncoder().encodeToString(valueToEncode.toByteArray()),
+        )
+    }
+
+    private fun httpPost(
+        path: String,
+        body: RequestBody,
+        needAuthorization: Boolean = false,
+    ): Response {
+        val valueToEncode = "jerry8282:password".toByteArray()
+        Log.d("asdf", "valueToEncode  $valueToEncode")
+        val request: Request =
+            Request
+                .Builder()
+                .url(baseUrl + path)
+                .apply {
+                    if (needAuthorization) {
+                        header(
+                            "Authorization",
+                            "Basic " + Base64.getEncoder().encodeToString(valueToEncode),
+                        )
+                    }
+                }.post(body)
+                .build()
+
+        Log.d("asdf", "request  ${request.headers}")
+        return client.newCall(request).execute()
+    }
+
+    companion object {
+        private const val PATH_CART_ITEMS = "/cart-items"
+        private const val MEDIA_TYPE_JSON = "application/json"
     }
 }
---
+++ app/src/main/java/woowacourse/shopping/data/product/dto/CartRequest.kt
@@ -0,0 +1,13 @@
+package woowacourse.shopping.data.product.dto
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CartRequest(
+    @SerialName("productId")
+    val productId: Long?,
+    @SerialName("quantity")
+    val quantity: Int?
+)
