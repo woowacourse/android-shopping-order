86570099cff04b0f472ca861c51f6bab1ccd2450
cucumber99 <dlsguq1@gmail.com>
2025-05-27 22:12:07 +0900

    refactor: PagingData 패키지 이동

---
+++ app/src/main/java/woowacourse/shopping/domain/model/PagingData.kt
@@ -0,0 +1,8 @@
+package woowacourse.shopping.domain.model
+
+import woowacourse.shopping.presentation.product.catalog.ProductUiModel
+
+data class PagingData(
+    val products: List<ProductUiModel>,
+    val hasNext: Boolean,
+)
