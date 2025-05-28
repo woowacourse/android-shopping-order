c17278f651c41653d3477370bc7accc77f4c90b4
cucumber99 <dlsguq1@gmail.com>
2025-05-28 17:45:57 +0900

    refactor: pageSize 파라미터명 size로 변경

--- app/src/main/java/woowacourse/shopping/domain/repository/ProductsRepository.kt
+++ app/src/main/java/woowacourse/shopping/domain/repository/ProductsRepository.kt
@@ -6,7 +6,7 @@ import woowacourse.shopping.presentation.product.catalog.ProductUiModel
 interface ProductsRepository {
     fun getProducts(
         page: Int,
-        pageSize: Int = 20,
+        size: Int = 20,
         onResult: (Result<PagingData>) -> Unit,
     )
 
