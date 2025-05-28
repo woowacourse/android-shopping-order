4236e31de0fd7a487c4ed19d7a43c11075d60679
hwannow <vnfdlvgid061@naver.com>
2025-05-28 22:57:15 +0900

    feat: 장바구니 추천 상품 화면 레이아웃 구현

--- app/src/main/java/woowacourse/shopping/view/cart/recommendation/CartProductRecommendationFragment.kt
+++ app/src/main/java/woowacourse/shopping/view/cart/recommendation/CartProductRecommendationFragment.kt
@@ -5,17 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import woowacourse.shopping.R
+import woowacourse.shopping.databinding.FragmentCartProductRecommendationBinding
 import woowacourse.shopping.view.cart.ShoppingCartViewModel
 
 class CartProductRecommendationFragment(
     private val viewModel: ShoppingCartViewModel,
 ) : Fragment() {
+    private var _binding: FragmentCartProductRecommendationBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? = inflater.inflate(R.layout.fragment_cart_product_recommendation, container, false)
+    ): View {
+        _binding = FragmentCartProductRecommendationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(
         view: View,
@@ -23,5 +29,10 @@ class CartProductRecommendationFragment(
     ) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.loadRecommendedProducts()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
