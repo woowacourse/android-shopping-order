2798f71e475095610898d7ad2cc6d2099c090c0e
yrsel <xxknu88@gmail.com>
2025-05-28 16:30:39 +0900

    feat: 캐싱 장바구니 조회, 추가기능

--- app/src/main/java/woowacourse/shopping/domain/model/Cart.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Cart.kt
@@ -1,8 +1,20 @@
 package woowacourse.shopping.domain.model
 
 data class Cart(
-    private val cartProducts: List<CartItem> = emptyList()
+    private val cartProducts: List<CartItem> = emptyList(),
 ) {
     private val cartMapByProductId: Map<Long, CartItem> =
         cartProducts.associateBy { it.product.productId }
+
+    private constructor(cartMapByProductId: Map<Long, CartItem>) : this(cartMapByProductId.values.toList())
+
+    fun add(cartItem: CartItem): Cart {
+        val productId = cartItem.product.productId
+        val newCartItems = cartMapByProductId.plus(productId to cartItem)
+        return Cart(newCartItems)
+    }
+
+    fun exist(productId: Long): Boolean = cartMapByProductId.containsKey(productId)
+
+    fun findCartItem(productId: Long): CartItem? = cartMapByProductId[productId]
 }
