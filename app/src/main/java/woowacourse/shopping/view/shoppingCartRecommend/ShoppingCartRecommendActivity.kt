544913bf0987da77a3a509f784476b0def1ffc1e
m6z1 <sonmyungj1zz@gmail.com>
2025-05-28 22:56:47 +0900

    feat: 장바구니 추천 페이지 ui 구현

---
+++ app/src/main/java/woowacourse/shopping/view/shoppingCartRecommend/ShoppingCartRecommendActivity.kt
@@ -0,0 +1,21 @@
+package woowacourse.shopping.view.shoppingCartRecommend
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import woowacourse.shopping.R
+
+class ShoppingCartRecommendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_shopping_cart_recommend)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
