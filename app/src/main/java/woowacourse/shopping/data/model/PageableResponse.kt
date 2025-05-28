aff7dd0c30ea56602973a44fc85e55bf59b5e9d3
윤동현 <donghyunyoon81@gmail.com>
2025-05-28 15:22:08 +0900

    refactor: data layer 모델 추가

--- app/src/main/java/woowacourse/shopping/data/model/PageableResponse.kt
+++
@@ -1,9 +0,0 @@
-package woowacourse.shopping.data.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PageableResponse<T>(
-    val items: List<T>,
-    val hasMore: Boolean,
-)
