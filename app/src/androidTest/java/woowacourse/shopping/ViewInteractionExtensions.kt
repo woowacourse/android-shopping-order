a87306d9611f15607920cf743376a023affc09fe
Ham BeomJoon <hbj0802@naver.com>
2025-05-29 17:50:02 +0900

    chore: 사용하지 않는 파일 제거

--- app/src/androidTest/java/woowacourse/shopping/ViewInteractionExtensions.kt
+++ app/src/androidTest/java/woowacourse/shopping/ViewInteractionExtensions.kt
@@ -2,7 +2,6 @@ package woowacourse.shopping
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.ViewInteraction
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
@@ -11,8 +10,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.hamcrest.Matchers.allOf
 
 fun ViewInteraction.checkIsDisplayed(): ViewInteraction = this.check(matches(isDisplayed()))
-
-fun ViewInteraction.performClick(): ViewInteraction = this.perform(click())
 
 fun ViewInteraction.scrollToPosition(position: Int): ViewInteraction =
     this.perform(
