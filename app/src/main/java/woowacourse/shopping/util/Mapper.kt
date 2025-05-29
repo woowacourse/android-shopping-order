a82c603e8ac24539dab13b4643d7652841bc0325
devfeijoa <devfeijoa@gmail.com>
2025-05-29 14:32:55 +0900

    refactor: 사용하지 않는 함수 제거

--- app/src/main/java/woowacourse/shopping/util/Mapper.kt
+++ app/src/main/java/woowacourse/shopping/util/Mapper.kt
@@ -4,30 +4,6 @@ import woowacourse.shopping.data.remote.cart.CartResponse.Content.CartRemoteProd
 import woowacourse.shopping.data.remote.product.ProductResponse.Content
 import woowacourse.shopping.domain.model.Cart
 import woowacourse.shopping.domain.model.Product
-import woowacourse.shopping.feature.model.CartUiModel
-
-fun Cart.toUi(): CartUiModel =
-    CartUiModel(
-        id = product.id,
-        name = product.name,
-        price = product.price,
-        imageUrl = product.imageUrl,
-        quantity = quantity,
-    )
-
-fun CartUiModel.toDomain(): Cart =
-    Cart(
-        id = id.toLong(),
-        quantity = quantity,
-        product =
-            Product(
-                id = id,
-                name = name,
-                price = price,
-                imageUrl = imageUrl,
-                category = "",
-            ),
-    )
 
 fun List<Any>.replaceCartByProductId(newCart: Cart): List<Any> =
     map {
