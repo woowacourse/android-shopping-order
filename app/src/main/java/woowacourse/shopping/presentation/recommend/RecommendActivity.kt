9524ac3eac15ad3ed8a4793c5e018c35a37ab89d
cucumber99 <dlsguq1@gmail.com>
2025-05-29 17:52:17 +0900

    feat: 추천 액티비티 UI 및 로직 구현

--- app/src/main/java/woowacourse/shopping/presentation/recommend/RecommendActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/recommend/RecommendActivity.kt
@@ -1,21 +1,68 @@
 package woowacourse.shopping.presentation.recommend
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityRecommendBinding
 
 class RecommendActivity : AppCompatActivity() {
+    private val binding: ActivityRecommendBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_recommend)
+    }
+
+    private val viewModel: RecommendViewModel by viewModels {
+        RecommendViewModel.FACTORY
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpScreen()
+        setUpBinding()
+        setOrderInfo()
+    }
+
+    private fun setUpScreen() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_recommend)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun setUpBinding() {
+        binding.apply {
+            vm = viewModel
+            lifecycleOwner = this@RecommendActivity
+        }
+    }
+
+    private fun setOrderInfo() {
+        val price = intent.getIntExtra(TOTAL_PRICE_KEY, 0)
+        val count = intent.getIntExtra(TOTAL_COUNT_KEY, 0)
+        viewModel.setOrderInfo(price, count)
+    }
+
+    companion object {
+        private const val TOTAL_PRICE_KEY = "Price"
+        private const val TOTAL_COUNT_KEY = "Count"
+
+        fun newIntent(
+            context: Context,
+            price: Int,
+            count: Int,
+        ): Intent {
+            return Intent(context, RecommendActivity::class.java).apply {
+                putExtra(TOTAL_PRICE_KEY, price)
+                putExtra(TOTAL_COUNT_KEY, count)
+            }
         }
     }
 }
