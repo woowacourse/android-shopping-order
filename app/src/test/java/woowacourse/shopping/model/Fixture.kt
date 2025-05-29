c2c97c6c648038317135f2f3fa560a135c00f3c1
박지민 <pjm5489@naver.com>
2025-05-29 17:38:32 +0900

    test: 더미데이터 수정

--- app/src/test/java/woowacourse/shopping/model/Fixture.kt
+++ app/src/test/java/woowacourse/shopping/model/Fixture.kt
@@ -1,8 +1,7 @@
 package woowacourse.shopping.model
 
-import woowacourse.shopping.domain.model.CartProduct
-import woowacourse.shopping.domain.model.CartProducts
 import woowacourse.shopping.domain.model.HistoryProduct
+import woowacourse.shopping.domain.model.Page
 import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.model.ProductDetail
 import woowacourse.shopping.domain.model.Products
@@ -13,6 +12,7 @@ val DUMMY_PRODUCT_DETAIL_1 =
         name = "[병천아우내] 모듬순대",
         imageUrl = "https://product-image.kurly.com/hdims/resize/%5E%3E360x%3E468/cropcenter/360x468/quality/85/src/product/image/00fb05f8-cb19-4d21-84b1-5cf6b9988749.jpg",
         price = 11900,
+        category = "공백제이",
     )
 
 val DUMMY_PRODUCT_DETAIL_2 =
@@ -21,6 +21,7 @@ val DUMMY_PRODUCT_DETAIL_2 =
         name = "[빙그래] 요맘때 파인트 710mL 3종 (택1)",
         imageUrl = "https://product-image.kurly.com/hdims/resize/%5E%3E360x%3E468/cropcenter/360x468/quality/85/src/product/image/73061aab-a2e2-443a-b0f9-f19b7110045e.jpg",
         price = 5000,
+        category = "공백제이",
     )
 
 val DUMMY_PRODUCT_DETAIL_3 =
@@ -29,6 +30,7 @@ val DUMMY_PRODUCT_DETAIL_3 =
         name = "[애슐리] 크런치즈엣지 포테이토피자 495g",
         imageUrl = "https://product-image.kurly.com/hdims/resize/%5E%3E360x%3E468/cropcenter/360x468/quality/85/src/product/image/23efcafe-0765-478f-afe9-f9af7bb9b7df.jpg",
         price = 10900,
+        category = "공백제이",
     )
 
 val DUMMY_PRODUCT_DETAIL_4 =
@@ -37,6 +39,7 @@ val DUMMY_PRODUCT_DETAIL_4 =
         name = "치밥하기 좋은 순살 바베큐치킨",
         imageUrl = "https://product-image.kurly.com/hdims/resize/%5E%3E360x%3E468/cropcenter/360x468/quality/85/src/product/image/f864b361-85da-4482-aec8-909397caac4e.jpg",
         price = 13990,
+        category = "공백제이",
     )
 
 val DUMMY_PRODUCT_DETAIL_5 =
@@ -45,19 +48,21 @@ val DUMMY_PRODUCT_DETAIL_5 =
         name = "[이연복의 목란] 짜장면 2인분",
         imageUrl = "https://product-image.kurly.com/hdims/resize/%5E%3E360x%3E468/cropcenter/360x468/quality/85/src/product/image/90256eb2-b02f-493a-ab7a-29a8724254e4.jpeg",
         price = 9980,
+        category = "공백제이",
     )
 
-val DUMMY_CART_PRODUCT_1 = CartProduct(DUMMY_PRODUCT_DETAIL_1, 1)
-val DUMMY_CART_PRODUCT_2 = CartProduct(DUMMY_PRODUCT_DETAIL_2, 1)
-val DUMMY_CART_PRODUCT_3 = CartProduct(DUMMY_PRODUCT_DETAIL_3, 1)
-val DUMMY_CART_PRODUCT_4 = CartProduct(DUMMY_PRODUCT_DETAIL_4, 1)
-val DUMMY_CART_PRODUCT_5 = CartProduct(DUMMY_PRODUCT_DETAIL_5, 1)
+val DUMMY_CART_PRODUCT_1 = Product(DUMMY_PRODUCT_DETAIL_1, 1)
+val DUMMY_CART_PRODUCT_2 = Product(DUMMY_PRODUCT_DETAIL_2, 1)
+val DUMMY_CART_PRODUCT_3 = Product(DUMMY_PRODUCT_DETAIL_3, 1)
+val DUMMY_CART_PRODUCT_4 = Product(DUMMY_PRODUCT_DETAIL_4, 1)
+val DUMMY_CART_PRODUCT_5 = Product(DUMMY_PRODUCT_DETAIL_5, 1)
 
 val DUMMY_HISTORY_PRODUCT_1 =
     HistoryProduct(
         productId = DUMMY_PRODUCT_DETAIL_1.id,
         name = DUMMY_PRODUCT_DETAIL_1.name,
         imageUrl = DUMMY_PRODUCT_DETAIL_1.imageUrl,
+        category = "공백제이",
     )
 
 val DUMMY_CATALOG_PRODUCT_1 = Product(DUMMY_PRODUCT_DETAIL_1, quantity = 5)
@@ -65,7 +70,7 @@ val DUMMY_CATALOG_PRODUCT_2 = Product(DUMMY_PRODUCT_DETAIL_2, quantity = 6)
 val DUMMY_CATALOG_PRODUCT_3 = Product(DUMMY_PRODUCT_DETAIL_3, quantity = 7)
 
 val DUMMY_CART_PRODUCTS_1 =
-    CartProducts(
+    Products(
         products =
             listOf(
                 DUMMY_CART_PRODUCT_1,
@@ -74,7 +79,7 @@ val DUMMY_CART_PRODUCTS_1 =
                 DUMMY_CART_PRODUCT_4,
                 DUMMY_CART_PRODUCT_5,
             ),
-        totalPage = 2,
+        page = Page(2, isFirst = false, isLast = false),
     )
 
 val DUMMY_CATALOG_PRODUCTS_1 =
@@ -85,10 +90,8 @@ val DUMMY_CATALOG_PRODUCTS_1 =
                 DUMMY_CATALOG_PRODUCT_2,
                 DUMMY_CATALOG_PRODUCT_3,
             ),
-        hasMore = true,
     )
 val DUMMY_CATALOG_PRODUCTS_2 =
     Products(
         products = listOf(DUMMY_CATALOG_PRODUCT_1),
-        hasMore = true,
     )
