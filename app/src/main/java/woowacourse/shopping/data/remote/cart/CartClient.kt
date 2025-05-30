fc8512a00925cce4806aa8f81f13dec826c46b8c
rosemin928 <rosemin928@naver.com>
2025-05-30 16:25:28 +0900

    refactor: BuildConfig에서 BASE_URL, USER_ID, USER_PASSWORD 가져오도록 변경

--- app/src/main/java/woowacourse/shopping/data/remote/cart/CartClient.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/cart/CartClient.kt
@@ -5,17 +5,21 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import woowacourse.shopping.BuildConfig
 
 object CartClient {
     fun getRetrofitService(): CartService {
-        val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor(HeaderInterceptor("rosemin928", "password"))
-            .build()
+        val okHttpClient =
+            OkHttpClient
+                .Builder()
+                .addInterceptor(HeaderInterceptor(BuildConfig.USER_ID, BuildConfig.USER_PASSWORD))
+                .build()
 
         val contentType = "application/json".toMediaType()
 
-        return Retrofit.Builder()
-            .baseUrl("http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com")
+        return Retrofit
+            .Builder()
+            .baseUrl(BuildConfig.BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(Json.asConverterFactory(contentType))
             .build()
--- app/src/main/java/woowacourse/shopping/data/remote/product/ProductClient.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/product/ProductClient.kt
@@ -4,17 +4,19 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import woowacourse.shopping.BuildConfig
 
 object ProductClient {
     fun getRetrofitService(): ProductService {
         val contentType = "application/json".toMediaType()
 
-        val retrofitService = Retrofit.Builder()
-            .baseUrl("http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com")
-            .addConverterFactory(Json.asConverterFactory(contentType))
-            .build()
-            .create(ProductService::class.java)
+        val retrofitService =
+            Retrofit
+                .Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(Json.asConverterFactory(contentType))
+                .build()
+                .create(ProductService::class.java)
 
         return retrofitService
     }
