96f78975e9c01aefa3e71c9ad5dcf3cde3cd8a75
DoableTuple <doabletuple@gmail.com>
2025-05-30 16:12:08 +0900

    chore: 파일 이름 변경 및 코멘트 제거

--- app/src/test/java/woowacourse/shopping/domain/util/Extensions.kt
+++ app/src/test/java/woowacourse/shopping/domain/util/Extensions.kt
@@ -6,8 +6,6 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
-/* Copyright 2019 Google LLC.
-   SPDX-License-Identifier: Apache-2.0 */
 fun <T> LiveData<T>.getOrAwaitValue(
     time: Long = 2,
     timeUnit: TimeUnit = TimeUnit.SECONDS,
@@ -25,7 +23,6 @@ fun <T> LiveData<T>.getOrAwaitValue(
 
     this.observeForever(observer)
 
-    // Don't wait indefinitely if the LiveData is not set.
     if (!latch.await(time, timeUnit)) {
         throw TimeoutException("LiveData value was never set.")
     }
