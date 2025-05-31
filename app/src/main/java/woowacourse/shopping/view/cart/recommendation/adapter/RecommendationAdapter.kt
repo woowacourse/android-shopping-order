0c4797930803888f4676c59cb387ffbc7c89c4c6
hwannow <vnfdlvgid061@naver.com>
2025-05-31 17:43:55 +0900

    refactor: `RecommendationAdapter`를 `ListAdapter`로 변경

--- app/src/main/java/woowacourse/shopping/view/cart/recommendation/adapter/RecommendationAdapter.kt
+++ app/src/main/java/woowacourse/shopping/view/cart/recommendation/adapter/RecommendationAdapter.kt
@@ -1,47 +1,43 @@
 package woowacourse.shopping.view.cart.recommendation.adapter
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import woowacourse.shopping.view.util.product.ProductViewHolder
 
 class RecommendationAdapter(
-    items: List<ProductItem> = emptyList(),
     private val eventHandler: ProductViewHolder.EventHandler,
-) : RecyclerView.Adapter<ProductViewHolder>() {
-    private val items = items.toMutableList()
+) : ListAdapter<ProductItem, ProductViewHolder>(
+        object : DiffUtil.ItemCallback<ProductItem>() {
+            override fun areItemsTheSame(
+                oldItem: ProductItem,
+                newItem: ProductItem,
+            ): Boolean {
+                return oldItem.product.id == newItem.product.id
+            }
 
+            override fun areContentsTheSame(
+                oldItem: ProductItem,
+                newItem: ProductItem,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        },
+    ) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): ProductViewHolder = ProductViewHolder.from(parent, eventHandler)
 
-    override fun getItemCount(): Int = items.size
-
     override fun onBindViewHolder(
         holder: ProductViewHolder,
         position: Int,
     ) {
-        val item = items[position]
+        val item = getItem(position)
         holder.bind(item.product, item.quantity)
     }
 
     fun updateItems(newItems: List<ProductItem>) {
-        if (newItems.size > items.size) {
-            items.addAll(newItems.subList(items.size, newItems.size))
-            notifyItemRangeInserted(0, newItems.size)
-            return
-        }
-        val oldItems = items.toList()
-        items.clear()
-        items.addAll(newItems)
-
-        for (i in items.indices) {
-            val oldItem = oldItems[i]
-            val newItem = newItems[i]
-
-            if (oldItem != newItem) {
-                notifyItemChanged(i)
-            }
-        }
+        submitList(newItems)
     }
 }
