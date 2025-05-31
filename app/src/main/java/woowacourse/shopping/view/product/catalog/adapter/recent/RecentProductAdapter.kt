506ba38cda8003aa6ff22760c55f74a3e51804f7
hwannow <vnfdlvgid061@naver.com>
2025-05-31 17:59:18 +0900

    refactor: `RecentProductAdapter`를 `ListAdapter`로 변경

--- app/src/main/java/woowacourse/shopping/view/product/catalog/adapter/recent/RecentProductAdapter.kt
+++ app/src/main/java/woowacourse/shopping/view/product/catalog/adapter/recent/RecentProductAdapter.kt
@@ -1,48 +1,38 @@
 package woowacourse.shopping.view.product.catalog.adapter.recent
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import woowacourse.shopping.domain.model.RecentProduct
 
 class RecentProductAdapter(
-    items: List<RecentProduct> = emptyList(),
     private val eventHandler: RecentProductViewHolder.EventHandler,
-) : RecyclerView.Adapter<RecentProductViewHolder>() {
-    private val items = items.toMutableList()
+) : ListAdapter<RecentProduct, RecentProductViewHolder>(
+        object : DiffUtil.ItemCallback<RecentProduct>() {
+            override fun areItemsTheSame(
+                oldItem: RecentProduct,
+                newItem: RecentProduct,
+            ): Boolean {
+                return oldItem.product.id == newItem.product.id
+            }
 
+            override fun areContentsTheSame(
+                oldItem: RecentProduct,
+                newItem: RecentProduct,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        },
+    ) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): RecentProductViewHolder = RecentProductViewHolder.from(parent, eventHandler)
 
-    override fun getItemCount(): Int = items.size
-
     override fun onBindViewHolder(
         holder: RecentProductViewHolder,
         position: Int,
     ) {
-        holder.bind(items[position])
-    }
-
-    fun updateItems(newItems: List<RecentProduct>) {
-        val oldSize = items.size
-        val newSize = newItems.size
-        val minSize = minOf(oldSize, newSize)
-
-        for (i in 0 until minSize) {
-            val oldItem = items[i]
-            val newItem = newItems[i]
-
-            if (oldItem != newItem) {
-                items[i] = newItem
-                notifyItemChanged(i)
-            }
-        }
-
-        if (newSize > oldSize) {
-            val addedItems = newItems.subList(oldSize, newSize)
-            items.addAll(addedItems)
-            notifyItemRangeInserted(oldSize, addedItems.size)
-        }
+        holder.bind(getItem(position))
     }
 }
