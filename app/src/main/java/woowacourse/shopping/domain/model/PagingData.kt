0d447e07fe9ae6f816416bec2d4b09a81d2e31dc
gahyunkim <mapledt001@naver.com>
2025-05-31 22:31:18 +0900

    feat: PagingData에 page 추가

--- app/src/main/java/woowacourse/shopping/domain/model/PagingData.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/PagingData.kt
@@ -4,6 +4,7 @@ import woowacourse.shopping.presentation.product.catalog.ProductUiModel
 
 data class PagingData(
     val products: List<ProductUiModel>,
+    val page: Int,
     val hasNext: Boolean,
     val hasPrevious: Boolean,
 )
