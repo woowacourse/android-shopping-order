39e4338bfc1c7eec9997e09bccdafc9d9e022003
gahyunkim <mapledt001@naver.com>
2025-06-01 13:49:33 +0900

    feat: DiffCallback 추가

---
+++ app/src/main/java/woowacourse/shopping/presentation/util/DiffCallback.kt
@@ -0,0 +1,16 @@
+package woowacourse.shopping.presentation.util
+
+import androidx.recyclerview.widget.DiffUtil
+import woowacourse.shopping.presentation.product.catalog.ProductUiModel
+
+class DiffCallback : DiffUtil.ItemCallback<ProductUiModel>() {
+    override fun areItemsTheSame(
+        oldItem: ProductUiModel,
+        newItem: ProductUiModel,
+    ): Boolean = oldItem.id == newItem.id
+
+    override fun areContentsTheSame(
+        oldItem: ProductUiModel,
+        newItem: ProductUiModel,
+    ): Boolean = oldItem == newItem
+}
