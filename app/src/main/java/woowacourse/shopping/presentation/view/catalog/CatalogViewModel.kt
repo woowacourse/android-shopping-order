f3d557d65224d5670862dbb6164155b5441b9b3e
DoableTuple <doabletuple@gmail.com>
2025-06-02 01:12:22 +0900

    refactor: 상품 목록 화면에서 목록의 항목을 가져오는 로직 수정

--- app/src/main/java/woowacourse/shopping/presentation/view/catalog/CatalogViewModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/view/catalog/CatalogViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import woowacourse.shopping.RepositoryProvider
 import woowacourse.shopping.domain.model.CartItem
+import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.domain.repository.ProductRepository
 import woowacourse.shopping.presentation.model.CartItemUiModel
@@ -32,9 +33,7 @@ class CatalogViewModel(
     private val _totalCartCount = MutableLiveData<Int>()
     val totalCartCount: LiveData<Int> = _totalCartCount
 
-    private val loadSize: Int = 20
-    private var lastId: Long = DEFAULT_ID
-    private var currentPage: Int = 0
+    private var pageIndex = 0
 
     init {
         fetchProducts()
@@ -42,55 +41,51 @@ class CatalogViewModel(
 
     fun fetchProducts() {
         _isLoading.value = true
-
-        productRepository.loadCartItems { cartItems ->
-            val totalCount = cartItems?.sumOf { it.quantity } ?: 0
-            _totalCartCount.postValue(totalCount)
-
-            val cartItemState =
-                cartItems?.associateBy(
-                    { it.product.id },
-                    { it.toUiModel() },
-                )
-
-            productRepository.loadRecentProducts(limit = 10) { recentProducts ->
-                val recentUiModels = recentProducts.map { it.toUiModel() }
-                val recentItem = CatalogItem.RecentProductItem(recentUiModels)
-
-                productRepository.loadProducts(currentPage, loadSize) { fetchedProducts, hasMore ->
-                    val fetchedUiModels =
-                        fetchedProducts.map { product ->
-                            cartItemState?.get(product.id) ?: product.toUiModel()
+        calculateTotalCartCount()
+        productRepository.loadProducts(pageIndex, PAGE_SIZE) { products, hasMore ->
+            productRepository.loadCartItems { cartItems ->
+                productRepository.loadRecentProducts(RECENTLY_VIEWED_PRODUCTS_COUNT) { recentProducts ->
+                    val productUiModels = matchProductsToCartItems(products, cartItems.orEmpty())
+                    val items =
+                        buildList {
+                            if (recentProducts.isNotEmpty()) {
+                                val recentProductItem =
+                                    CatalogItem.RecentProductItem(recentProducts.map(Product::toUiModel))
+                                add(recentProductItem)
+                            }
+                            addAll(productUiModels.map { uiModel -> CatalogItem.ProductItem(uiModel) })
+                            if (hasMore) add(CatalogItem.LoadMoreItem)
                         }
-                    lastId = fetchedUiModels.lastOrNull()?.id ?: DEFAULT_ID
-
-                    val currentUiModels =
-                        _items.value
-                            .orEmpty()
-                            .filterIsInstance<CatalogItem.ProductItem>()
-                            .map { it.product }
-
-                    val combinedUiModels =
-                        (currentUiModels + fetchedUiModels)
-                            .distinctBy { it.id }
-
-                    val updatedItems = mutableListOf<CatalogItem>()
-                    if (recentUiModels.isNotEmpty()) {
-                        updatedItems.add(recentItem)
-                    }
-                    updatedItems.addAll(
-                        combinedUiModels.map { CatalogItem.ProductItem(it) },
-                    )
-                    if (hasMore && updatedItems.none { it is CatalogItem.LoadMoreItem }) {
-                        updatedItems.add(CatalogItem.LoadMoreItem)
-                    }
-
-                    _items.postValue(updatedItems)
-                    currentPage++
+                    _items.postValue(items)
+                    pageIndex++
                 }
             }
             _isLoading.value = false
         }
+    }
+
+    private fun matchProductsToCartItems(
+        products: List<Product>,
+        cartItems: List<CartItem>,
+    ): List<ProductUiModel> {
+        val currentUiModels =
+            _items.value
+                .orEmpty()
+                .filterIsInstance<CatalogItem.ProductItem>()
+                .map(CatalogItem.ProductItem::product)
+
+        val idToCartItem =
+            cartItems.associateBy(
+                { cartItem -> cartItem.product.id },
+                { cartItem -> cartItem.toUiModel() },
+            )
+
+        val fetchedUiModels =
+            products.map { product ->
+                idToCartItem[product.id] ?: product.toUiModel()
+            }
+
+        return (currentUiModels + fetchedUiModels)
     }
 
     fun refreshCartState() {
@@ -109,17 +104,16 @@ class CatalogViewModel(
             _totalCartCount.postValue(totalCount)
 
             val updatedItems =
-                _items.value
-                    ?.map { updatedItem ->
-                        if (updatedItem is CatalogItem.ProductItem) {
-                            val updatedProduct = updatedCartState?.get(updatedItem.product.id)
-                            CatalogItem.ProductItem(
-                                updatedProduct ?: updatedItem.product.copy(quantity = 0),
-                            )
-                        } else {
-                            updatedItem
-                        }
+                _items.value?.map { updatedItem ->
+                    if (updatedItem is CatalogItem.ProductItem) {
+                        val updatedProduct = updatedCartState?.get(updatedItem.product.id)
+                        CatalogItem.ProductItem(
+                            updatedProduct ?: updatedItem.product.copy(quantity = 0),
+                        )
+                    } else {
+                        updatedItem
                     }
+                }
 
             _items.postValue(updatedItems.orEmpty())
         }
@@ -155,10 +149,12 @@ class CatalogViewModel(
         val newItem = cartItem.cartItem.copy(quantity = 1)
         cartRepository.addCartItem(newItem) {
             cartRepository.getAllCartItems { cartItems ->
-                cartItems?.find { cartItem -> cartItem.product.id == newItem.product.id }?.let { foundItem ->
-                    _itemUpdateEvent.postValue(foundItem.toUiModel())
-                    calculateTotalCartCount()
-                }
+                cartItems
+                    ?.find { cartItem -> cartItem.product.id == newItem.product.id }
+                    ?.let { foundItem ->
+                        _itemUpdateEvent.postValue(foundItem.toUiModel())
+                        calculateTotalCartCount()
+                    }
             }
         }
     }
@@ -211,7 +207,8 @@ class CatalogViewModel(
     }
 
     companion object {
-        private const val DEFAULT_ID = 0L
+        private const val PAGE_SIZE = 20
+        private const val RECENTLY_VIEWED_PRODUCTS_COUNT = 10
 
         val Factory: ViewModelProvider.Factory =
             object : ViewModelProvider.Factory {
