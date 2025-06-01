810f5652cc06cb17a54f2737e387c10fbbbb5447
medAndro <med4ndro@gmail.com>
2025-06-01 23:45:13 +0900

    feat: gson @SerializedName 추가

--- app/src/main/java/woowacourse/shopping/data/carts/dto/CartItemRequest.kt
+++ app/src/main/java/woowacourse/shopping/data/carts/dto/CartItemRequest.kt
@@ -1,6 +1,10 @@
 package woowacourse.shopping.data.carts.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class CartItemRequest(
+    @SerializedName("productId")
     val productId: Int,
+    @SerializedName("quantity")
     val quantity: Int,
 )
