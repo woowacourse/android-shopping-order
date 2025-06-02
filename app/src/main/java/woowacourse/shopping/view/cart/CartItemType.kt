ad1dbe179bf4abe99aa8037602e8fd4e3a498f66
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-06-02 19:56:34 +0900

    refactor: don't override equals, hashCode in data class

--- app/src/main/java/woowacourse/shopping/view/cart/CartItemType.kt
+++ app/src/main/java/woowacourse/shopping/view/cart/CartItemType.kt
@@ -7,24 +7,13 @@ sealed interface CartItemType {
 
     data class ProductItem(
         val cartItem: CartItem,
-        var checked: Boolean = false,
+        val checked: Boolean = false,
     ) : CartItemType {
         val cartItemId: Long = cartItem.id
         override val viewType: ItemType = ItemType.PRODUCT
         val imageUrl = cartItem.imageUrl
         var quantity = cartItem.quantity
         val price get() = cartItem.productPrice * quantity
-
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as ProductItem
-
-            return cartItemId == other.cartItemId
-        }
-
-        override fun hashCode(): Int = cartItemId.hashCode()
     }
 
     data class PaginationItem(
