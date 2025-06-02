f4f43627c540e86e4ca394e01f25feeac335a188
m6z1 <sonmyungj1zz@gmail.com>
2025-06-02 17:44:09 +0900

    refactor: 추천 상품 ListAdapter로 개선

--- app/src/main/java/woowacourse/shopping/view/shoppingCartRecommend/RecommendProductAdapter.kt
+++ app/src/main/java/woowacourse/shopping/view/shoppingCartRecommend/RecommendProductAdapter.kt
@@ -1,31 +1,35 @@
 package woowacourse.shopping.view.shoppingCartRecommend
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import woowacourse.shopping.view.common.ProductQuantityClickListener
 import woowacourse.shopping.view.product.ProductsItem
 
 class RecommendProductAdapter(
     private val productListener: ProductQuantityClickListener,
-) : RecyclerView.Adapter<RecommendProductViewHolder>() {
-    private var recommendItems: List<ProductsItem.ProductItem> = emptyList()
+) : ListAdapter<ProductsItem.ProductItem, RecommendProductViewHolder>(
+        object : DiffUtil.ItemCallback<ProductsItem.ProductItem>() {
+            override fun areItemsTheSame(
+                oldItem: ProductsItem.ProductItem,
+                newItem: ProductsItem.ProductItem,
+            ): Boolean = oldItem.product.id == newItem.product.id
 
+            override fun areContentsTheSame(
+                oldItem: ProductsItem.ProductItem,
+                newItem: ProductsItem.ProductItem,
+            ): Boolean = oldItem == newItem
+        },
+    ) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): RecommendProductViewHolder = RecommendProductViewHolder.of(parent, productListener)
 
-    override fun getItemCount(): Int = recommendItems.size
-
     override fun onBindViewHolder(
         holder: RecommendProductViewHolder,
         position: Int,
     ) {
-        holder.bind(recommendItems[position])
-    }
-
-    fun submitList(productItems: List<ProductsItem.ProductItem>) {
-        recommendItems = productItems
-        notifyDataSetChanged()
+        holder.bind(getItem(position))
     }
 }
