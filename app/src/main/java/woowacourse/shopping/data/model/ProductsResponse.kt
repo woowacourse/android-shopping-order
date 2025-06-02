b614d23aa40530c25d6d0ff4561049706f227f03
gahyunkim <mapledt001@naver.com>
2025-06-02 23:47:52 +0900

    fix: ProductsResponse 불필요한 필드들 삭제

--- app/src/main/java/woowacourse/shopping/data/model/ProductsResponse.kt
+++ app/src/main/java/woowacourse/shopping/data/model/ProductsResponse.kt
@@ -4,12 +4,5 @@ data class ProductsResponse(
     val content: List<ProductResponse>,
     val pageable: Pageable,
     val last: Boolean,
-    val totalPages: Int,
-    val totalElements: Int,
-    val sort: Sort,
     val first: Boolean,
-    val number: Int,
-    val numberOfElements: Int,
-    val size: Int,
-    val empty: Boolean,
 )
