39b549a9815c8335d6129a91a4ac341ef625579a
gahyunkim <mapledt001@naver.com>
2025-06-02 16:47:55 +0900

    refactor: RecommendAdapter ListAdapter로 수정

--- app/src/main/java/woowacourse/shopping/presentation/recommend/RecommendAdapter.kt
+++ app/src/main/java/woowacourse/shopping/presentation/recommend/RecommendAdapter.kt
@@ -1,29 +1,38 @@
 package woowacourse.shopping.presentation.recommend
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import woowacourse.shopping.presentation.product.ProductQuantityHandler
 import woowacourse.shopping.presentation.product.catalog.ProductUiModel
+import woowacourse.shopping.presentation.util.DiffCallback
 
 class RecommendAdapter(
+    private val handler: ProductQuantityHandler,
     private val onQuantityClick: (ProductUiModel) -> Unit,
-) : RecyclerView.Adapter<RecommendViewHolder>() {
-    private var items: List<ProductUiModel> = emptyList()
-
+) : ListAdapter<ProductUiModel, RecyclerView.ViewHolder>(DiffCallback()) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): RecommendViewHolder {
-        return RecommendViewHolder(parent,onQuantityClick)
+        return RecommendViewHolder(parent, onQuantityClick, handler)
     }
 
-    override fun onBindViewHolder(holder: RecommendViewHolder, position: Int) {
-        holder.bind(items[position])
+    override fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        position: Int,
+    ) {
+        (holder as RecommendViewHolder).bind(getItem(position))
     }
 
-    override fun getItemCount(): Int = items.count()
+    override fun getItemCount(): Int = currentList.count()
 
-    fun setItems(newItems: List<ProductUiModel>) {
-        items = newItems
-        notifyDataSetChanged()
+    fun updateProduct(product: ProductUiModel) {
+        val newList = currentList.toMutableList()
+        val index = newList.indexOfFirst { it.id == product.id }
+        if (index != -1) {
+            newList[index] = product
+            submitList(newList)
+        }
     }
 }
