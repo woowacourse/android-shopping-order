88c6daa38c1753eb1e98e595d1032ea07be4a247
junseo511 <prime54@g.skku.edu>
2025-06-03 17:05:36 +0900

    feat: 주문 완료 문구 출력

--- app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.fragment.app.commit
 import com.google.android.material.snackbar.Snackbar
@@ -80,6 +81,7 @@ class CartActivity : DataBindingActivity<ActivityCartBinding>(R.layout.activity_
         }
 
         viewModel.isOrdered.observe(this) {
+            Toast.makeText(this, getString(R.string.cart_order_success), Toast.LENGTH_SHORT).show()
             finish()
         }
 
