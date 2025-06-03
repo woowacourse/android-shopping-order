31c582ad1b85c2eedf065fa10aa935953b0a6a59
devfeijoa <devfeijoa@gmail.com>
2025-06-03 21:26:26 +0900

    refactor: 사용하지 않는 CartService 제거

--- app/src/test/java/woowacourse/shopping/server/TestCartServiceImpl.kt
+++ app/src/test/java/woowacourse/shopping/server/TestCartServiceImpl.kt
@@ -7,7 +7,6 @@ import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
-import woowacourse.shopping.data.local.cart.server.CartService
 import woowacourse.shopping.data.local.cart.server.DummyCarts
 import woowacourse.shopping.domain.model.Cart
 import woowacourse.shopping.domain.model.Carts
