bc96ecd03296ff1786e4328f665f319cb727483f
Ham BeomJoon <hbj0802@naver.com>
2025-06-05 08:52:01 +0900

    refactor: 현재 사용 가능한 쿠폰인지 Coupon에서 판단하도록 로직 이동

--- app/src/main/java/woowacourse/shopping/domain/model/Coupon.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Coupon.kt
@@ -1,12 +1,35 @@
 package woowacourse.shopping.domain.model
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 data class Coupon(
     val code: String,
     val description: String,
     val expirationDate: LocalDate,
+    val buyQuantity: Int?,
+    val getQuantity: Int?,
     val discount: Int?,
     val minimumAmount: Int?,
     val availableTime: AvailableTime?,
-)
+) {
+    fun isValidForOrder(
+        orderPrice: Int,
+        now: LocalDateTime = LocalDateTime.now(),
+    ): Boolean {
+        val isNotExpired = expirationDate >= now.toLocalDate()
+
+        val isAvailableTime =
+            availableTime?.let {
+                val currentTime = now.toLocalTime()
+                currentTime >= it.start && currentTime <= it.end
+            } ?: true
+
+        val isAmountSatisfied =
+            minimumAmount?.let {
+                orderPrice >= it
+            } ?: true
+
+        return isNotExpired && isAvailableTime && isAmountSatisfied
+    }
+}
