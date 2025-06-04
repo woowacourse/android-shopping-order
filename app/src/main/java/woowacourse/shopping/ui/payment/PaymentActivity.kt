cb6eb3dfcfeff5467356cf4c0515a46bc2c6f35f
junseo511 <prime54@g.skku.edu>
2025-06-04 10:56:06 +0900

    feat: 쿠폰 목록을 제외한 결제 UI 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/payment/PaymentActivity.kt
@@ -0,0 +1,7 @@
+package woowacourse.shopping.ui.payment
+
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityPaymentBinding
+import woowacourse.shopping.ui.common.DataBindingActivity
+
+class PaymentActivity : DataBindingActivity<ActivityPaymentBinding>(R.layout.activity_payment)
