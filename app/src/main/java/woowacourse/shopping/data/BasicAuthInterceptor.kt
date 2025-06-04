9ab03386aa8fa7a199ee7c0c3fad686d94aaacd1
cucumber99 <dlsguq1@gmail.com>
2025-06-04 22:31:04 +0900

    refactor: Interceptor 로직 개선

--- app/src/main/java/woowacourse/shopping/data/BasicAuthInterceptor.kt
+++ app/src/main/java/woowacourse/shopping/data/BasicAuthInterceptor.kt
@@ -1,30 +1,26 @@
 package woowacourse.shopping.data
 
+import okhttp3.Credentials
 import okhttp3.Interceptor
 import okhttp3.Response
 import woowacourse.shopping.BuildConfig
-import java.util.Base64
 
-class BasicAuthInterceptor : Interceptor {
+class BasicAuthInterceptor(
+    private val id: String = BuildConfig.USER_ID,
+    private val password: String = BuildConfig.USER_PASSWORD,
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val credential = Credentials.basic(id, password)
         val request =
             chain.request()
                 .newBuilder()
-                .addHeader(HEADER_NAME, getBasicCredential())
+                .addHeader(HEADER_NAME, credential)
                 .build()
 
         return chain.proceed(request)
     }
 
-    private fun getBasicCredential(): String {
-        val credential = "${BuildConfig.USER_ID}$DELIMITER${BuildConfig.USER_PASSWORD}"
-        val encoded = Base64.getEncoder().encodeToString(credential.toByteArray())
-        return "$AUTHORIZATION_HEADER$encoded"
-    }
-
     companion object {
         private const val HEADER_NAME = "Authorization"
-        private const val AUTHORIZATION_HEADER = "Basic "
-        private const val DELIMITER = ":"
     }
 }
--- app/src/main/java/woowacourse/shopping/data/source/remote/Client.kt
+++ app/src/main/java/woowacourse/shopping/data/source/remote/Client.kt
@@ -8,12 +8,20 @@ import woowacourse.shopping.data.source.remote.api.CartApiService
 import woowacourse.shopping.data.source.remote.api.ProductsApiService
 
 object Client {
+    val getProductsApiService: ProductsApiService by lazy {
+        retrofit.create(
+            ProductsApiService::class.java,
+        )
+    }
+
+    val getCartApiService: CartApiService by lazy { authRetrofit.create(CartApiService::class.java) }
+
     private val okHttpClient =
         OkHttpClient.Builder()
             .addInterceptor(BasicAuthInterceptor())
             .build()
 
-    private val cartApiService: Retrofit by lazy {
+    private val authRetrofit: Retrofit by lazy {
         Retrofit.Builder()
             .client(okHttpClient)
             .baseUrl(BASE_URL)
@@ -21,19 +29,12 @@ object Client {
             .build()
     }
 
-    private val productApiService: Retrofit by lazy {
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
-
-    val getProductRetrofitService: ProductsApiService by lazy {
-        productApiService.create(
-            ProductsApiService::class.java,
-        )
-    }
-    val getCartRetrofitService: CartApiService by lazy { cartApiService.create(CartApiService::class.java) }
 
     private const val BASE_URL =
         "http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com"
