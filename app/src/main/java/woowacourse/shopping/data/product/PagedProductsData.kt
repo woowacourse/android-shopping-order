d9d67422cb52def0a715db4f6802c257f4f9e2ec
jerry8282 <ka06196@naver.com>
2025-06-05 17:34:33 +0900

    refactor: remove unused PagedCartItemData and PagedProductsData

--- app/src/main/java/woowacourse/shopping/data/product/PagedProductsData.kt
+++
@@ -1,8 +0,0 @@
-package woowacourse.shopping.data.product
-
-import woowacourse.shopping.data.product.entity.ProductEntity
-
-class PagedProductsData(
-    val products: List<ProductEntity>,
-    val loadable: Boolean,
-)
