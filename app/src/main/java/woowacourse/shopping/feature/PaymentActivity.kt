186b7831d37f5575f69f71cc5a3528b7b5b9c665
rosemin928 <rosemin928@naver.com>
2025-06-06 16:55:30 +0900

    feat: 결제 화면 UI 구현

---
+++ app/src/main/java/woowacourse/shopping/feature/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.shopping.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
