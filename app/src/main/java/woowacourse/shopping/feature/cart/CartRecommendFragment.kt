ef0e0c4b74a402115fc404e707e5310b01d46968
rosemin928 <rosemin928@naver.com>
2025-06-06 17:14:11 +0900

    feat: 주문 버튼 클릭 시 결제 화면으로 이동 기능 구현

--- app/src/main/java/woowacourse/shopping/feature/cart/CartRecommendFragment.kt
+++ app/src/main/java/woowacourse/shopping/feature/cart/CartRecommendFragment.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping.feature.cart
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import woowacourse.shopping.databinding.FragmentCartRecommendBinding
 import woowacourse.shopping.feature.cart.adapter.RecommendAdapter
+import woowacourse.shopping.feature.payment.PaymentActivity
 import kotlin.getValue
 
 class CartRecommendFragment : Fragment() {
@@ -31,6 +33,16 @@ class CartRecommendFragment : Fragment() {
                 LinearLayoutManager.HORIZONTAL,
                 false,
             )
+
+        binding.tvOrderButton.setOnClickListener {
+            Log.d("CartRecommendFragment", "order button clicked")
+            navigate()
+        }
         return binding.root
+    }
+
+    private fun navigate() {
+        val intent = PaymentActivity.newIntent(this.requireActivity())
+        startActivity(intent)
     }
 }
--- app/src/main/java/woowacourse/shopping/feature/payment/PaymentActivity.kt
+++ app/src/main/java/woowacourse/shopping/feature/payment/PaymentActivity.kt
@@ -1,5 +1,7 @@
-package woowacourse.shopping.feature
+package woowacourse.shopping.feature.payment
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.shopping.R
@@ -8,5 +10,12 @@ class PaymentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent =
+            Intent(context, PaymentActivity::class.java).apply {
+//                putExtra(GOODS_KEY, id)
+            }
     }
 }
