24c71d12bd6d801b388cde9facdc5c47658d7d37
medAndro <med4ndro@gmail.com>
2025-06-08 02:00:53 +0900

    feat: 쿠폰 DTO 작성

---
+++ app/src/main/java/woowacourse/shopping/data/coupon/dto/Coupon.kt
@@ -0,0 +1,7 @@
+package woowacourse.shopping.data.coupon.dto
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+class Coupon : ArrayList<CouponItem>()
---
+++ app/src/main/java/woowacourse/shopping/data/coupon/dto/CouponItem.kt
@@ -0,0 +1,29 @@
+package woowacourse.shopping.data.coupon.dto
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CouponItem(
+    @SerialName("availableTime")
+    val availableTime: AvailableTime,
+    @SerialName("buyQuantity")
+    val buyQuantity: Int,
+    @SerialName("code")
+    val code: String,
+    @SerialName("description")
+    val description: String,
+    @SerialName("discount")
+    val discount: Int,
+    @SerialName("discountType")
+    val discountType: String,
+    @SerialName("expirationDate")
+    val expirationDate: String,
+    @SerialName("getQuantity")
+    val getQuantity: Int,
+    @SerialName("id")
+    val id: Int,
+    @SerialName("minimumAmount")
+    val minimumAmount: Int
+)
