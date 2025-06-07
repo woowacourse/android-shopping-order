6c53ed81645f42c46418a75be93db4915907e414
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-06-07 20:13:59 +0900

    feat: implement coupon apply view

---
+++ app/src/main/java/woowacourse/shopping/view/payment/CouponApplyActivity.kt
@@ -0,0 +1,28 @@
+package woowacourse.shopping.view.payment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityCouponApplyBinding
+
+class CouponApplyActivity : AppCompatActivity() {
+    private val binding: ActivityCouponApplyBinding by lazy {
+        ActivityCouponApplyBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.couponApplyRoot)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.onClickBackButton = { finish() }
+    }
+}
