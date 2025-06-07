3da1bca9cc00c9361737f1bc081a5f13f2a29ac5
Ham BeomJoon <hbj0802@naver.com>
2025-06-07 11:41:12 +0900

    feat: couponUiModel, availableTimeUiModel 매퍼 함수 추가

--- app/src/main/java/woowacourse/shopping/presentation/model/AvailableTimeUiModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/model/AvailableTimeUiModel.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping.presentation.model
 
 import woowacourse.shopping.domain.model.AvailableTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 data class AvailableTimeUiModel(
@@ -14,4 +15,13 @@ fun AvailableTime.toPresentation(): AvailableTimeUiModel {
         )
     }"
     return AvailableTimeUiModel(timeRange)
+}
+
+fun AvailableTimeUiModel.toDomain(): AvailableTime {
+    val (startStr, endStr) = timeRange.split(" ~ ")
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    return AvailableTime(
+        start = LocalTime.parse(startStr, formatter),
+        end = LocalTime.parse(endStr, formatter),
+    )
 }
--- app/src/main/java/woowacourse/shopping/presentation/model/CouponUiModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/model/CouponUiModel.kt
@@ -2,6 +2,7 @@ package woowacourse.shopping.presentation.model
 
 import woowacourse.shopping.domain.model.Coupon
 import woowacourse.shopping.domain.model.DiscountType
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 data class CouponUiModel(
@@ -9,11 +10,35 @@ data class CouponUiModel(
     val description: String,
     val expirationDate: String,
     val discount: Int?,
+    val buyQuantity: Int?,
+    val getQuantity: Int?,
     val discountType: String,
     val minimumAmount: Int?,
     val availableTime: AvailableTimeUiModel?,
     val isSelected: Boolean = false,
 )
+
+fun CouponUiModel.toDomain(): Coupon =
+    Coupon(
+        code = code,
+        description = description,
+        expirationDate =
+            LocalDate.parse(
+                expirationDate,
+                DateTimeFormatter.ofPattern("yyyy년 M월 d일"),
+            ),
+        discount = discount,
+        discountType =
+            discountType.toDiscountType(
+                discount,
+                buyQuantity = buyQuantity,
+                getQuantity = getQuantity,
+            ),
+        minimumAmount = minimumAmount,
+        availableTime = availableTime?.toDomain(),
+        buyQuantity = buyQuantity,
+        getQuantity = getQuantity,
+    )
 
 fun Coupon.toPresentation(): CouponUiModel =
     CouponUiModel(
@@ -21,6 +46,8 @@ fun Coupon.toPresentation(): CouponUiModel =
         description = description,
         expirationDate = expirationDate.format(DateTimeFormatter.ofPattern("yyyy년 M월 d일")),
         discount = discount,
+        buyQuantity = buyQuantity,
+        getQuantity = getQuantity,
         discountType = discountType.toPresentation(),
         minimumAmount = minimumAmount,
         availableTime = availableTime?.toPresentation(),
@@ -33,4 +60,22 @@ fun DiscountType.toPresentation(): String =
         is DiscountType.Percentage -> "percentage"
         is DiscountType.FreeShipping -> "free_shipping"
         is DiscountType.BuyXGetY -> "buyXgetY"
+    }
+
+fun String.toDiscountType(
+    discount: Int?,
+    buyQuantity: Int?,
+    getQuantity: Int?,
+): DiscountType =
+    when (this) {
+        "fixed" -> DiscountType.FixedAmount(discount ?: 0)
+        "percentage" -> DiscountType.Percentage(discount ?: 0)
+        "free_shipping" -> DiscountType.FreeShipping
+        "buyXgetY" ->
+            DiscountType.BuyXGetY(
+                buyQuantity = buyQuantity ?: 0,
+                getQuantity = getQuantity ?: 0,
+            )
+
+        else -> throw IllegalArgumentException("알 수 없는 discountType: $this")
     }
