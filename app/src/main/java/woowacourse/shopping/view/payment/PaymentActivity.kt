6f231c0b14b1fca36377f74ec0dcacb47384ce44
hwannow <vnfdlvgid061@naver.com>
2025-06-07 15:26:13 +0900

    feat: 결제 화면 초기 설정
    - PaymentActivity DataBinding 설정
    - PaymentActivity `newIntent` 추가

--- app/src/main/java/woowacourse/shopping/view/payment/PaymentActivity.kt
+++ app/src/main/java/woowacourse/shopping/view/payment/PaymentActivity.kt
@@ -1,21 +1,46 @@
 package woowacourse.shopping.view.payment
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityPaymentBinding
+import woowacourse.shopping.domain.model.CartProduct
 
 class PaymentActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityPaymentBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpView()
+    }
+
+    private fun setUpView() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_payment)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    companion object {
+        const val KEY_SELECTED_PRODUCTS = "selected_products"
+
+        fun newIntent(
+            context: Context,
+            selectedProducts: ArrayList<CartProduct>,
+        ): Intent =
+            Intent(
+                context,
+                PaymentActivity::class.java,
+            ).apply {
+                putExtra(KEY_SELECTED_PRODUCTS, selectedProducts)
+            }
     }
 }
