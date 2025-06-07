6e960542bf26f91d6e4508b94b951e440235aa88
moondev03 <moondev03@gmail.com>
2025-06-07 17:36:11 +0900

    feat: PaymentSummary 도메인 및 UI 모델 추가

---
+++ app/src/main/java/woowacourse/shopping/domain/model/PaymentSummary.kt
@@ -0,0 +1,18 @@
+package woowacourse.shopping.domain.model
+
+data class PaymentSummary(
+    val products: List<CartProduct>,
+    val discountPrice: Int = DEFAULT_DISCOUNT_PRICE,
+    val deliveryFee: Int = DEFAULT_DELIVERY_FEE,
+) {
+    val orderPrice: Int
+        get() = products.sumOf { it.totalPrice }
+
+    val finalPaymentPrice: Int
+        get() = orderPrice - discountPrice + deliveryFee
+
+    companion object {
+        private const val DEFAULT_DELIVERY_FEE = 3000
+        private const val DEFAULT_DISCOUNT_PRICE = 0
+    }
+}
