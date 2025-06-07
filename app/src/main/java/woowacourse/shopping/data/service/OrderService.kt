d7779ed9296de5518d767005b088cd2eacbc6d54
moondev03 <moondev03@gmail.com>
2025-06-08 00:00:00 +0900

    feat: 주문 요청 API 인증 헤더 추가

--- app/src/main/java/woowacourse/shopping/data/service/OrderService.kt
+++ app/src/main/java/woowacourse/shopping/data/service/OrderService.kt
@@ -2,12 +2,14 @@ package woowacourse.shopping.data.service
 
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 import woowacourse.shopping.data.model.order.OrderRequest
 
 interface OrderService {
     @POST("/orders")
-    fun postOrder(
+    suspend fun postOrder(
+        @Header("Authorization") key: String,
         @Body orderRequest: OrderRequest,
     ): Response<Unit>
 }
