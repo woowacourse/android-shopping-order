fe26d3857c39feb90b9ee4dc8daac1459405cc88
JunHyuk Im <imjh0417@gmail.com>
2025-06-07 16:32:06 +0900

    feat: OrderActivity 및 레이아웃 추가

---
+++ app/src/main/java/woowacourse/shopping/order/OrderActivity.kt
@@ -0,0 +1,37 @@
+package woowacourse.shopping.order
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityOrderBinding
+
+class OrderActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOrderBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_order)
+
+        applyWindowInsets()
+        setSupportActionBar()
+    }
+
+    private fun setSupportActionBar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.text_order_action_bar)
+    }
+
+    private fun applyWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
