72c1c35248cc4682a3399e88eb3c9213a1133160
gahyunkim <mapledt001@naver.com>
2025-06-08 15:56:19 +0900

    feat: CouponResponseMapper 구현

---
+++ app/src/main/java/woowacourse/shopping/mapper/CouponResponsemapper.kt
@@ -0,0 +1,24 @@
+package woowacourse.shopping.mapper
+
+import woowacourse.shopping.data.model.Coupon
+import woowacourse.shopping.data.model.Coupon.AvailableTime
+import woowacourse.shopping.data.model.CouponResponse
+
+fun CouponResponse.toDomain() =
+    Coupon(
+        id = this.id,
+        code = this.code,
+        description = this.description,
+        expirationDate = this.expirationDate,
+        discountType = this.discountType,
+        discount = this.discount,
+        buyQuantity = this.buyQuantity,
+        getQuantity = this.getQuantity,
+        minimumAmount = this.minimumAmount,
+        availableTime = this.availableTime?.let {
+            AvailableTime(
+                start = it.start,
+                end = it.end
+            )
+        }
+    )
