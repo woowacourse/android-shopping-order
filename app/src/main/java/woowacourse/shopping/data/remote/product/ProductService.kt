8f185b9d40e07a2d72d9286511bc73e18d64270d
devfeijoa <devfeijoa@gmail.com>
2025-06-08 22:19:43 +0900

    refactor: ProductService의 API 요청 함수를 코루틴으로 변경하고, ProductRepository에 Result 패턴 적용

--- app/src/main/java/woowacourse/shopping/data/remote/product/ProductService.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/product/ProductService.kt
@@ -1,19 +1,19 @@
 package woowacourse.shopping.data.remote.product
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ProductService {
     @GET("/products")
-    fun requestGoods(
+    suspend fun requestGoods(
         @Query("page") page: Int = 0,
         @Query("size") size: Int = 20,
-    ): Call<ProductResponse>
+    ): Response<ProductResponse>
 
     @GET("/products/{productId}")
     fun requestProductDetails(
         @Path("productId") productId: Long,
-    ): Call<ProductResponse.Content>
+    ): Response<ProductDetailResponse>
 }
