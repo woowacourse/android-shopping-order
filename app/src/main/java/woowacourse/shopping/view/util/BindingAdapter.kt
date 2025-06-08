3a8d3dce4dba5aff584a690f73920fbe42ea4f98
jiyunlee <02leejiyun@naver.com>
2025-06-08 18:30:05 +0900

    feat: 쿠폰 만료일 표시

--- app/src/main/java/woowacourse/shopping/view/util/BindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/view/util/BindingAdapter.kt
@@ -1,9 +1,12 @@
 package woowacourse.shopping.view.util
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import woowacourse.shopping.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImage(image: String?) {
@@ -14,4 +17,12 @@ fun ImageView.setImage(image: String?) {
         .placeholder(R.drawable.ic_launcher_foreground)
         .centerCrop()
         .into(this)
+}
+
+@BindingAdapter("expirationDate")
+fun TextView.setExpirationDate(date: LocalDate?) {
+    date ?: return
+
+    val formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일")
+    this.text = context.getString(R.string.expiration_date, date.format(formatter))
 }
