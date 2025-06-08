05cefdeadcc83f018b6b1faba58113a276f7e665
DoableTuple <doabletuple@gmail.com>
2025-06-09 00:50:11 +0900

    refactor: 결제 화면에서 선택된 상품들의 아이디 목록을 받을 수 있도록 변경

--- app/src/main/java/woowacourse/shopping/presentation/view/checkout/CheckoutFragment.kt
+++ app/src/main/java/woowacourse/shopping/presentation/view/checkout/CheckoutFragment.kt
@@ -2,6 +2,7 @@ package woowacourse.shopping.presentation.view.checkout
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.FragmentCheckoutBinding
@@ -18,6 +19,8 @@ class CheckoutFragment : BaseFragment<FragmentCheckoutBinding>(R.layout.fragment
     ) {
         super.onViewCreated(view, savedInstanceState)
 
+        val selectedProductIds = arguments?.getLongArray(SELECTED_PRODUCT_IDS)
+
         initBinding()
         initObserver()
         viewModel.loadCoupons()
@@ -32,5 +35,11 @@ class CheckoutFragment : BaseFragment<FragmentCheckoutBinding>(R.layout.fragment
         viewModel.coupons.observe(viewLifecycleOwner) { coupons ->
             adapter.submitList(coupons)
         }
+    }
+
+    companion object {
+        private const val SELECTED_PRODUCT_IDS = "selected_product_ids"
+
+        fun newBundle(ids: LongArray) = bundleOf(SELECTED_PRODUCT_IDS to ids)
     }
 }
