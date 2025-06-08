8edc825c38a3b09e04528da3ed2f595154ae04b8
gahyunkim <mapledt001@naver.com>
2025-06-08 16:00:29 +0900

    feat: 쿠폰 정책 적용 기능 구현

---
+++ app/src/main/java/woowacourse/shopping/presentation/payment/ApplyCouponPolicyUseCase.kt
@@ -0,0 +1,46 @@
+package woowacourse.shopping.presentation.payment
+
+import woowacourse.shopping.data.model.Coupon
+import woowacourse.shopping.data.model.CouponType
+import woowacourse.shopping.domain.repository.CouponPolicy
+import woowacourse.shopping.presentation.product.catalog.ProductUiModel
+
+class ApplyCouponPolicyUseCase {
+    fun applyPolicy(
+        coupon: Coupon,
+        initialOrderPrice: Long,
+        checkedItems: List<ProductUiModel>,
+    ): OrderAdjustment {
+        val policy = CouponType.from(coupon.code).getPolicy()
+        val discount = coupon.discount ?: 0L
+        val discountPercentage = discount / 100
+        return when (policy) {
+            CouponPolicy.Fixed5000 -> OrderAdjustment(discount = discount)
+            CouponPolicy.MiracleSale -> OrderAdjustment(discount = (initialOrderPrice * discountPercentage).toLong())
+            CouponPolicy.FreeShipping -> OrderAdjustment(deliveryCharge = 0)
+            CouponPolicy.Bogo -> {
+                val discount = calculateBogoDiscount(coupon,checkedItems)
+                OrderAdjustment(discount = discount)
+            }
+
+            CouponPolicy.Default -> OrderAdjustment()
+        }
+    }
+
+    private fun calculateBogoDiscount(coupon: Coupon, checkedItems: List<ProductUiModel>): Long {
+        val targetProduct = checkedItems
+            .filter { it.quantity >= (coupon.buyQuantity ?: 0) + (coupon.getQuantity ?: 0) }
+            .maxByOrNull { it.price }
+
+        val buyQuantity = coupon.buyQuantity ?: 0
+        val getQuantity = coupon.getQuantity ?: 0
+
+        return if (targetProduct != null && buyQuantity > 0 && getQuantity > 0) {
+            val discountQuantity = getQuantity
+            discountQuantity * targetProduct.price.toLong()
+        } else {
+            0L
+        }
+    }
+}
+
---
+++ app/src/main/java/woowacourse/shopping/presentation/payment/OrderAdjustment.kt
@@ -0,0 +1,11 @@
+package woowacourse.shopping.presentation.payment
+
+data class OrderAdjustment(
+    val discount: Long = INITIAL_DISCOUNT_CHARGE,
+    val deliveryCharge: Long = INITIAL_DELIVERY_CHARGE
+){
+    companion object{
+        private const val INITIAL_DELIVERY_CHARGE = 3_000L
+        private const val INITIAL_DISCOUNT_CHARGE = 0L
+    }
+}
