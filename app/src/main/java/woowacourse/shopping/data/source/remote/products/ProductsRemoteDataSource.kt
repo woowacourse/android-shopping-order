4aafff5f29098b88e49533469d3b76b8027ef280
cucumber99 <dlsguq1@gmail.com>
2025-06-09 04:49:11 +0900

    refactor: suspend 함수로 변경 및 safeApiCall 사용

--- app/src/main/java/woowacourse/shopping/data/source/remote/products/ProductsRemoteDataSource.kt
+++ app/src/main/java/woowacourse/shopping/data/source/remote/products/ProductsRemoteDataSource.kt
@@ -3,30 +3,24 @@ package woowacourse.shopping.data.source.remote.products
 import woowacourse.shopping.data.model.ProductResponse
 import woowacourse.shopping.data.model.ProductsResponse
 import woowacourse.shopping.data.source.remote.api.ProductsApiService
-import woowacourse.shopping.data.source.remote.util.enqueueResult
+import woowacourse.shopping.data.source.remote.util.safeApiCall
 
 class ProductsRemoteDataSource(
     private val api: ProductsApiService,
 ) : ProductsDataSource {
-    override fun getProducts(
+    override suspend fun getProducts(
         page: Int?,
         size: Int?,
-        onResult: (Result<ProductsResponse>) -> Unit,
-    ) {
-        api.getProducts(page = page, size = size).enqueueResult(onResult)
+    ): Result<ProductsResponse> = safeApiCall {
+        api.getProducts(page = page, size = size)
     }
 
-    override fun getProductById(
-        id: Long,
-        onResult: (Result<ProductResponse>) -> Unit,
-    ) {
-        api.getProductById(id = id).enqueueResult(onResult)
+    override suspend fun getProductById(id: Long): Result<ProductResponse> = safeApiCall {
+        api.getProductById(id = id)
     }
 
-    override fun getProductsByCategory(
-        category: String,
-        onResult: (Result<ProductsResponse>) -> Unit,
-    ) {
-        api.getProductsByCategory(category = category).enqueueResult(onResult)
-    }
+    override suspend fun getProductsByCategory(category: String): Result<ProductsResponse> =
+        safeApiCall {
+            api.getProductsByCategory(category = category)
+        }
 }
