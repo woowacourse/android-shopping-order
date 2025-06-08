b9deccb7b1c954cdfe25bc5049756e580c8bee51
cucumber99 <dlsguq1@gmail.com>
2025-06-09 04:49:39 +0900

    refactor: 코루틴 적용에 따른 ProductsRepository 코드 개선

--- app/src/main/java/woowacourse/shopping/domain/repository/ProductsRepository.kt
+++ app/src/main/java/woowacourse/shopping/domain/repository/ProductsRepository.kt
@@ -2,22 +2,14 @@ package woowacourse.shopping.domain.repository
 
 import woowacourse.shopping.domain.model.PagingData
 import woowacourse.shopping.domain.model.Product
-import woowacourse.shopping.presentation.product.catalog.ProductUiModel
 
 interface ProductsRepository {
-    fun getProducts(
+    suspend fun getProducts(
         page: Int,
         size: Int = 20,
-        onResult: (Result<PagingData>) -> Unit,
-    )
+    ): Result<PagingData>
 
-    fun getProductById(
-        id: Long,
-        onResult: (Result<ProductUiModel>) -> Unit,
-    )
+    suspend fun getProductById(id: Long): Result<Product>
 
-    fun getRecommendProducts(
-        category: String,
-        onResult: (Result<List<Product>>) -> Unit,
-    )
+    suspend fun getRecommendProducts(category: String): Result<List<Product>>
 }
