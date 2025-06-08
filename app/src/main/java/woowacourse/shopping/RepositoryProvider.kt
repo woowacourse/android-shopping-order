2044e65eb45e7dc03c1eadd19bbe6cd90cc9f031
DongjooSEO <readyjoo24@gmail.com>
2025-06-08 14:17:28 +0900

    feat: order, coupon Repository 추가

--- app/src/main/java/woowacourse/shopping/RepositoryProvider.kt
+++ app/src/main/java/woowacourse/shopping/RepositoryProvider.kt
@@ -5,13 +5,20 @@ import woowacourse.shopping.data.CartRepositoryImpl
 import woowacourse.shopping.data.ProductRepositoryImpl
 import woowacourse.shopping.data.api.ApiClient
 import woowacourse.shopping.data.datasource.CartItemDataSourceImpl
+import woowacourse.shopping.data.datasource.CouponDataSourceImpl
+import woowacourse.shopping.data.datasource.OrderDataSourceImpl
 import woowacourse.shopping.data.datasource.ProductDataSourceImpl
 import woowacourse.shopping.data.db.ShoppingDatabase
 import woowacourse.shopping.domain.repository.CartRepository
+import woowacourse.shopping.domain.repository.CouponRepository
+import woowacourse.shopping.domain.repository.CouponRepositoryImpl
+import woowacourse.shopping.domain.repository.OrderRepository
+import woowacourse.shopping.domain.repository.OrderRepositoryImpl
 import woowacourse.shopping.domain.repository.ProductRepository
 
 object RepositoryProvider {
     private const val NOT_INITIALIZED_MESSAGE = "%s가 초기화되지 않았습니다"
+    private var isInitialized = false
 
     private var _cartRepository: CartRepository? = null
     val cartRepository
@@ -31,7 +38,22 @@ object RepositoryProvider {
                 )
             }
 
-    private var isInitialized = false
+    private var _orderRepository: OrderRepository? = null
+    val orderRepository
+        get() =
+            requireNotNull(_orderRepository) {
+                NOT_INITIALIZED_MESSAGE.format(
+                    OrderRepository::class.simpleName,
+                )
+            }
+    private var _couponRepository: CouponRepository? = null
+    val couponRepository
+        get() =
+            requireNotNull(_couponRepository) {
+                NOT_INITIALIZED_MESSAGE.format(
+                    CouponRepository::class.simpleName,
+                )
+            }
 
     fun initialize(context: Context) {
         if (isInitialized) return
@@ -41,6 +63,8 @@ object RepositoryProvider {
 
         val productDataSource = ProductDataSourceImpl(ApiClient.productService)
         val cartItemDataSource = CartItemDataSourceImpl(ApiClient.cartItemService)
+        val orderDataSource = OrderDataSourceImpl(ApiClient.orderService)
+        val couponDataSource = CouponDataSourceImpl(ApiClient.couponService)
 
         _cartRepository = CartRepositoryImpl(cartItemDataSource)
         _productRepository =
@@ -48,6 +72,8 @@ object RepositoryProvider {
                 recentProductDao,
                 productDataSource,
             )
+        _orderRepository = OrderRepositoryImpl(orderDataSource)
+        _couponRepository = CouponRepositoryImpl(couponDataSource)
 
         isInitialized = true
     }
@@ -58,5 +84,13 @@ object RepositoryProvider {
 
     fun initProductRepository(repository: ProductRepository) {
         _productRepository = repository
+    }
+
+    fun initOrderRepository(repository: OrderRepository) {
+        _orderRepository = repository
+    }
+
+    fun initCouponRepository(repository: CouponRepository) {
+        _couponRepository = repository
     }
 }
