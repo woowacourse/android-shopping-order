51e094b9432fb5adf3a912e3f9282d2212e1a0b4
cucumber99 <dlsguq1@gmail.com>
2025-06-09 17:13:00 +0900

    feat: 쿠폰 인터페이스 및 구현체 추가

---
+++ app/src/main/java/woowacourse/shopping/domain/model/coupon/PercentageCoupon.kt
@@ -0,0 +1,28 @@
+package woowacourse.shopping.domain.model.coupon
+
+import woowacourse.shopping.domain.model.OrderInfo
+import java.time.LocalDate
+
+data class PercentageCoupon(
+    override val code: String,
+    override val id: Long,
+    override val description: String,
+    override val expirationDate: LocalDate,
+    val discount: Int,
+    val availableTime: AvailableTime,
+    override val discountType: String,
+) : Coupon {
+    override fun calculateDiscount(orderInfo: OrderInfo): OrderInfo {
+        val discount = (orderInfo.orderAmount * DISCOUNT_PERCENTAGE).toInt()
+        return orderInfo.copy(discount = -discount)
+    }
+
+    override fun isAvailable(): Boolean {
+        val isAvailable = availableTime.isAvailableTime()
+        return super.isAvailable() && isAvailable
+    }
+
+    companion object {
+        private const val DISCOUNT_PERCENTAGE = 0.3
+    }
+}
