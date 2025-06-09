9e974be16bcbdaf137f72456ed735bf687c59f3a
cucumber99 <dlsguq1@gmail.com>
2025-06-09 17:11:27 +0900

    feat: 쿠폰 UI 어댑터 및 뷰홀더 구현

---
+++ app/src/main/java/woowacourse/shopping/presentation/order/CouponViewHolder.kt
@@ -0,0 +1,24 @@
+package woowacourse.shopping.presentation.order
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ItemCouponBinding
+
+class CouponViewHolder(
+    parent: ViewGroup,
+    private val couponClickListener: CouponClickListener,
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.item_coupon, parent, false)
+) {
+    private val binding: ItemCouponBinding = ItemCouponBinding.bind(itemView)
+
+    init {
+        binding.clickListener = couponClickListener
+    }
+
+    fun bind(coupon: CouponUiModel) {
+        binding.coupon = coupon
+    }
+}
