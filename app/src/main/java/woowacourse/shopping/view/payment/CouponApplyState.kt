d4f3cc871c91a8076c42bc4ed3a58936312c1910
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-06-09 15:45:48 +0900

    feat: implement CouponApplyState

---
+++ app/src/main/java/woowacourse/shopping/view/payment/CouponApplyState.kt
@@ -0,0 +1,34 @@
+package woowacourse.shopping.view.payment
+
+import woowacourse.shopping.domain.cart.CartItem
+import woowacourse.shopping.domain.payment.Coupon
+
+data class CouponApplyState(
+    val loading: Boolean,
+    val coupons: List<CouponsItem>,
+    val selectedCoupon: Coupon?,
+    val cartItems: List<CartItem>,
+    val deliveryFee: Int,
+) {
+    val orderAmount: Int = cartItems.sumOf(CartItem::price)
+    val discountAmount: Int =
+        when (selectedCoupon) {
+            is Coupon.BuyNGetNCoupon -> TODO()
+            is Coupon.FixedDiscountCoupon -> selectedCoupon.discount
+            is Coupon.FreeShippingCoupon -> deliveryFee
+            is Coupon.PercentageCoupon -> (orderAmount * selectedCoupon.discountRate).toInt()
+            null -> 0
+        }
+    val totalPaymentAmount: Int = orderAmount - discountAmount
+
+    companion object {
+        val Loading: CouponApplyState =
+            CouponApplyState(
+                loading = true,
+                coupons = emptyList(),
+                selectedCoupon = null,
+                cartItems = emptyList(),
+                deliveryFee = 0,
+            )
+    }
+}
