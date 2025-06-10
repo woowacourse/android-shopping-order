e2258a3d7ded37cdd6df316b681a944cbd977483
yrsel <xxknu88@gmail.com>
2025-06-10 14:00:59 +0900

    refactor: 장바구니 ViewModel LiveData map 활용하는 방식으로 변경 및 코드 포맷팅

--- app/src/main/java/woowacourse/shopping/presentation/cart/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/cart/CartViewModel.kt
@@ -1,9 +1,9 @@
 package woowacourse.shopping.presentation.cart
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import woowacourse.shopping.R
@@ -26,27 +26,12 @@ class CartViewModel(
     val uiState: LiveData<ResultState<Unit>> = _uiState
     private val _cartItems: MutableLiveData<List<CartItemUiModel>> = MutableLiveData()
     val cartItems: LiveData<List<CartItemUiModel>> = _cartItems
-    private val _selectedTotalPrice: MediatorLiveData<Int> =
-        MediatorLiveData<Int>().apply {
-            addSource(_cartItems) { cartItems ->
-                value = cartItems?.filter { it.isSelected }?.sumOf { it.totalPrice } ?: 0
-            }
-        }
-    val selectedTotalPrice: LiveData<Int> = _selectedTotalPrice
-    private val _selectedTotalCount: MediatorLiveData<Int> =
-        MediatorLiveData<Int>().apply {
-            addSource(_cartItems) { cartItems ->
-                value = cartItems?.filter { it.isSelected }?.sumOf { it.quantity } ?: 0
-            }
-        }
-    val selectedTotalCount: LiveData<Int> = _selectedTotalCount
-    private val _isCheckAll: MediatorLiveData<Boolean> =
-        MediatorLiveData<Boolean>().apply {
-            addSource(_cartItems) { cartItems ->
-                value = cartItems?.all { it.isSelected } ?: false
-            }
-        }
-    val isCheckAll: LiveData<Boolean> = _isCheckAll
+    val selectedTotalPrice: LiveData<Int> =
+        cartItems.map { cartItems -> cartItems.filter { it.isSelected }.sumOf { it.totalPrice } }
+    val selectedTotalCount =
+        cartItems.map { cartItems -> cartItems.filter { it.isSelected }.sumOf { it.quantity } }
+    val isCheckAll: LiveData<Boolean> =
+        cartItems.map { cartItems -> cartItems.all { it.isSelected } }
     private val _toastMessage = SingleLiveData<Int>()
     val toastMessage: LiveData<Int> = _toastMessage
     private val _navigateTo = SingleLiveData<LongArray>()
@@ -117,14 +102,14 @@ class CartViewModel(
     }
 
     override fun onClickSelect(cartId: Long) {
+        val cartItems = _cartItems.value ?: return
         val newCartItems =
-            _cartItems.value?.map { if (it.id == cartId) it.copy(isSelected = !it.isSelected) else it }
-                ?: return
+            cartItems.map { if (it.id == cartId) it.copy(isSelected = !it.isSelected) else it }
         _cartItems.value = newCartItems
     }
 
     override fun onClickCheckAll() {
-        val currentCheckState = _isCheckAll.value ?: return
+        val currentCheckState = isCheckAll.value ?: return
         val toggledState = !currentCheckState
         _cartItems.value = _cartItems.value?.map { it.copy(isSelected = toggledState) }.orEmpty()
     }
@@ -154,18 +139,16 @@ class CartViewModel(
         }
 
         viewModelScope.launch {
-            decreaseProductQuantityUseCase(
-                id,
-            ).onSuccess { updateQuantity(id, -1) }
+            decreaseProductQuantityUseCase(id)
+                .onSuccess { updateQuantity(id, -1) }
                 .onFailure { _toastMessage.value = R.string.cart_toast_decrease_fail }
         }
     }
 
     override fun onClickPlus(id: Long) {
         viewModelScope.launch {
-            increaseProductQuantityUseCase(
-                id,
-            ).onSuccess { updateQuantity(id, 1) }
+            increaseProductQuantityUseCase(id)
+                .onSuccess { updateQuantity(id, 1) }
                 .onFailure { _toastMessage.value = R.string.cart_toast_increase_fail }
         }
     }
