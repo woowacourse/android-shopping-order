a7d397ead1daee108f4d08d1cafa0c35d532e03b
hwannow <vnfdlvgid061@naver.com>
2025-06-10 17:49:38 +0900

    refactor: ResponseAdapterFactory에 에러 메시지 추가

--- app/src/main/java/woowacourse/shopping/data/network/ResponseAdapterFactory.kt
+++ app/src/main/java/woowacourse/shopping/data/network/ResponseAdapterFactory.kt
@@ -13,14 +13,19 @@ class ResponseAdapterFactory : CallAdapter.Factory() {
         retrofit: Retrofit,
     ): CallAdapter<*, *>? {
         if (Call::class.java != getRawType(returnType)) return null
-        check(returnType is ParameterizedType)
+        check(returnType is ParameterizedType) { ERROR_RETURN_GENERIC_TYPE }
 
         val responseType = getParameterUpperBound(0, returnType)
         if (getRawType(responseType) != Result::class.java) return null
-        check(responseType is ParameterizedType)
+        check(responseType is ParameterizedType) { ERROR_RESULT_GENERIC_TYPE }
 
         val successType = getParameterUpperBound(0, responseType)
 
         return ResponseAdapter<Any>(successType)
+    }
+
+    companion object {
+        private const val ERROR_RETURN_GENERIC_TYPE = "반환 타입은 제네릭 타입이어야 합니다"
+        private const val ERROR_RESULT_GENERIC_TYPE = "Result는 제네릭 타입이어야 합니다"
     }
 }
