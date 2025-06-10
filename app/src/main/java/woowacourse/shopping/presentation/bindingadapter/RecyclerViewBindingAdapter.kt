ce6af1c57757df53bd1e5c96eecd9267ecaed25f
Ham BeomJoon <hbj0802@naver.com>
2025-06-10 15:07:31 +0900

    refactor: submitList 바인딩 어댑터 제네릭으로 묶기

--- app/src/main/java/woowacourse/shopping/presentation/bindingadapter/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/presentation/bindingadapter/RecyclerViewBindingAdapter.kt
@@ -1,43 +1,21 @@
 package woowacourse.shopping.presentation.bindingadapter
 
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.shopping.domain.model.Product
-import woowacourse.shopping.presentation.cart.CartAdapter
-import woowacourse.shopping.presentation.model.CartItemUiModel
-import woowacourse.shopping.presentation.model.CouponUiModel
-import woowacourse.shopping.presentation.order.CouponAdapter
-import woowacourse.shopping.presentation.product.RecentAdapter
 
-@BindingAdapter("coupons")
-fun bindCoupons(
+@BindingAdapter("submitList")
+fun <T> bindRecyclerViewItems(
     view: RecyclerView,
-    coupons: List<CouponUiModel>?,
+    items: List<T>?,
 ) {
     val adapter = view.adapter
-    if (adapter is CouponAdapter) {
-        adapter.submitList(coupons ?: emptyList())
-    }
-}
-
-@BindingAdapter("recentProducts")
-fun bindRecentProducts(
-    view: RecyclerView,
-    recentProducts: List<Product>?,
-) {
-    val adapter = view.adapter
-    if (adapter is RecentAdapter) {
-        adapter.submitList(recentProducts ?: emptyList())
-    }
-}
-
-@BindingAdapter("cartItems")
-fun bindCartItems(
-    view: RecyclerView,
-    cartItems: List<CartItemUiModel>?,
-) {
-    val adapter = view.adapter
-    if (adapter is CartAdapter) {
-        adapter.submitList(cartItems ?: emptyList())
+    if (adapter is ListAdapter<*, *>) {
+        try {
+            @Suppress("UNCHECKED_CAST")
+            (adapter as ListAdapter<T, *>).submitList(items ?: emptyList())
+        } catch (e: ClassCastException) {
+            throw Exception("타입이 맞지 않습니다.")
+        }
     }
 }
