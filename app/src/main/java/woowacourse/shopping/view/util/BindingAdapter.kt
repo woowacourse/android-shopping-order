f377e32c402c870980261dffbc1ddf1671c88e51
jiyunlee <02leejiyun@naver.com>
2025-06-10 13:45:26 +0900

    feat: 쿠폰 최소 주문 금액 표시

--- app/src/main/java/woowacourse/shopping/view/util/BindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/view/util/BindingAdapter.kt
@@ -5,6 +5,7 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import woowacourse.shopping.R
+import woowacourse.shopping.domain.model.Discount
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -25,4 +26,18 @@ fun TextView.setExpirationDate(date: LocalDate?) {
 
     val formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일")
     this.text = context.getString(R.string.expiration_date, date.format(formatter))
+}
+
+@BindingAdapter("minimumAmount")
+fun TextView.setMinimumAmount(discount: Discount?) {
+    discount ?: return
+    val amount =
+        when (discount) {
+            is Discount.FixedAmount -> discount.minimumAmount
+            is Discount.BuyXGetYFree -> null
+            is Discount.FreeShipping -> discount.minimumAmount
+            is Discount.Percentage -> null
+        }
+    amount ?: return
+    this.text = context.getString(R.string.minimum_amount, amount)
 }
