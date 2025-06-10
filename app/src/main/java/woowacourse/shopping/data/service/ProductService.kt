3d6140ebd58e08388d8da29eb24bf94146271cba
DongjooSEO <readyjoo24@gmail.com>
2025-06-10 15:36:55 +0900

    fix: 상품 요청 시 헤더 제거

--- app/src/main/java/woowacourse/shopping/data/service/ProductService.kt
+++ app/src/main/java/woowacourse/shopping/data/service/ProductService.kt
@@ -2,7 +2,6 @@ package woowacourse.shopping.data.service
 
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 import woowacourse.shopping.data.model.response.ProductResponse
@@ -11,7 +10,6 @@ import woowacourse.shopping.data.model.response.ProductsResponse
 interface ProductService {
     @GET("/products")
     suspend fun getProducts(
-        @Header("accept") accept: String = "*/*",
         @Query("page") page: Int = 0,
         @Query("size") size: Int = 20,
     ): Response<ProductsResponse>
