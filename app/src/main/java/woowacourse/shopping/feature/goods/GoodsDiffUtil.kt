17766648454693a672c3f33dee8f38a284298d97
devfeijoa <devfeijoa@gmail.com>
2025-06-12 02:09:52 +0900

    feat: GoodsDiffUtil 추가

---
+++ app/src/main/java/woowacourse/shopping/feature/goods/GoodsDiffUtil.kt
@@ -0,0 +1,30 @@
+package woowacourse.shopping.feature.goods
+
+import androidx.recyclerview.widget.DiffUtil
+import woowacourse.shopping.feature.goods.adapter.GoodsRvItems
+
+val GoodsDiffUtil =
+    object : DiffUtil.ItemCallback<GoodsRvItems>() {
+        override fun areItemsTheSame(
+            oldItem: GoodsRvItems,
+            newItem: GoodsRvItems,
+        ): Boolean {
+            if (oldItem::class != newItem::class) return false
+            when (oldItem) {
+                is GoodsRvItems.GoodsItem -> {
+                    if (newItem !is GoodsRvItems.GoodsItem) return false
+                    return oldItem.item.product.id == newItem.item.product.id
+                }
+                is GoodsRvItems.HistoryItem -> {
+                    if (newItem !is GoodsRvItems.HistoryItem) return false
+                    return oldItem.items.map { it.id } == newItem.items.map { it.id }
+                }
+                else -> return true
+            }
+        }
+
+        override fun areContentsTheSame(
+            oldItem: GoodsRvItems,
+            newItem: GoodsRvItems,
+        ): Boolean = oldItem == newItem
+    }
