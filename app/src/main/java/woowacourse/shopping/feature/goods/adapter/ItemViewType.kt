fe1ac451442d8dea5dfa1a4e74a477f24e0b516a
devfeijoa <devfeijoa@gmail.com>
2025-06-12 02:05:53 +0900

    refactor: 상품 목록 화면 구분선 추가

--- app/src/main/java/woowacourse/shopping/feature/goods/adapter/ItemViewType.kt
+++ app/src/main/java/woowacourse/shopping/feature/goods/adapter/ItemViewType.kt
@@ -4,8 +4,10 @@ enum class ItemViewType(
     val type: Int,
 ) {
     HISTORY(0),
-    GOODS(1),
-    LOAD_MORE(2),
+    DIVIDER(1),
+    GOODS(2),
+    LOAD_MORE(3),
+
     ;
 
     companion object {
