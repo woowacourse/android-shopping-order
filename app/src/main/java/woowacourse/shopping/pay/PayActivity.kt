6f829820b860cfeee41a97ff00671a3b7ea9a89d
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-06-13 16:47:47 +0900

    feat: 결제 화면 activity 구현

--- app/src/main/java/woowacourse/shopping/pay/PayActivity.kt
+++ app/src/main/java/woowacourse/shopping/pay/PayActivity.kt
@@ -3,13 +3,17 @@ package woowacourse.shopping.pay
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityPayBinding
+import woowacourse.shopping.product.catalog.ProductUiModel
+
 
 class PayActivity : AppCompatActivity() {
     private val binding: ActivityPayBinding by lazy {
@@ -18,11 +22,34 @@ class PayActivity : AppCompatActivity() {
             R.layout.activity_pay
         )
     }
+    private val viewModel: PayViewModel by lazy {
+        ViewModelProvider(
+            this,
+            PayViewModelFactory(),
+        )[PayViewModel::class.java]
+    }
+    private val adapter: CouponAdapter by lazy {
+        CouponAdapter(
+            coupons = viewModel.couponList.value.orEmpty(),
+            onCheckClick = { coupon, position -> viewModel.updateCoupon(coupon) }
+        )
+    }
+    private val orderProducts by lazy {
+        intent.getParcelableArrayListExtra<ProductUiModel>(KEY_PRODUCT_ORDER)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
         initActionBar()
+        initDataBinding()
+        observeData()
+        setOrderProducts()
+    }
+
+    private fun setOrderProducts() {
+        val products = intent.getParcelableArrayListExtra<ProductUiModel>(KEY_PRODUCT_ORDER)
+        viewModel.getOrderProducts(products?.toList().orEmpty())
     }
 
     private fun initActionBar() {
@@ -45,7 +72,26 @@ class PayActivity : AppCompatActivity() {
         }
     }
 
+    private fun observeData() {
+        viewModel.orderProducts.observe(this) {
+            viewModel.getOrderAmount()
+            viewModel.getTotalAmount()
+        }
+        viewModel.couponList.observe(this) { couponList -> }
+    }
+
+    private fun initDataBinding() {
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+        binding.recyclerViewPay.adapter = adapter
+    }
+
     companion object {
-        fun newIntent(context: Context): Intent = Intent(context, PayActivity::class.java)
+        private const val KEY_PRODUCT_ORDER = "orderProducts"
+
+        fun newIntent(context: Context, orderProducts: List<ProductUiModel>): Intent =
+            Intent(context, PayActivity::class.java).apply {
+                putParcelableArrayListExtra(KEY_PRODUCT_ORDER, ArrayList(orderProducts))
+            }
     }
 }
