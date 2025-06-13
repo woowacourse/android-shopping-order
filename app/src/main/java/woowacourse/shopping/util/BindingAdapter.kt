27a1982ac7b950319d6cfe1d9f4769c347348ddf
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-06-13 16:46:11 +0900

    feat: 쿠폰 날짜 보여주기 위한 bindingAdapter 구현

--- app/src/main/java/woowacourse/shopping/util/BindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/util/BindingAdapter.kt
@@ -1,10 +1,13 @@
 package woowacourse.shopping.util
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import woowacourse.shopping.R
 import woowacourse.shopping.product.catalog.ProductUiModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 object BindingAdapter {
     @JvmStatic
@@ -17,5 +20,18 @@ object BindingAdapter {
             .fallback(R.drawable.iced_americano)
             .error(R.drawable.iced_americano)
             .into(this)
+    }
+
+    private val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
+
+    @JvmStatic
+    @BindingAdapter("couponExpirationDate")
+    fun setCouponExpirationDate(textView: TextView, date: LocalDate?) {
+        date?.let {
+            val context = textView.context
+            val formattedDate = it.format(formatter)
+            val finalText = context.getString(R.string.coupon_expiration_date, formattedDate)
+            textView.text = finalText
+        }
     }
 }
