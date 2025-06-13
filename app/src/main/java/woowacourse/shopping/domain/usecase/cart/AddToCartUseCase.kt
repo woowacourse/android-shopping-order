7f02492ad236ca54ee8689609b09a1c6d93c1ca9
jiyunlee <02leejiyun@naver.com>
2025-06-13 15:56:41 +0900

    feat: 장바구니 상품 수량 유효성 검사 추가

--- app/src/main/java/woowacourse/shopping/domain/usecase/cart/AddToCartUseCase.kt
+++ app/src/main/java/woowacourse/shopping/domain/usecase/cart/AddToCartUseCase.kt
@@ -11,8 +11,11 @@ class AddToCartUseCase(
     suspend operator fun invoke(
         product: Product,
         quantityToAdd: Int,
-    ): Result<CartProduct?> =
-        getCartProductByProductId(product.id).fold(
+    ): Result<CartProduct?> {
+        if (quantityToAdd < MINIMUM_QUANTITY) {
+            return Result.failure(IllegalArgumentException(ERROR_INVALID_QUANTITY))
+        }
+        return getCartProductByProductId(product.id).fold(
             onSuccess = { cartProduct ->
                 if (cartProduct == null) {
                     cartProductRepository.insert(product.id, quantityToAdd).map { cartId ->
@@ -25,9 +28,15 @@ class AddToCartUseCase(
             },
             onFailure = { Result.failure(it) },
         )
+    }
 
     private suspend fun getCartProductByProductId(productId: Int): Result<CartProduct?> =
         cartProductRepository.getPagedProducts().map { pagedResult ->
             pagedResult.items.firstOrNull { it.product.id == productId }
         }
+
+    companion object {
+        private const val MINIMUM_QUANTITY = 0
+        private const val ERROR_INVALID_QUANTITY = "수량은 0 이상이어야 합니다."
+    }
 }
--- app/src/main/java/woowacourse/shopping/domain/usecase/cart/UpdateCartQuantityUseCase.kt
+++ app/src/main/java/woowacourse/shopping/domain/usecase/cart/UpdateCartQuantityUseCase.kt
@@ -12,10 +12,18 @@ class UpdateCartQuantityUseCase(
         newQuantity: Int,
     ): Result<CartProduct?> =
         when {
-            newQuantity == 0 -> removeFromCartUseCase(cartProduct).map { null }
+            newQuantity < MINIMUM_QUANTITY ->
+                Result.failure(IllegalArgumentException(ERROR_INVALID_QUANTITY))
+
+            newQuantity == MINIMUM_QUANTITY -> removeFromCartUseCase(cartProduct).map { null }
             else ->
                 cartProductRepository.updateQuantity(cartProduct, newQuantity).map {
                     cartProduct.copy(quantity = newQuantity)
                 }
         }
+
+    companion object {
+        private const val MINIMUM_QUANTITY = 0
+        private const val ERROR_INVALID_QUANTITY = "수량은 0 이상이어야 합니다."
+    }
 }
