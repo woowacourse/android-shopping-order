869c1ee23d570a844724fa3fb1c51094f5d118c5
parkhyomi <phyomi3@naver.com>
2026-05-10 02:29:58 +0900

    refactor: Product 관련 저장소 클래스 삭제 및 PreparedData의 모델 타입을 ShoppingItem으로 변경

--- app/src/main/java/woowacourse/shopping/repository/ProductRepository.kt
+++
@@ -1,9 +0,0 @@
-package woowacourse.shopping.repository
-
-import woowacourse.shopping.model.Product
-
-interface ProductRepository {
-    fun getProductOrNull(productId: Long): Product?
-
-    fun getProducts(): List<Product>
-}
