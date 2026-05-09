7061cc2764be74e015d62f613878ea66c1cdb78c
parkhyomi <phyomi3@naver.com>
2026-05-09 19:57:18 +0900

    feat: QuantityRepository 추가 및 ShoppingApplication에 등록

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -2,13 +2,16 @@ package woowacourse.shopping
 
 import android.app.Application
 import woowacourse.shopping.repository.MemoryProductRepository
+import woowacourse.shopping.repository.MemoryQuantityRepository
 import woowacourse.shopping.repository.MemoryShoppingCartRepository
 import woowacourse.shopping.repository.ProductRepository
+import woowacourse.shopping.repository.QuantityRepository
 import woowacourse.shopping.repository.ShoppingCartRepository
 
 class ShoppingApplication : Application() {
     companion object {
         val productRepository: ProductRepository = MemoryProductRepository(products = preparedProducts)
         val shoppingCartRepository: ShoppingCartRepository = MemoryShoppingCartRepository()
+        val quantityRepository: QuantityRepository = MemoryQuantityRepository(products = preparedProducts)
     }
 }
