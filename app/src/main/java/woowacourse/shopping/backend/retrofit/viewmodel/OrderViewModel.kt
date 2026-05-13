a366f3ecb5682a18697b87d6a8742df3cd20e828
parkhyomi <phyomi3@naver.com>
2026-05-13 18:31:16 +0900

    feat: Retrofit 기반의 ViewModel 및 Factory 구현

---
+++ app/src/main/java/woowacourse/shopping/backend/retrofit/viewmodel/OrderViewModel.kt
@@ -0,0 +1,23 @@
+package woowacourse.shopping.backend.retrofit.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import woowacourse.shopping.backend.retrofit.awaitBody
+import woowacourse.shopping.backend.retrofit.dto.OrderInfo
+import woowacourse.shopping.backend.retrofit.repository.OrderRetrofitRepository
+
+class OrderViewModel(
+    private val orderRetrofitRepository: OrderRetrofitRepository,
+) : ViewModel() {
+    fun order(orderInfo: OrderInfo) {
+        viewModelScope.launch {
+            runCatching {
+                orderRetrofitRepository
+                    .order(
+                        order = orderInfo,
+                    ).awaitBody(errorPrefix = "주문 실패")
+            }
+        }
+    }
+}
