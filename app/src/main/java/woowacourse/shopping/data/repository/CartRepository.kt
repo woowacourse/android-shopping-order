ec0bcac888ec2c4bae8e3f51694088b8ded2569c
gykim_kr <k_bright_st@pusan.ac.kr>
2026-05-13 23:48:03 +0900

    refactor: 장바구니 repository를 인터페이스로 변경

--- app/src/main/java/woowacourse/shopping/data/repository/CartRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/repository/CartRepository.kt
@@ -1,54 +1,21 @@
 package woowacourse.shopping.data.repository
 
-import kotlinx.coroutines.flow.Flow
-import woowacourse.shopping.data.localdb.dao.CartItemDao
-import woowacourse.shopping.data.localdb.entity.CartItemEntity
+import woowacourse.shopping.model.CartItem
 
-class CartRepository(
-    private val cartItemDao: CartItemDao,
-) {
-    fun observeCartItems(): Flow<List<CartItemEntity>> = cartItemDao.getAll()
+interface CartRepository {
+    suspend fun getCartItemsByPage(
+        page: Int,
+        size: Int,
+    ): List<CartItem>
 
-    suspend fun setQuantity(
-        id: String,
+    suspend fun setCartItem(
+        productId: String,
         quantity: Int,
-    ) {
-        require(quantity >= 0) { "Quantity must be 0 or greater." }
+    )
 
-        if (quantity == 0) {
-            cartItemDao.deleteById(id)
-            return
-        }
+    suspend fun deleteItem(cartItemId: String)
 
-        val cartItem =
-            CartItemEntity(
-                id,
-                quantity,
-                System.currentTimeMillis(),
-            )
-        cartItemDao.insert(cartItem)
-    }
+    suspend fun getCartItemQuantity(cartItemId: String): Int?
 
-    suspend fun updateQuantity(
-        id: String,
-        quantity: Int,
-    ) {
-        require(quantity >= 0) { "수량은 0 이상이어야 합니다." }
-
-        if (quantity == 0) {
-            cartItemDao.deleteById(id)
-            return
-        }
-
-        val cartItem = cartItemDao.findById(id) ?: return
-        cartItemDao.insert(cartItem.copy(quantity = quantity))
-    }
-
-    suspend fun deleteItem(id: String) {
-        cartItemDao.deleteById(id)
-    }
-
-    suspend fun getCartItemQuantity(id: String): Int? = cartItemDao.findById(id)?.quantity
-
-    suspend fun getCartSize(): Int = cartItemDao.getTotalCount()
+    suspend fun getTotalCartItemQuantity(): Int
 }
