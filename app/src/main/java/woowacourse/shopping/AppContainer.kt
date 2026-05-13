60445735eb3f349f0ed21ad0357a34dfefbbfb1a
gykim_kr <k_bright_st@pusan.ac.kr>
2026-05-13 23:49:39 +0900

    feat: 원격 API 클라이언트와 장바구니 repository 연결

--- app/src/main/java/woowacourse/shopping/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/AppContainer.kt
@@ -2,18 +2,19 @@ package woowacourse.shopping
 
 import android.content.Context
 import kotlinx.serialization.json.Json
+import okhttp3.Credentials
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import woowacourse.shopping.data.localdb.ShoppingDB
-import woowacourse.shopping.data.remote.HttpProductServer
 import woowacourse.shopping.data.remote.NetworkManager
 import woowacourse.shopping.data.remote.NetworkObserver
+import woowacourse.shopping.data.remote.api.CartApi
 import woowacourse.shopping.data.remote.api.ProductApi
 import woowacourse.shopping.data.repository.CartRepository
-import woowacourse.shopping.data.repository.HttpProductRepository
+import woowacourse.shopping.data.repository.CartRepositoryImpl
 import woowacourse.shopping.data.repository.ProductRepository
 import woowacourse.shopping.data.repository.ProductRepositoryImpl
 import woowacourse.shopping.data.repository.RecentItemRepository
@@ -37,12 +38,12 @@ class AppContainer(
 
     val productRepository: ProductRepository by lazy {
         ProductRepositoryImpl(
-            api = retrofitService.create(ProductApi::class.java)
+            api = retrofitService.create(ProductApi::class.java),
         )
     }
 
     val cartRepository: CartRepository by lazy {
-        CartRepository(database.cartItemDao())
+        CartRepositoryImpl(api = retrofitService.create(CartApi::class.java))
     }
 
     val recentItemRepository: RecentItemRepository by lazy {
@@ -56,20 +57,32 @@ class AppContainer(
     private val loggingInterceptor =
         HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
+            redactHeader("Authorization")
         }
 
     private val client =
-        OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
+        OkHttpClient
+            .Builder()
+            .addInterceptor { chain ->
+                val request =
+                    chain
+                        .request()
+                        .newBuilder()
+                        .header("Authorization", Credentials.basic("byunghyunkim0", "password"))
+                        .build()
+
+                chain.proceed(request)
+            }.addInterceptor(loggingInterceptor)
             .build()
 
-    val retrofitService = Retrofit.Builder()
-        .baseUrl("http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com/")
-        .addConverterFactory(
-            Json.asConverterFactory(
-                "application/json".toMediaType(),
-            )
-        )
-        .client(client)
-        .build()
+    val retrofitService =
+        Retrofit
+            .Builder()
+            .baseUrl("http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com/")
+            .addConverterFactory(
+                Json.asConverterFactory(
+                    "application/json".toMediaType(),
+                ),
+            ).client(client)
+            .build()
 }
