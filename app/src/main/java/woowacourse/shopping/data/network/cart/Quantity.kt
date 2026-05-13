1e23546a3b6c9228e44cac1b2b6abde966f81a38
ujeong <qnf323@naver.com>
2026-05-13 14:50:09 +0900

    refactor: @Serializable 어노테이션 추가

--- app/src/main/java/woowacourse/shopping/data/network/cart/Quantity.kt
+++ app/src/main/java/woowacourse/shopping/data/network/cart/Quantity.kt
@@ -1,5 +1,10 @@
 package woowacourse.shopping.data.network.cart
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Quantity(
+    @SerialName("quantity")
     val quantity: Int,
 )
