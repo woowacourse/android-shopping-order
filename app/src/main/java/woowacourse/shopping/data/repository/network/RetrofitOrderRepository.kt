a6ada180df1912625d5b11875d2717e7fcddbac0
todays-sun-day <178suna@gmail.com>
2026-05-14 15:15:56 +0900

    feat: 주문 API 연동 및 OrderRepository 구현

---
+++ app/src/main/java/woowacourse/shopping/data/repository/network/RetrofitOrderRepository.kt
@@ -0,0 +1,18 @@
+package woowacourse.shopping.data.repository.network
+
+import woowacourse.shopping.data.remote.auth.BasicAuthEncoder
+import woowacourse.shopping.data.remote.dto.OrderRequest
+import woowacourse.shopping.data.remote.service.OrderService
+import woowacourse.shopping.data.repository.OrderRepository
+
+class RetrofitOrderRepository(
+    private val encoder: BasicAuthEncoder,
+    private val service: OrderService
+) : OrderRepository {
+    override suspend fun requestOrder(ids: List<Long>) {
+        service.requestOrder(
+            auth = encoder.getHeader(),
+            request = OrderRequest(ids)
+        )
+    }
+}
