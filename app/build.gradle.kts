b2c6ad4dad3b2790ada4249d675bb45918018c20
chohs4164 <josh100as@gmail.com>
2026-05-14 10:59:54 +0900

    docs: 1, 2단계 기능 요구 사항 추가 및 DataStore Core 의존성 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.datastore.core)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.kotest.runner.junit5)
     androidTestImplementation(libs.androidx.espresso.core)
