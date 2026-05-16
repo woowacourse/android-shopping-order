2da81d979f328bafedb372186a89d7f2ee00c347
ujeong <qnf323@naver.com>
2026-05-16 18:06:11 +0900

    test: 추천 상품은 최대 10개까지 노출된다 테스트

--- app/src/test/java/woowacourse/shopping/feature/recommend/RecommendViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/feature/recommend/RecommendViewModelTest.kt
@@ -19,8 +19,8 @@ class RecommendViewModelTest {
     private val comnProducts = TestProductFixture.products(35)
     private val comnCartContents = TestCartContentFixture.cartContentsOf(comnProducts)
 
-    private val beverageProducts = TestProductFixture.products(5, category = "음료")
-    private val clothesProducts = TestProductFixture.products(5, category = "의류", startIndex = 6)
+    private val beverageProducts = TestProductFixture.products(15, category = "음료")
+    private val clothesProducts = TestProductFixture.products(15, category = "의류", startIndex = 16)
 
     @Test
     fun `최근에 본 상품이 없으면 상품 목록을 불러온다`() {
@@ -57,5 +57,24 @@ class RecommendViewModelTest {
         // then:  의류 카테고리에 속하는 상품만 추천 목록에 노출된다
         val expectedSize = viewModel.uiState.value.recommendList.all { it.category == clothesProducts.first().category }
         assertThat(expectedSize).isEqualTo(true)
+    }
+
+    @Test
+    fun `추천 상품은 최대 10개까지 노출된다`() {
+        // given: 해당 카테고리에 10 개를 초과하는 상품이 존재한다
+        viewModel = RecommendViewModel(
+            productRepository = FakeProductRepository(
+                initial = beverageProducts + clothesProducts,
+            ),
+            cartRepository = FakeCartRepository(productCatalog = beverageProducts + clothesProducts),
+            orderRepository = FakeOrderRepository(),
+            recentProductRepository = FakeRecentProductRepository(initial = listOf(clothesProducts.first().id)),
+        )
+        // when:  초기 로딩을 호출할 때
+        viewModel.initialLoading()
+
+        // then:  추천 목록의 사이즈가 10 이다
+        val expected = viewModel.uiState.value.recommendList
+        assertThat(expected).hasSize(10)
     }
 }
