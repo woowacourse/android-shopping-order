f244bae7005b5c244d7d31485bb6976a5ff14b34
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-05-17 15:04:04 +0900

    refactor: 유효하지 않은 import 제거

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -6,7 +6,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.room.Room
-import kotlin.jvm.java
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import okhttp3.Credentials
