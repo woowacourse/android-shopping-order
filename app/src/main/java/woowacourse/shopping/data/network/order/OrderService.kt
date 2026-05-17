cf68a97c7e2ebe57f2e015a573fda9d4b55c4637
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-05-17 17:17:20 +0900

    feat: 주문 API 명세를 GET에서 POST로 변경

--- app/src/main/java/woowacourse/shopping/data/network/order/OrderService.kt
+++ app/src/main/java/woowacourse/shopping/data/network/order/OrderService.kt
@@ -4,10 +4,11 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import woowacourse.shopping.data.network.product.dto.ProductResponse
 
 interface OrderService {
-    @GET("/orders")
+    @POST("/orders")
     fun orders(
         @Header("accept")
         accept: String = "*/*",
