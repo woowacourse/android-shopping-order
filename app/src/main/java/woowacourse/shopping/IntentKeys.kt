a9f00cbd6c8f45422026ea9ab395cf328a844a2b
first-woosun <dbdntjs620@gmail.com>
2026-05-18 14:57:34 +0900

    refactor: 상태 주입을 위한 intentKey 추가

--- app/src/main/java/woowacourse/shopping/IntentKeys.kt
+++ app/src/main/java/woowacourse/shopping/IntentKeys.kt
@@ -3,4 +3,6 @@ package woowacourse.shopping
 object IntentKeys {
     const val SELECTED_PRODUCT_ID_KEY = "selected_product"
     const val LATEST_VIEWED_PRODUCT_ID_KEY = "latest_viewed_product"
+    const val SELECTED_CART_ITEM_IDS = "selected_cart_item_ids"
+    const val SELECTED_TOTAL_PRICE = "selected_total_price"
 }
