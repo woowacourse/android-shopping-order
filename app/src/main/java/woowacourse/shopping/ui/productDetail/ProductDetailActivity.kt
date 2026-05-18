72f948e35ac0fc40d4e4682431296ee1fb431940
baekcii <bsy8470@naver.com>
2026-05-18 17:53:17 +0900

    fix: viewModel 오타 수정

--- app/src/main/java/woowacourse/shopping/ui/productDetail/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/productDetail/ProductDetailActivity.kt
@@ -23,7 +23,7 @@ class ProductDetailActivity : ComponentActivity() {
             val viewModel: ProductDetailViewModel =
                 viewModel(
                     factory =
-                        ProductListViewModel.Factory,
+                        ProductDetailViewModel.Factory,
                 )
             Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                 ProductDetailScreen(
