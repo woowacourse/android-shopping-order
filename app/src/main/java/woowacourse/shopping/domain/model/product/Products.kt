02f5021509d0ae5ea1dd49da3531870d7e7e88c5
katie0109 <0109katie@naver.com>
2026-05-19 00:26:50 +0900

    refactor: Products 내 페이징 로직 제거 및 카테고리별 상품 노출 개수 제한(10개)

--- app/src/main/java/woowacourse/shopping/domain/model/product/Products.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/product/Products.kt
@@ -4,20 +4,5 @@ class Products(
     val items: List<Product>,
     val isLast: Boolean,
 ) {
-    fun getCategoryProducts(category: String): List<Product> = items.filter { it.category.value == category }
-
-    // TODO: 제거
-    fun getPage(
-        page: Int,
-        pageSize: Int,
-    ): List<Product> {
-        require(page >= 0) { "page는 0 이상이어야 합니다. page=$page" }
-        require(pageSize > 0) { "pageSize는 1 이상이어야 합니다. pageSize=$pageSize" }
-
-        val fromIndex = page * pageSize
-        if (fromIndex >= items.size) return emptyList()
-
-        val toIndex = minOf(fromIndex + pageSize, items.size)
-        return items.subList(fromIndex, toIndex)
-    }
+    fun getCategoryProducts(category: String): List<Product> = items.filter { it.category.value == category }.take(10)
 }
