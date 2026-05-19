b5c613c7fe8b9bfffca9c89751708b17ba3f3864
katie0109 <0109katie@naver.com>
2026-05-19 21:01:45 +0900

    feat: 디버그 환경을 위한 로컬 Mock 서버 구축 (서버 터짐 이슈)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,13 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField(
+                "String",
+                "BASE_URL",
+                "\"http://127.0.0.1:8080/\"",
+            )
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
