796b2a286e1ff3b370b0edb02763647cb572e17a
aksworns22 <wkdeogks17@gmail.com>
2026-05-19 23:50:59 +0900

    chore: URL 주소를 원래 서버 주소로 복구

--- app/src/main/java/woowacourse/shopping/di/RepositoryProvider.kt
+++ app/src/main/java/woowacourse/shopping/di/RepositoryProvider.kt
@@ -40,7 +40,7 @@ object RepositoryProvider {
             authDataSource.saveToken(id, password)
             retrofitServices =
                 RetrofitServices(
-                    baseUrl = "http://192.168.2.152:3000",
+                    baseUrl = "http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com/",
                     interceptor = AuthInterceptor(authDataSource.getToken()),
                 )
         }
