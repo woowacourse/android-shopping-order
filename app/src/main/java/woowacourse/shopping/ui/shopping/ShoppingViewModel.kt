8b401aad72ec4e4f4a5270a1fa86a717c5b4e67d
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-05-20 21:15:50 +0900

    fix: 더보기 클릭 시 새로운 데이터부터 스크롤되도록 수정

--- app/src/main/java/woowacourse/shopping/ui/shopping/ShoppingViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/shopping/ShoppingViewModel.kt
@@ -214,8 +214,40 @@ class ShoppingViewModel(
         val contentState = _uiState.value.productListState as? ProductListUiState.Content ?: return
         if (!contentState.hasNext) return
 
-        visibleCount = minOf(visibleCount + PAGE_SIZE, productRepository.size)
-        loadProducts()
+        viewModelScope.launch {
+            productRepository
+                .getProducts(contentState.products.size, PAGE_SIZE)
+                .onSuccess { newProducts ->
+                    val hasNext =
+                        productRepository
+                            .hasNext(
+                                contentState.products.size + newProducts.count() - 1,
+                            ).getOrDefault(false)
+
+                    val newCartState =
+                        createCartState(newProducts.toList())
+                            .getOrElse {
+                                updateErrorState(it)
+                                return@onSuccess
+                            }
+
+                    _uiState.update { currentState ->
+                        val latestContent =
+                            currentState.productListState as? ProductListUiState.Content
+                                ?: return@update currentState
+                        currentState.copy(
+                            productListState =
+                                latestContent.copy(
+                                    products = currentState.productListState.products + newCartState.products,
+                                    hasNext = hasNext,
+                                ),
+                            cartQuantity = currentState.cartQuantity + newCartState.cartQuantity,
+                        )
+                    }
+                }.onFailure { throwable ->
+                    updateErrorState(throwable)
+                }
+        }
     }
 
     fun addToCart(productId: Long) = increaseQuantity(productId)
