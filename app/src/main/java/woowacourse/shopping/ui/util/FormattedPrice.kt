e706239ddeea006a94fbc4698088bff6c6cfb9eb
cksckckcks <chan679999@naver.com>
2026-05-20 20:52:05 +0900

    refactor: 0원일 경우에도 정상적으로 출력되도록 수정

--- app/src/main/java/woowacourse/shopping/ui/util/FormattedPrice.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/FormattedPrice.kt
@@ -3,6 +3,6 @@ package woowacourse.shopping.ui.util
 import java.text.DecimalFormat
 
 fun formattedPrice(value: Long): String {
-    val decimal = DecimalFormat("#,###원")
+    val decimal = DecimalFormat("#,##0원")
     return decimal.format(value)
 }
