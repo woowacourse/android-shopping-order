1eff31234bd876b2a16aa6c03e5d6fd1e6c91f29
first-woosun <dbdntjs620@gmail.com>
2026-05-21 11:07:56 +0900

    feat: ProductDetailUiState 추가

---
+++ app/src/main/java/woowacourse/shopping/ui/state/ProductDetailUIState.kt
@@ -0,0 +1,9 @@
+package woowacourse.shopping.ui.state
+
+import woowacourse.shopping.domain.Product
+
+data class ProductDetailUIState(
+    val product: Product? = null,
+    val lastViewProduct: Product? = null,
+    val count: Int = 1
+)
