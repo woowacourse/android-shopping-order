97d71f805fcfbab99361267268bfdd2db3ecab30
first-woosun <dbdntjs620@gmail.com>
2026-05-21 17:35:11 +0900

    feat: wrapper 함수 적용 및 viewModel 주입

--- app/src/main/java/woowacourse/shopping/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.launch
+import woowacourse.shopping.ui.component.route.ShoppingRoute
 import woowacourse.shopping.ui.component.screen.CatalogScreen
 import woowacourse.shopping.ui.theme.AndroidshoppingTheme
 import woowacourse.shopping.ui.viewmodel.ShoppingViewModel
@@ -37,49 +38,22 @@ class MainActivity : ComponentActivity() {
                             (application as ShoppingApplication).productRepository,
                         ),
                 )
-            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-            val lastViewedProductId by viewModel.lastViewProductId.collectAsStateWithLifecycle()
 
             AndroidshoppingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CatalogScreen(
-                        catalog = uiState.products,
-                        recentlyViewedProducts = uiState.recentlyViewedProducts,
-                        onRecentlyViewedClick = { product ->
-                            viewModel.updateHistory(product)
+                    ShoppingRoute(
+                        viewModel = viewModel,
+                        onNavigateToProductDetail = { productId, lastViewedId ->
                             ProductDetailActivity.startActivity(
                                 context = this,
-                                selectedProductId = product.id,
-                                lastViewedProductId = lastViewedProductId
+                                selectedProductId = productId,
+                                lastViewedProductId = lastViewedId
                             )
                         },
-                        onItemClick = { product ->
-                            viewModel.updateHistory(product)
-                            ProductDetailActivity.startActivity(
-                                context = this,
-                                selectedProductId = product.id,
-                                lastViewedProductId = lastViewedProductId
-                            )
-                        },
-                        onCartClick = {
+                        onNavigateToCart = {
                             CartActivity.startActivity(this)
                         },
-                        onLoadClick = {
-                            viewModel.loadMore()
-                        },
-                        modifier = Modifier.padding(innerPadding),
-                        onAdd = { id, updateAmount ->
-                            viewModel.updateCountWithID(id, updateAmount)
-                        },
-                        onMinus = { id, updateAmount ->
-                            viewModel.updateCountWithID(id, updateAmount)
-                        },
-                        onDelete = { viewModel.removeWithID(it) },
-                        onAddInCart = { viewModel.addToCart(it) },
-                        isContainedInCart = { uiState.cart.isContain(it) },
-                        specificProductCount = { uiState.cart.totalCountOfSpecificPurchaseProduct(it) },
-                        totalCount = uiState.totalCartCount(),
-                        isLoading = uiState.isLoading,
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
