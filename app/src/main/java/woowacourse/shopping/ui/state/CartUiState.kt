9b1b083a01071698282975294070aa687f4de743
first-woosun <dbdntjs620@gmail.com>
2026-05-21 09:50:44 +0900

    feat: cartUiState 추가

--- app/src/main/java/woowacourse/shopping/ui/state/CartUiState.kt
+++ app/src/main/java/woowacourse/shopping/ui/state/CartUiState.kt
@@ -4,12 +4,9 @@ import woowacourse.shopping.domain.PurchaseProducts
 
 data class CartUiState(
     val items: PurchaseProducts = PurchaseProducts(),
-    val allItems: PurchaseProducts = PurchaseProducts(),
-    val totalCount: Int = 0,
     val currentPage: Int = 0,
     val checkedItemIds: List<Long> = emptyList(),
     val totalPrice: Int = 0,
-    val isAllChecked: Boolean = false,
     val isLoading: Boolean = false,
     val isNextEnable: Boolean = false,
     val isPrevEnable: Boolean = false,
