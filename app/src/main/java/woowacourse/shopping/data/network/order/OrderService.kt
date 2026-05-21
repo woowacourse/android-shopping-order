8b20066df2ccb787bd231fdc864010bcd269b1eb
ujeong <qnf323@naver.com>
2026-05-21 19:23:22 +0900

    refactor: 서버 스펙에 맞게 orders 호출 수정

--- app/src/main/java/woowacourse/shopping/data/network/order/OrderService.kt
+++ app/src/main/java/woowacourse/shopping/data/network/order/OrderService.kt
@@ -3,11 +3,9 @@ package woowacourse.shopping.data.network.order
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import woowacourse.shopping.data.network.order.dto.OrderRequest
 
 interface OrderService {
     @POST("/orders")
-    suspend fun orders(
-        @Body
-        cartItemIds: List<String>,
-    ): Response<Unit>
+    suspend fun orders(@Body request: OrderRequest): Response<Unit>
 }
