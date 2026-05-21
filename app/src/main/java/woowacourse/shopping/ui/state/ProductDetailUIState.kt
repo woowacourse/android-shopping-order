a9745aa3f0f451e11187eaaefa0fd183aabfa553
first-woosun <dbdntjs620@gmail.com>
2026-05-21 17:56:12 +0900

    feat: errorMsg 파라미터 추가

--- app/src/main/java/woowacourse/shopping/ui/state/ProductDetailUIState.kt
+++ app/src/main/java/woowacourse/shopping/ui/state/ProductDetailUIState.kt
@@ -5,5 +5,6 @@ import woowacourse.shopping.domain.Product
 data class ProductDetailUIState(
     val product: Product? = null,
     val lastViewProduct: Product? = null,
-    val count: Int = 1
+    val count: Int = 1,
+    val errorMsg: String? = null,
 )
