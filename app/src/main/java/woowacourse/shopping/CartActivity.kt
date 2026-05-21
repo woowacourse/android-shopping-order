08cd4a0fcf546baafd8574ffa2f9e5a572c06010
first-woosun <dbdntjs620@gmail.com>
2026-05-21 19:26:13 +0900

    feat: wrapper 컴포저블 적용

--- app/src/main/java/woowacourse/shopping/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/CartActivity.kt
@@ -9,11 +9,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import woowacourse.shopping.ui.component.screen.CartScreen
+import woowacourse.shopping.ui.component.route.CartRoute
+import woowacourse.shopping.ui.theme.AndroidshoppingTheme
 import woowacourse.shopping.ui.viewmodel.CartViewModel
 import woowacourse.shopping.ui.viewmodel.CartViewModelFactory
 
@@ -33,54 +32,23 @@ class CartActivity : ComponentActivity() {
                         ),
                 )
 
-            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
-            Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
-                CartScreen(
-                    cart = uiState.items,
-                    onClose = {
-                        finish()
-                    },
-                    onAdd = { id, updateAmount ->
-                        viewModel.updateCountWithID(id, updateAmount)
-                    },
-                    onMinus = { id, updateAmount ->
-                        viewModel.updateCountWithID(id, updateAmount)
-                    },
-                    onDelete = { id ->
-                        viewModel.removeWithID(id)
-                    },
-                    currentPage = uiState.currentPage,
-                    onPrevious = {
-                        viewModel.prev()
-                    },
-                    onNext = {
-                        viewModel.next()
-                    },
-                    previousEnable = uiState.isPrevEnable,
-                    nextEnable = uiState.isNextEnable,
-                    isPageable = uiState.isPageable,
-                    isLoading = uiState.isLoading,
-                    onCheckedChanged = { viewModel.onItemChecked(it) },
-                    totalPrice = uiState.totalPrice,
-                    totalCount = uiState.checkedItemIds.size,
-                    isChecked = { id -> uiState.checkedItemIds.contains(id) },
-                    onSelectAllClick = { viewModel.onSelectAllClick() },
-                    onOrderClick = {
-                        if (uiState.checkedItemIds.isNotEmpty()) {
-                            RecommendationActivity.startActivity(
-                                context = this,
-                                totalPrice = uiState.totalPrice,
-                                checkedIds =  uiState.checkedItemIds,
-                            )
-                        }
-                    },
-                    isAllChecked = viewModel.isAllChecked(),
-                    modifier =
-                        Modifier
-                            .fillMaxSize()
-                            .padding(paddingValues),
-                )
+            AndroidshoppingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CartRoute(
+                        viewModel = viewModel,
+                        onClose = { finish() },
+                        onOrderClick = { totalPrice, checkedIds ->
+                            if (checkedIds.isNotEmpty()){
+                                RecommendationActivity.startActivity(
+                                    context = this,
+                                    totalPrice = totalPrice,
+                                    checkedIds = checkedIds
+                                )
+                            }
+                        },
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
             }
         }
     }
