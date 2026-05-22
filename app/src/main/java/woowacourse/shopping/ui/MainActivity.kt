3621faf3f20a46d789f751da3f0e56af2de2e8cb
cksckckcks <chan679999@naver.com>
2026-05-22 19:46:59 +0900

    refactor: 뷰모델 관련 코드 제거

--- app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import woowacourse.shopping.ui.nav.ShoppingNavHost
-import woowacourse.shopping.ui.shopping.ShoppingViewModel
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: ShoppingViewModel by viewModels { ShoppingViewModel.Companion.Factory }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,10 +14,5 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingNavHost()
         }
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        viewModel.loadProducts()
     }
 }
