8edb5b45721c3e1a03110df490c6f9f47e654e72
joon0447 <joon0447@naver.com>
2026-05-23 00:37:11 +0900

    feat: 쿠폰 도메인 모델 및 할인 로직 구현

---
+++ app/src/main/java/woowacourse/shopping/domain/model/AvailableTime.kt
@@ -0,0 +1,10 @@
+package woowacourse.shopping.domain.model
+
+import java.time.LocalTime
+
+data class AvailableTime(
+    val start: LocalTime,
+    val end: LocalTime,
+) {
+    fun contains(time: LocalTime): Boolean = time in start..end
+}
--- app/src/main/java/woowacourse/shopping/domain/model/Money.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Money.kt
@@ -3,10 +3,12 @@ package woowacourse.shopping.domain.model
 @JvmInline
 value class Money(
     val amount: Long,
-) {
+) : Comparable<Money> {
     init {
         require(amount >= 0) { "금액은 0원 이상이어야 합니다." }
     }
 
     operator fun times(time: Int): Money = Money(this.amount * time)
+
+    override fun compareTo(other: Money): Int = amount.compareTo(other.amount)
 }
