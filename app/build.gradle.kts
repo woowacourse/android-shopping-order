ed9ccb472edb77c73a37781d729629a15c4a7246
parkhyomi <phyomi3@naver.com>
2026-05-22 13:21:33 +0900

    build: Gson converter를 kotlinx-serialization으로          전환

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
     id("org.jetbrains.kotlin.kapt")
 }
 
@@ -139,7 +140,6 @@ dependencies {
     implementation(libs.logging.interceptor)
     implementation(libs.mockwebserver)
     implementation(libs.retrofit)
-    implementation(libs.converter.gson)
 
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit.kotlinx.serialization.converter)
