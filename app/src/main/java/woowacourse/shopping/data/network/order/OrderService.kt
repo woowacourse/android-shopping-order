fbc126a956f4dda4ed0c525e6f990524368c2a91
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-05-22 21:43:52 +0900

    refactor: OrderService의 반환 타입을 Call에서 Response<Unit>으로 변경

--- app/src/main/java/woowacourse/shopping/data/network/order/OrderService.kt
+++ app/src/main/java/woowacourse/shopping/data/network/order/OrderService.kt
@@ -1,10 +1,9 @@
 package woowacourse.shopping.data.network.order
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
-import woowacourse.shopping.data.network.product.dto.ProductResponse
 
 interface OrderService {
     @POST("/orders")
@@ -13,5 +12,5 @@ interface OrderService {
         accept: String = "*/*",
         @Body
         cartItemIds: List<Long>,
-    ): ProductResponse
+    ): Response<Unit>
 }
