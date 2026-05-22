8d9201ac75e9fa68bcca4801863ae9974e9f32b3
parkhyomi <phyomi3@naver.com>
2026-05-22 13:43:44 +0900

    refactor: Retrofit 인터페이스에서 불필요한 Header 및 Unit 반환 타입 제거

--- app/src/main/java/woowacourse/shopping/data/remote/retrofit/api/ProductRetrofitInterface.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/retrofit/api/ProductRetrofitInterface.kt
@@ -3,7 +3,6 @@ package woowacourse.shopping.data.remote.retrofit.api
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,7 +12,6 @@ import woowacourse.shopping.data.remote.retrofit.dto.ProductResponse
 interface ProductRetrofitInterface {
     @GET("/products")
     suspend fun requestProducts(
-        @Header("Accept") accept: String = "*/*",
         @Query("page") page: Int,
         @Query("size") size: Int,
         @Query("sort") sort: List<String>? = null,
@@ -22,19 +20,16 @@ interface ProductRetrofitInterface {
 
     @GET("/products/{id}")
     suspend fun requestProductDetail(
-        @Header("Accept") accept: String = "*/*",
         @Path("id") id: Long,
     ): ProductDto
 
     @POST("/products")
     suspend fun addProduct(
-        @Header("Accept") accept: String = "*/*",
         @Body product: ProductDto,
-    ): Unit
+    )
 
     @DELETE("/products/{id}")
     suspend fun deleteProduct(
-        @Header("Accept") accept: String = "*/*",
         @Path("id") id: Long,
-    ): Unit
+    )
 }
--- app/src/main/java/woowacourse/shopping/data/remote/retrofit/api/ShoppingCartRetrofitInterface.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/retrofit/api/ShoppingCartRetrofitInterface.kt
@@ -3,7 +3,6 @@ package woowacourse.shopping.data.remote.retrofit.api
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -15,7 +14,6 @@ import woowacourse.shopping.data.remote.retrofit.dto.ShoppingCartResponse
 interface ShoppingCartRetrofitInterface {
     @GET("/cart-items")
     suspend fun requestCartItems(
-        @Header("Accept") accept: String = "*/*",
         @Query("page") page: Int,
         @Query("size") size: Int,
         @Query("sort") sort: List<String>? = null,
@@ -23,20 +21,17 @@ interface ShoppingCartRetrofitInterface {
 
     @POST("/cart-items")
     suspend fun addCartItem(
-        @Header("Accept") accept: String = "*/*",
         @Body product: CartRequest,
-    ): Unit
+    )
 
     @DELETE("/cart-items/{id}")
     suspend fun deleteCartItem(
-        @Header("Accept") accept: String = "*/*",
         @Path("id") id: Int,
-    ): Unit
+    )
 
     @PATCH("/cart-items/{id}")
     suspend fun updateQuantityCartItem(
-        @Header("Accept") accept: String = "*/*",
         @Path("id") id: Int,
         @Body product: CartQuantity,
-    ): Unit
+    )
 }
