fc6a4a6aa51f66548ed52869d417de8301bb7600
cksckckcks <chan679999@naver.com>
2026-05-22 19:46:47 +0900

    refactor: MainActivity로 이름 변경 및 패키지 수정

--- app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package woowacourse.shopping.ui.shopping
+package woowacourse.shopping.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,9 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import woowacourse.shopping.ui.nav.ShoppingNavHost
+import woowacourse.shopping.ui.shopping.ShoppingViewModel
 
-class ShoppingActivity : ComponentActivity() {
-    private val viewModel: ShoppingViewModel by viewModels { ShoppingViewModel.Factory }
+class MainActivity : ComponentActivity() {
+    private val viewModel: ShoppingViewModel by viewModels { ShoppingViewModel.Companion.Factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
