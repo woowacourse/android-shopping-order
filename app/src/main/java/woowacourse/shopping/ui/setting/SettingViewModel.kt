efb6f57b555098eeaecb4ffad0703ce5cbd3fb58
nadajinny <jinsun23007@gmail.com>
2026-05-23 01:49:11 +0900

    feat: Setting 화면 기능 로직 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/setting/SettingViewModel.kt
@@ -0,0 +1,29 @@
+package woowacourse.shopping.ui.setting
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import woowacourse.shopping.repository.NotificationSettingRepository
+import woowacourse.shopping.repository.ShoppingRepositoryProvider
+
+class SettingViewModel(
+    private val notificationSettingRepository: NotificationSettingRepository =
+        ShoppingRepositoryProvider.notificationSettingRepository,
+) : ViewModel() {
+    private val _uiState =
+        MutableStateFlow(
+            SettingUiState(
+                isUnpaidNotificationEnabled = notificationSettingRepository.isUnpaidNotificationEnabled(),
+            ),
+        )
+    val uiState: StateFlow<SettingUiState> = _uiState.asStateFlow()
+
+    fun setUnpaidNotificationEnabled(isEnabled: Boolean) {
+        notificationSettingRepository.setUnpaidNotificationEnabled(isEnabled)
+        _uiState.update { currentState ->
+            currentState.copy(isUnpaidNotificationEnabled = isEnabled)
+        }
+    }
+}
