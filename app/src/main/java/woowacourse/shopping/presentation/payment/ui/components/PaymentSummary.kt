be3e94b206a55a661864b3ce8fed0535f4563709
joon0447 <joon0447@naver.com>
2026-05-22 18:17:57 +0900

    feat: 결제 요약 정보 가로 패딩 추가

--- app/src/main/java/woowacourse/shopping/presentation/payment/ui/components/PaymentSummary.kt
+++ app/src/main/java/woowacourse/shopping/presentation/payment/ui/components/PaymentSummary.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -57,7 +58,10 @@ private fun PaymentSummaryItem(
     modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier = modifier.fillMaxWidth(),
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .padding(horizontal = 18.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
