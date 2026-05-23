ee6a30cfec2018cf2ec230cf0640e7aa09641bcf
first-woosun <dbdntjs620@gmail.com>
2026-05-23 12:31:35 +0900

    feat: 단발성 이벤트 추가
    - 장바구니 추가, 수량 변경, 삭제
    - loadMore

--- app/src/main/java/woowacourse/shopping/ui/shopping/ShoppingEvent.kt
+++ app/src/main/java/woowacourse/shopping/ui/shopping/ShoppingEvent.kt
@@ -1,5 +1,7 @@
 package woowacourse.shopping.ui.shopping
 
+import woowacourse.shopping.domain.PurchaseProduct
+
 sealed interface ShoppingEvent {
     data class ShowSnackBar(val message: String): ShoppingEvent
     data class NavigateToProductDetail(
@@ -7,4 +9,9 @@ sealed interface ShoppingEvent {
         val lastViewedProductId: Long? = null
     ): ShoppingEvent
     object NavigateToCart: ShoppingEvent
+    data class AddToCart(val purchaseProduct: PurchaseProduct): ShoppingEvent
+    data class UpdateCount(val productID: Long, val updateAmount: Int): ShoppingEvent
+
+    data class RemoveFormCart(val purchaseProductId: Long): ShoppingEvent
+    object LoadMore: ShoppingEvent
 }
