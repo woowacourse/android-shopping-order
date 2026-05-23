ce889f9ec23b82cd120c22670d011709bd277ec8
todays-sun-day <178suna@gmail.com>
2026-05-23 20:06:16 +0900

    feat: Money 클래스에 Comparable 인터페이스 구현

--- app/src/main/java/woowacourse/shopping/model/Money.kt
+++ app/src/main/java/woowacourse/shopping/model/Money.kt
@@ -3,10 +3,12 @@ package woowacourse.shopping.model
 @JvmInline
 value class Money(
     val value: Long,
-) {
+): Comparable<Money> {
     init {
         require(value >= 0) { "금액은 0 이상이어야 합니다." }
     }
 
     operator fun times(quantity: Int): Money = Money(this.value * quantity)
+
+    override fun compareTo(other: Money): Int = this.value.compareTo(other.value)
 }
