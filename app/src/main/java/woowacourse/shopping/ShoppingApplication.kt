9f3c585be9db7006f2eaf05ea6abf3a6806c0384
first-woosun <dbdntjs620@gmail.com>
2026-05-24 01:02:08 +0900

    feat: service, repository 객체 초기화 추가

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -6,8 +6,12 @@ import woowacourse.shopping.data.local.database.DataBase
 import woowacourse.shopping.data.local.repository.RecentlyViewedProductRepositoryImpl
 import woowacourse.shopping.data.remote.server.RetrofitProvider
 import woowacourse.shopping.data.remote.server.repository.CartRepositoryImpl
+import woowacourse.shopping.data.remote.server.repository.CouponRepositoryImpl
+import woowacourse.shopping.data.remote.server.repository.OrderRepositoryImpl
 import woowacourse.shopping.data.remote.server.repository.ProductRepositoryImpl
 import woowacourse.shopping.data.remote.server.service.CartService
+import woowacourse.shopping.data.remote.server.service.CouponService
+import woowacourse.shopping.data.remote.server.service.OrderService
 import woowacourse.shopping.data.remote.server.service.ProductService
 import kotlin.jvm.java
 
@@ -36,11 +40,27 @@ class ShoppingApplication : Application() {
         retrofitClient.create(CartService::class.java)
     }
 
+    val orderService: OrderService by lazy {
+        retrofitClient.create(OrderService::class.java)
+    }
+
+    val couponService: CouponService by lazy {
+        retrofitClient.create(CouponService::class.java)
+    }
+
     val productRepository by lazy {
         ProductRepositoryImpl(productService)
     }
 
     val cartRepository by lazy {
         CartRepositoryImpl(cartService)
+    }
+
+    val orderRepository by lazy {
+        OrderRepositoryImpl(orderService)
+    }
+
+    val couponRepository by lazy {
+        CouponRepositoryImpl(couponService)
     }
 }
