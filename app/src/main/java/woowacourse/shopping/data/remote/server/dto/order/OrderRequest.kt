64d81d61ca9a03a377c48330d7fec5fde4036427
first-woosun <dbdntjs620@gmail.com>
2026-05-24 01:05:48 +0900

    feat: SerialName 어노테이션 추가

--- app/src/main/java/woowacourse/shopping/data/remote/server/dto/order/OrderRequest.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/server/dto/order/OrderRequest.kt
@@ -1,9 +1,11 @@
 package woowacourse.shopping.data.remote.server.dto.order
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class OrderRequest(
+    @SerialName("cartItemIds")
     val ids: List<Long>
 ) {
 
