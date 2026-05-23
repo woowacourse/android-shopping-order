a9f5294d4a87f6e628d31a93bb631023d256026c
first-woosun <dbdntjs620@gmail.com>
2026-05-24 01:05:03 +0900

    feat: Api 반환타입 변경

--- app/src/main/java/woowacourse/shopping/data/remote/server/service/CouponService.kt
+++ app/src/main/java/woowacourse/shopping/data/remote/server/service/CouponService.kt
@@ -1,9 +1,9 @@
 package woowacourse.shopping.data.remote.server.service
 
 import retrofit2.http.GET
-import woowacourse.shopping.data.remote.server.dto.coupon.CouponResult
+import woowacourse.shopping.data.remote.server.dto.coupon.item.CouponResultItem
 
 interface CouponService {
     @GET("coupons")
-    suspend fun getCoupons(): CouponResult
+    suspend fun getCoupons(): List<CouponResultItem>
 }
