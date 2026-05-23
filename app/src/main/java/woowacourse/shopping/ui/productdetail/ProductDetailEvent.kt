2a873dab9641f6e09eececd458e71dc6855ca691
first-woosun <dbdntjs620@gmail.com>
2026-05-23 11:59:33 +0900

    feat: 단발성 이벤트 정의

---
+++ app/src/main/java/woowacourse/shopping/ui/productdetail/ProductDetailEvent.kt
@@ -0,0 +1,7 @@
+package woowacourse.shopping.ui.productdetail
+
+sealed interface ProductDetailEvent{
+    data class SnackbarEvent(val errorMsg: String): ProductDetailEvent
+    data class MoveToLastViewedProductDetail(val lastViewedProductId: Long): ProductDetailEvent
+    object MoveToShopping: ProductDetailEvent
+}
