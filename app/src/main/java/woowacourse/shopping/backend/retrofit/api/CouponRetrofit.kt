e9ee790c00c5c98cb34b23f5b9c71246502f6772
chohs4164 <josh100as@gmail.com>
2026-05-23 13:43:02 +0900

    feat: 쿠폰 목록 조회를 위한 Retrofit 인터페이스 구현

---
+++ app/src/main/java/woowacourse/shopping/backend/retrofit/api/CouponRetrofit.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.backend.retrofit.api
+
+import retrofit2.http.GET
+import retrofit2.http.Header
+import woowacourse.shopping.backend.retrofit.dto.coupon.CouponResponse
+
+interface CouponRetrofit {
+    @GET("/coupons")
+    suspend fun getCoupons(
+        @Header("Accept") accept: String = "application/json"
+    ): List<CouponResponse>
+}
