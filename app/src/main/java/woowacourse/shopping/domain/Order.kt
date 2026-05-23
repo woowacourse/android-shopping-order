2f8222e128070e55d6ea5dfbcbf09686a512498e
first-woosun <dbdntjs620@gmail.com>
2026-05-23 21:28:26 +0900

    feat: 최종 가격 계산 함수 추가

--- app/src/main/java/woowacourse/shopping/domain/Order.kt
+++ app/src/main/java/woowacourse/shopping/domain/Order.kt
@@ -1,12 +1,17 @@
 package woowacourse.shopping.domain
 
+import woowacourse.shopping.domain.coupon.Discount
 import java.time.LocalDateTime
 
 data class Order(
     val purchaseProducts: List<PurchaseProduct>,
-    val shippingFee: Int,
+    val shippingFee: Int = 3000,
     val currentTime: LocalDateTime,
     val isRemoteArea: Boolean
 ) {
     val totalProductPrice: Int = purchaseProducts.sumOf { it.totalPrice() }
+
+    fun calculateFinalPrice(discount: Discount): Int {
+        return (totalProductPrice + shippingFee) - discount.totalAmount
+    }
 }
