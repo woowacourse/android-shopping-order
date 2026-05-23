02b171a20ac5120c122a22a6b04090d50992c27d
first-woosun <dbdntjs620@gmail.com>
2026-05-24 01:00:37 +0900

    feat: Payment uiState 정의

---
+++ app/src/main/java/woowacourse/shopping/ui/payment/PaymentUiState.kt
@@ -0,0 +1,14 @@
+package woowacourse.shopping.ui.payment
+
+import woowacourse.shopping.domain.Order
+import woowacourse.shopping.domain.coupon.Coupon
+import woowacourse.shopping.domain.coupon.Discount
+import java.time.LocalDateTime
+
+data class PaymentUiState(
+    val coupons: List<Coupon> = emptyList(),
+    val order: Order = Order(emptyList(), currentTime = LocalDateTime.now(), isRemoteArea = false),
+    val selectedCoupon: Coupon? = null,
+    val discount: Discount = Discount(),
+    val isLoading: Boolean = false
+)
