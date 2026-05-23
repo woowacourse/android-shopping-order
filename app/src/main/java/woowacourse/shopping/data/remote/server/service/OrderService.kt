a1f61145c8ca056c6cf483e214fedc11a56defa5
first-woosun <dbdntjs620@gmail.com>
2026-05-23 17:05:25 +0900

    refactor: order service 객체 및 함수 정의

---
+++ app/src/main/java/woowacourse/shopping/data/remote/server/service/OrderService.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.data.remote.server.service
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+import woowacourse.shopping.data.remote.server.dto.order.OrderRequest
+
+interface OrderService {
+    @POST("orders")
+    suspend fun requestOrder(
+        @Body request: OrderRequest
+    )
+}
