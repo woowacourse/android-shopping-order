349384f04be6e7e26bd68844f1cd23b99469d146
cbnuLeehyunwoo <henow123@naver.com>
2026-05-23 17:31:03 +0900

    build: Navigation Compose 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.retrofit2.kotlinx.serialization.converter)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.datastore.preferences)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
