697df1da94e53bb25bd76880cbe4b7b8d4f9b878
first-woosun <dbdntjs620@gmail.com>
2026-05-23 16:48:21 +0900

    feat: 상품 추천 화면 이벤트 정의

---
+++ app/src/main/java/woowacourse/shopping/ui/recommendation/RecommendationEvent.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping.ui.recommendation
+
+import woowacourse.shopping.domain.PurchaseProduct
+
+sealed interface RecommendationEvent {
+    data class SnackbarEvent(val errorMsg: String) : RecommendationEvent
+
+    data class AddToCart(val purchaseProduct: PurchaseProduct) : RecommendationEvent
+    data class UpdateAmount(val targetID: Long, val updateAmount: Int) : RecommendationEvent
+    data class RemoveFromCart(val targetId: Long) : RecommendationEvent
+
+    data class NavigateToPayment(val checkedIds: List<Long>) : RecommendationEvent
+    data class NavigateToProductDetail(val selectedProductId: Long) : RecommendationEvent
+    object NavigateToCart : RecommendationEvent
+}
