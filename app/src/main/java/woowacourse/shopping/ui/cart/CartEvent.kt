df03e5632899e620c329ec0441b48a5eeabbe5e8
first-woosun <dbdntjs620@gmail.com>
2026-05-23 14:04:52 +0900

    feat: 단발성 이벤트 정의

---
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartEvent.kt
@@ -0,0 +1,13 @@
+package woowacourse.shopping.ui.cart
+
+sealed interface CartEvent {
+    data class SnackbarEvent(val errorMsg: String) : CartEvent
+
+    data class UpdateCount(val targetId: Long, val updateAmount: Int) : CartEvent
+    data class RemoveFromCart(val targetId: Long) : CartEvent
+    object NextPage : CartEvent
+    object PrevPage : CartEvent
+
+    object NavigateToShopping : CartEvent
+    data class NavigateToRecommendation(val totalPrice: Int, val checkedIds: List<Long>) : CartEvent
+}
