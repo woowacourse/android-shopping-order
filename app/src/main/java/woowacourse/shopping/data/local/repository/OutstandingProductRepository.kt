869e25683637fc5de34295c7064e5690a116e670
first-woosun <dbdntjs620@gmail.com>
2026-05-24 12:20:06 +0900

    feat: 결제 예정 상품 레포지토리 정의

---
+++ app/src/main/java/woowacourse/shopping/data/local/repository/OutstandingProductRepository.kt
@@ -0,0 +1,9 @@
+package woowacourse.shopping.data.local.repository
+
+interface OutstandingProductRepository {
+    suspend fun getAll(): List<Long>
+
+    suspend fun insertAll(cartItemIds: List<Long>)
+
+    suspend fun deleteAll()
+}
---
+++ app/src/main/java/woowacourse/shopping/data/local/repository/OutstandingProductRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package woowacourse.shopping.data.local.repository
+
+import woowacourse.shopping.data.local.dao.OutstandingProductDao
+import woowacourse.shopping.data.local.entity.OutstandingProductEntity
+
+class OutstandingProductRepositoryImpl(
+    private val outstandingProductDao: OutstandingProductDao
+) : OutstandingProductRepository {
+    override suspend fun getAll(): List<Long> {
+        val items = outstandingProductDao.getAll()
+        return items.map { it.cartItemId }
+    }
+
+    override suspend fun insertAll(cartItemIds: List<Long>) {
+        val entities = cartItemIds.map { OutstandingProductEntity(it) }
+        outstandingProductDao.insertAll(entities)
+    }
+
+    override suspend fun deleteAll() {
+        outstandingProductDao.deleteAll()
+    }
+}
