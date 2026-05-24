defa12466ff6e7816fe6cf86d10e916c85885d45
first-woosun <dbdntjs620@gmail.com>
2026-05-25 00:45:33 +0900

    feat: AlarmManager를 통한 알림 스케쥴러 정의

---
+++ app/src/main/java/woowacourse/shopping/ui/alarm/AlarmScheduler.kt
@@ -0,0 +1,64 @@
+package woowacourse.shopping.ui.alarm
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+
+class AlarmScheduler(context: Context) {
+    private val applicationContext = context.applicationContext
+    private val alarmManager =
+        applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+    fun createAlarmSchedule(delayTime: Long) {
+        val intent = Intent(applicationContext, PaymentAlarmReceiver::class.java)
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            ALARM_ID,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val triggerTime = System.currentTimeMillis() + delayTime
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && !alarmManager.canScheduleExactAlarms()) {
+            alarmManager.set(
+                AlarmManager.RTC_WAKEUP,
+                triggerTime,
+                pendingIntent
+            )
+        }
+
+        try {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                triggerTime,
+                pendingIntent
+            )
+        } catch (e: SecurityException) {
+            alarmManager.set(
+                AlarmManager.RTC_WAKEUP,
+                triggerTime,
+                pendingIntent
+            )
+        }
+    }
+
+    fun cancel() {
+        val intent = Intent(applicationContext, PaymentAlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            ALARM_ID,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        alarmManager.cancel(pendingIntent)
+    }
+
+    companion object {
+        private const val ALARM_ID = 1
+    }
+}
