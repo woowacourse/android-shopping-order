a1dd27da74307e86687948d52c171e6665410d71
joon0447 <joon0447@naver.com>
2026-05-25 04:40:22 +0900

    refactor: RemoteConfig 내 미사용 주석 제거

--- app/src/main/java/woowacourse/shopping/data/source/remote/RemoteConfig.kt
+++ app/src/main/java/woowacourse/shopping/data/source/remote/RemoteConfig.kt
@@ -1,6 +1,5 @@
 package woowacourse.shopping.data.source.remote
 
 object RemoteConfig {
-//    const val BASE_URL = "http://10.0.2.2:8080"
     const val BASE_URL = "http://techcourse-lv2-alb-250216202.ap-northeast-2.elb.amazonaws.com/"
 }
