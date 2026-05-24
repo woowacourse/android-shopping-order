a71eded2b73a88dac798e0ba828bb3b1023b772a
first-woosun <dbdntjs620@gmail.com>
2026-05-25 03:07:38 +0900

    refactor: order Repository 테스트 더블 추가

---
+++ app/src/test/java/woowacourse/shopping/viewmodel/fakes/FakeOrderRepository.kt
@@ -0,0 +1,11 @@
+package woowacourse.shopping.viewmodel.fakes
+
+import woowacourse.shopping.data.remote.server.apiresult.ApiResult
+import woowacourse.shopping.data.remote.server.repository.OrderRepository
+import woowacourse.shopping.domain.Order
+
+class FakeOrderRepository : OrderRepository {
+    override suspend fun order(order: Order): ApiResult<Unit> {
+        return ApiResult.Success(Unit)
+    }
+}
