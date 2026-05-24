3a2a167b1f08654c4622453c4f28d1beed04c3f7
baekcii <bsy8470@naver.com>
2026-05-25 03:38:33 +0900

    refactor: systemBarsPadding 적용

--- app/src/main/java/woowacourse/shopping/ui/order/OrderScreen.kt
+++ app/src/main/java/woowacourse/shopping/ui/order/OrderScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -45,7 +46,12 @@ fun OrderScreen(
         }
     }
 
-    Column {
+    Column(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .systemBarsPadding(),
+    ) {
         PaymentTopAppBar(onNavigateBack = onClickClose)
         OrderScreenContent(
             uiState = uiState,
