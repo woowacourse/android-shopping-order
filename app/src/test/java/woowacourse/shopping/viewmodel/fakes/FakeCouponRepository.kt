d76cbc08aa11e2f4a6212f211dfb7b9b9010d4eb
first-woosun <dbdntjs620@gmail.com>
2026-05-25 03:07:30 +0900

    refactor: coupon Repository 테스트 더블 추가

---
+++ app/src/test/java/woowacourse/shopping/viewmodel/fakes/FakeCouponRepository.kt
@@ -0,0 +1,17 @@
+package woowacourse.shopping.viewmodel.fakes
+
+import woowacourse.shopping.data.remote.server.apiresult.ApiResult
+import woowacourse.shopping.data.remote.server.repository.CouponRepository
+import woowacourse.shopping.domain.coupon.Coupon
+
+class FakeCouponRepository : CouponRepository {
+    private var coupons = listOf<Coupon>()
+
+    fun setCoupons(newCoupons: List<Coupon>) {
+        coupons = newCoupons
+    }
+
+    override suspend fun getCoupons(): ApiResult<List<Coupon>> {
+        return ApiResult.Success(coupons)
+    }
+}
