129563802bc0486cbdf0fd65a92a8e21e51b03ef
baekcii <bsy8470@naver.com>
2026-05-24 19:20:27 +0900

    feat: 추천 상품 화면 분리

---
+++ app/src/main/java/woowacourse/shopping/ui/cart/recommend/RecommendScreen.kt
@@ -0,0 +1,45 @@
+package woowacourse.shopping.ui.cart.recommend
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import woowacourse.shopping.ui.cart.CartTopAppBar
+import woowacourse.shopping.ui.cart.CartViewModel
+
+@Composable
+fun RecommendScreen(
+    cartViewModel: CartViewModel,
+    recommendViewModel: RecommendViewModel,
+    onClickClose: () -> Unit,
+) {
+    val cartUiState by cartViewModel.cartUiState.collectAsStateWithLifecycle()
+    val recommendUiState by recommendViewModel.uiState.collectAsStateWithLifecycle()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        CartTopAppBar(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+            onClick = onClickClose,
+        )
+        RecommendScreenContent(
+            recommendUiState = recommendUiState,
+            totalPrice = cartUiState.totalPrice,
+            totalCount = cartUiState.totalCount,
+            onAddClick = cartViewModel::addCartItem,
+            onIncrease = recommendViewModel::increaseRecommendProduct,
+            onDecrease = recommendViewModel::decreaseRecommendProduct,
+            onClickOrder = cartViewModel::onClickOrder,
+            modifier = Modifier.weight(1f),
+        )
+    }
+}
