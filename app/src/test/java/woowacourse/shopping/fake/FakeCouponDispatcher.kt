c1ada4ca774c3eef53d7edf8609cb7e53b40c0f9
joon0447 <joon0447@naver.com>
2026-05-24 12:17:51 +0900

    test: 쿠폰 레포지토리 테스트 코드 구현 및 Fake 객체 추가

---
+++ app/src/test/java/woowacourse/shopping/fake/FakeCouponDispatcher.kt
@@ -0,0 +1,66 @@
+package woowacourse.shopping.fake
+
+import mockwebserver3.Dispatcher
+import mockwebserver3.MockResponse
+import mockwebserver3.RecordedRequest
+
+class FakeCouponDispatcher : Dispatcher() {
+    override fun dispatch(request: RecordedRequest): MockResponse =
+        when {
+            request.method == "GET" && request.url.encodedPath.startsWith("/coupons") -> {
+                MockResponse
+                    .Builder()
+                    .code(200)
+                    .addHeader("Content-Type", "application/json")
+                    .body(COUPONS_JSON)
+                    .build()
+            }
+            else -> MockResponse.Builder().code(404).build()
+        }
+
+    companion object {
+        private val COUPONS_JSON =
+            """
+            [
+              {
+                "discountType": "fixed",
+                "id": 1,
+                "code": "FIXED5000",
+                "description": "5,000원 할인 쿠폰",
+                "expirationDate": "2026-12-31",
+                "discount": 5000,
+                "minimumAmount": 100000
+              },
+              {
+                "discountType": "buyXgetY",
+                "id": 2,
+                "code": "BUY2GET1",
+                "description": "2개 구매 시 1개 무료",
+                "expirationDate": "2026-12-31",                                                                                                                                    
+                "buyQuantity": 2,
+                "getQuantity": 1
+              },
+              {
+                "discountType": "freeShipping",
+                "id": 3,
+                "code": "FREESHIP",
+                "description": "무료 배송 쿠폰",
+                "expirationDate": "2026-12-31",
+                "minimumAmount": 50000
+              },
+              {
+                "discountType": "percentage",
+                "id": 4,
+                "code": "PERCENT30",
+                "description": "30% 할인 (점심시간)",
+                "expirationDate": "2026-12-31",
+                "discount": 30,
+                "availableTime": {
+                  "start": "12:00:00",
+                  "end": "14:00:00"
+                }
+              }
+            ]
+            """.trimIndent()
+    }
+}
