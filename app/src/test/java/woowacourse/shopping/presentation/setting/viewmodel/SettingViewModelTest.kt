da1d0a69e0eb751461bd04c477361fe29f344fcc
joon0447 <joon0447@naver.com>
2026-05-24 12:51:04 +0900

    test: SettingViewModel 초기 설정값 테스트 구현

---
+++ app/src/test/java/woowacourse/shopping/presentation/setting/viewmodel/SettingViewModelTest.kt
@@ -0,0 +1,26 @@
+package woowacourse.shopping.presentation.setting.viewmodel
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import woowacourse.shopping.fake.repository.FakeSettingRepository
+
+class SettingViewModelTest {
+
+    @Test
+    fun `초기 SharedPreferences의 알림 설정값이 false이면 초기 알림 여부도 false이다`() {
+        val settingRepository = FakeSettingRepository(paymentPendingNotificationEnabled = false)
+
+        val viewModel = SettingViewModel(settingRepository)
+
+        assertThat(viewModel.uiState.value.isPaymentPendingNotificationEnabled).isFalse()
+    }
+
+    @Test
+    fun `초기 SharedPreferences의 알림 설정값이 true이면 초기 알림 여부도 true이다`() {
+        val settingRepository = FakeSettingRepository(paymentPendingNotificationEnabled = true)
+
+        val viewModel = SettingViewModel(settingRepository)
+
+        assertThat(viewModel.uiState.value.isPaymentPendingNotificationEnabled).isTrue()
+    }
+}
