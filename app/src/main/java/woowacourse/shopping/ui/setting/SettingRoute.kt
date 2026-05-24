aa5a4c693bad4a5f631682a86b66c02ead8ae45a
cksckckcks <chan679999@naver.com>
2026-05-24 17:53:01 +0900

    feat: 설정 스크린 UI 이벤트 연결 및 알림 변경 시 토스트 메시지 기능 추가

--- app/src/main/java/woowacourse/shopping/ui/setting/SettingRoute.kt
+++ app/src/main/java/woowacourse/shopping/ui/setting/SettingRoute.kt
@@ -1,6 +1,9 @@
 package woowacourse.shopping.ui.setting
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -11,10 +14,25 @@ fun SettingRoute(
     viewModel: SettingViewModel = viewModel(factory = SettingViewModel.Factory),
 ) {
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+    LaunchedEffect(viewModel) {
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+                SettingUiEvent.NavigateBack -> {
+                    navController.popBackStack()
+                }
+
+                is SettingUiEvent.ShowToastMessage -> {
+                    Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
     SettingScreen(
         checked = uiState.value.isNotification,
-        onBackClick = { navController.popBackStack() },
+        onBackClick = viewModel::navigateBack,
         onCheckedChange = viewModel::updateIsNotification,
     )
 }
--- app/src/main/java/woowacourse/shopping/ui/setting/SettingViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/setting/SettingViewModel.kt
@@ -6,8 +6,10 @@ import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.AP
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -18,11 +20,22 @@ data class SettingUiState(
     val isNotification: Boolean = false,
 )
 
+sealed class SettingUiEvent {
+    object NavigateBack : SettingUiEvent()
+
+    data class ShowToastMessage(
+        val message: String,
+    ) : SettingUiEvent()
+}
+
 class SettingViewModel(
     private val dataStore: UserDataStore,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(SettingUiState())
     val uiState: StateFlow<SettingUiState> = _uiState.asStateFlow()
+
+    private val _uiEvent = MutableSharedFlow<SettingUiEvent>()
+    val uiEvent = _uiEvent.asSharedFlow()
 
     init {
         viewModelScope.launch {
@@ -37,6 +50,18 @@ class SettingViewModel(
     fun updateIsNotification(isNotification: Boolean) {
         viewModelScope.launch {
             dataStore.saveIsNotification(isNotification)
+
+            _uiEvent.emit(
+                SettingUiEvent.ShowToastMessage(
+                    message = "알림 변경에 성공했습니다.",
+                ),
+            )
+        }
+    }
+
+    fun navigateBack() {
+        viewModelScope.launch {
+            _uiEvent.emit(SettingUiEvent.NavigateBack)
         }
     }
 
