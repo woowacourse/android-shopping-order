917e9cc7fa94f6dfa28307ea651c22d510c4c09e
byunghyunkim0 <bb05qudgus@gmail.com>
2026-05-25 00:04:01 +0900

    feat: navigation 의존성 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.kotlinx.coroutines.android)
 
