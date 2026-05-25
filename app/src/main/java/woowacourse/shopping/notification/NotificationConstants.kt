3672aaa73180b7867c3744a23c816e30efd1f3d9
baekcii <bsy8470@naver.com>
2026-05-25 16:00:04 +0900

    chore: 주문 알림 간격을 5분으로 변경

--- app/src/main/java/woowacourse/shopping/notification/NotificationConstants.kt
+++ app/src/main/java/woowacourse/shopping/notification/NotificationConstants.kt
@@ -2,7 +2,7 @@ package woowacourse.shopping.notification
 
 object NotificationConstants {
     const val ORDER_ALARM_REQUEST_CODE = 1001
-    const val ORDER_REMINDER_INTERVAL_MS = 5 * 2 * 1000
+    const val ORDER_REMINDER_INTERVAL_MS = 5 * 60 * 1000
     const val ORDER_CHANNEL_ID = "order_notification_channel"
     const val ORDER_NOTIFICATION_ID = 1001
 }
