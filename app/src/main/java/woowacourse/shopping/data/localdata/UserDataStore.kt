029095095a9034c69b4cdff5da709964ed10d18d
cksckckcks <chan679999@naver.com>
2026-05-25 15:54:53 +0900

    refactor: ktlint 포맷팅 적용

--- app/src/main/java/woowacourse/shopping/data/localdata/UserDataStore.kt
+++ app/src/main/java/woowacourse/shopping/data/localdata/UserDataStore.kt
@@ -1,7 +1,6 @@
 package woowacourse.shopping.data.localdata
 
 import android.content.Context
-import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
