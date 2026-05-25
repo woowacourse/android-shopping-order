7f2ada9e58c946762d8c6951bb103a375dc43d37
katie0109 <0109katie@naver.com>
2026-05-25 09:33:05 +0900

    feat: 결제 UI 관련 상수 및 포맷팅 기능 추가

--- app/src/main/java/woowacourse/shopping/constant/Format.kt
+++ app/src/main/java/woowacourse/shopping/constant/Format.kt
@@ -1,5 +1,13 @@
 package woowacourse.shopping.constant
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
 object Format {
-    fun formatPrice(price: Int): String = String.format("%,d원", price)
+    private val dateFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일")
+
+    fun formatPrice(price: Int): String = String.format(Locale.KOREA, "%,d원", price)
+
+    fun formatDate(date: LocalDate): String = date.format(dateFormatter)
 }
