583dbc35a0640daba7c7e32f7b0213d93fb114ca
ujeong <qnf323@naver.com>
2026-05-28 19:37:29 +0900

    fix: 새로운 알림을 클릭해도 정보가 변경되지 않는 문제 수정

--- app/src/main/java/woowacourse/shopping/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -21,14 +20,13 @@ import woowacourse.shopping.feature.notification.AlarmReceiver
 import woowacourse.shopping.theme.AndroidshoppingTheme
 
 class MainActivity : ComponentActivity() {
+    private var startDestination by mutableStateOf<Any>(ProductListRoute)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContent {
             AndroidshoppingTheme {
-                var startDestination by remember { mutableStateOf<Any>(resolveStartDestination(intent)) }
-
                 LaunchedEffect(Unit) {
                     requestNotificationPermission()
                 }
@@ -40,11 +38,13 @@ class MainActivity : ComponentActivity() {
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         setIntent(intent)
+        startDestination = resolveStartDestination(intent)
     }
 
     private fun resolveStartDestination(intent: Intent): Any {
         val contentIds = intent.getStringArrayListExtra(AlarmReceiver.EXTRA_CONTENT_IDS)
         val totalPrice = intent.getIntExtra(AlarmReceiver.EXTRA_TOTAL_PRICE, 0)
+
         return if (!contentIds.isNullOrEmpty()) {
             PurchaseRoute(contentIds = contentIds, totalPrice = totalPrice)
         } else {
