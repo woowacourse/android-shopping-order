4d0e9e05dcb1d804e80a70ecdb89c57908ef070d
cksckckcks <chan679999@naver.com>
2026-05-28 21:01:46 +0900

    refactor: ID가 잘못되었을 때 예외 처리 로직 추가

--- app/src/main/java/woowacourse/shopping/data/repository/PaymentRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/repository/PaymentRepositoryImpl.kt
@@ -9,7 +9,12 @@ class PaymentRepositoryImpl(
     override suspend fun createOrder(cartItemIds: List<String>) {
         api.createOrder(
             OrderRequest(
-                cartItemIds = cartItemIds.map { it.toLong() },
+                cartItemIds =
+                    cartItemIds.map {
+                        it.toLongOrNull() ?: throw IllegalArgumentException(
+                            "id 값이 올바르지 않습니다.",
+                        )
+                    },
             ),
         )
     }
