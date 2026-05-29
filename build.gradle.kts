62825026bde62113869ade95ed1a5c264c94228f
nadajinny <jinsun23007@gmail.com>
2026-05-30 02:34:53 +0900

    fix: 애뮬레이터 실행 불가 오류 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import java.io.File
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -10,12 +8,6 @@ plugins {
     alias(libs.plugins.ktlint) apply false
 }
 
-// Keep Gradle outputs out of Desktop-synced paths to avoid intermittent file I/O timeouts.
-val stableBuildRoot = File(System.getProperty("java.io.tmpdir"), "gradle-build/${rootProject.name}")
-
-layout.buildDirectory.set(stableBuildRoot.resolve("root"))
-
 subprojects {
-    layout.buildDirectory.set(stableBuildRoot.resolve(name))
     apply(plugin = rootProject.libs.plugins.ktlint.get().pluginId)
 }
