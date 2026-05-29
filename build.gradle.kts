cfd90fdadc941a920ca4ad8c1c30445aab16afd7
nadajinny <jinsun23007@gmail.com>
2026-05-30 01:56:13 +0900

    fix: gradlew 실행 오류 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.File
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -8,6 +10,12 @@ plugins {
     alias(libs.plugins.ktlint) apply false
 }
 
+// Keep Gradle outputs out of Desktop-synced paths to avoid intermittent file I/O timeouts.
+val stableBuildRoot = File(System.getProperty("java.io.tmpdir"), "gradle-build/${rootProject.name}")
+
+layout.buildDirectory.set(stableBuildRoot.resolve("root"))
+
 subprojects {
+    layout.buildDirectory.set(stableBuildRoot.resolve(name))
     apply(plugin = rootProject.libs.plugins.ktlint.get().pluginId)
 }
