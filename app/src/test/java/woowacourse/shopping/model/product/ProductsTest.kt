f031cb062176596b7619908a65d169ab19e17dff
todays-sun-day <178suna@gmail.com>
2026-05-31 09:34:17 +0900

    refactor: ProductsTest 파일의 패키지 위치 이동

--- app/src/test/java/woowacourse/shopping/model/product/ProductsTest.kt
+++ app/src/test/java/woowacourse/shopping/model/product/ProductsTest.kt
@@ -1,11 +1,10 @@
 @file:Suppress("NonAsciiCharacters")
 
-package woowacourse.shopping.model
+package woowacourse.shopping.model.product
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import woowacourse.shopping.model.product.Product
-import woowacourse.shopping.model.product.Products
+import woowacourse.shopping.model.Money
 
 class ProductsTest {
     val product1 =
@@ -32,6 +31,6 @@ class ProductsTest {
         val expected = listOf(product1, product2)
         val actual = Products(listOf(product1, product2)).toList()
 
-        assertEquals(expected, actual)
+        Assertions.assertEquals(expected, actual)
     }
 }
