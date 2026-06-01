9b0874679834859b7d33d0b2a5dd7a7433e3733b
katie0109 <0109katie@naver.com>
2026-06-01 22:00:32 +0900

    refactor: UiEvent에서 사용하지 않는 네비게이션 이벤트 제거

--- app/src/main/java/woowacourse/shopping/ui/UiEvent.kt
+++ app/src/main/java/woowacourse/shopping/ui/UiEvent.kt
@@ -4,10 +4,4 @@ sealed interface UiEvent {
     data class ShowSnackbar(
         val message: String,
     ) : UiEvent
-
-    object NavigateToProductList : UiEvent
-
-    object NavigateToCart : UiEvent
-
-    object NavigateToPayment : UiEvent
 }
