2791c55d88e1e5c21a1b7e9444e2d63ae0b1719b
aksworns22 <wkdeogks17@gmail.com>
2026-06-02 19:15:56 +0900

    refactor: collectAsStateWithLifecycle을 사용하도록 수정

--- app/src/main/java/woowacourse/shopping/presentation/settings/NotificationSettingScreen.kt
+++ app/src/main/java/woowacourse/shopping/presentation/settings/NotificationSettingScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,6 +27,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import woowacourse.shopping.ui.theme.AndroidshoppingTheme
 import woowacourse.shopping.ui.theme.Gray30
@@ -40,7 +40,7 @@ fun NotificationSettingScreen(
     modifier: Modifier = Modifier,
     viewModel: NotificationSettingViewModel = viewModel(),
 ) {
-    val isNotificationEnabled by viewModel.isNotificationEnabled.collectAsState()
+    val isNotificationEnabled by viewModel.isNotificationEnabled.collectAsStateWithLifecycle()
 
     NotificationSettingsContent(
         isNotificationEnabled = isNotificationEnabled,
