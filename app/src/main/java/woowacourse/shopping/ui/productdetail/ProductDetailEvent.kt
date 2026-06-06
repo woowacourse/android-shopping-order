a411b1503b976d39a0e1806608f7849fcdf740db
first-woosun <dbdntjs620@gmail.com>
2026-06-06 15:09:47 +0900

    feat: ProductDetailEvent Message 객체 정의

--- app/src/main/java/woowacourse/shopping/ui/productdetail/ProductDetailEvent.kt
+++ app/src/main/java/woowacourse/shopping/ui/productdetail/ProductDetailEvent.kt
@@ -1,8 +1,25 @@
 package woowacourse.shopping.ui.productdetail
 
+import android.content.Context
+import woowacourse.shopping.R
+
 sealed interface ProductDetailEvent {
+    sealed interface Message {
+        fun toDisplayString(context: Context): String
+
+        data class NetworkError(val code: Int) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_network_error)}$code"
+        }
+
+        data class ExceptionError(val message: String?) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_generic_error)}${message}"
+        }
+    }
+
     data class SnackbarEvent(
-        val errorMsg: String,
+        val errorMsg: Message,
     ) : ProductDetailEvent
 
     data class MoveToLastViewedProductDetail(
