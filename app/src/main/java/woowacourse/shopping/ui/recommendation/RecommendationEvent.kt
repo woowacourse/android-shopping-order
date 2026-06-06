1aaac4fba19af01a2d0c218b05c8e3f5268c7aa2
first-woosun <dbdntjs620@gmail.com>
2026-06-06 15:06:15 +0900

    feat: Recommendation 스낵바 Message 객체 정의

--- app/src/main/java/woowacourse/shopping/ui/recommendation/RecommendationEvent.kt
+++ app/src/main/java/woowacourse/shopping/ui/recommendation/RecommendationEvent.kt
@@ -1,10 +1,41 @@
 package woowacourse.shopping.ui.recommendation
 
+import android.content.Context
+import woowacourse.shopping.R
 import woowacourse.shopping.domain.PurchaseProduct
 
 sealed interface RecommendationEvent {
+    sealed interface Message {
+        fun toDisplayString(context: Context): String
+
+        object CartAdded : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_cart_added)
+        }
+
+        object QuantityUpdated : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_quantity_updated)
+        }
+
+        object CartRemoved : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_cart_removed)
+        }
+
+        data class NetworkError(val code: Int) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_network_error)}$code"
+        }
+
+        data class ExceptionError(val message: String?) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_generic_error)}${message}"
+        }
+    }
+
     data class SnackbarEvent(
-        val errorMsg: String,
+        val errorMsg: Message,
     ) : RecommendationEvent
 
     data class AddToCart(
