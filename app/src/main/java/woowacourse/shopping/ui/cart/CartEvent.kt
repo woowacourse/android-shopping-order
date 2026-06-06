fa16216a88faed1241b3f6528d817345b0310d18
first-woosun <dbdntjs620@gmail.com>
2026-06-06 15:30:22 +0900

    feat: CartEvent Message 객체 정의

--- app/src/main/java/woowacourse/shopping/ui/cart/CartEvent.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartEvent.kt
@@ -1,8 +1,35 @@
 package woowacourse.shopping.ui.cart
 
+import android.content.Context
+import woowacourse.shopping.R
+
 sealed interface CartEvent {
+    sealed interface Message {
+        fun toDisplayString(context: Context): String
+
+        object QuantityUpdated : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_quantity_updated)
+        }
+
+        object RemoveFromCart : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_cart_removed)
+        }
+
+        data class NetworkError(val code: Int) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_network_error)}$code"
+        }
+
+        data class ExceptionError(val message: String?) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_generic_error)}${message}"
+        }
+    }
+
     data class SnackbarEvent(
-        val errorMsg: String,
+        val errorMsg: Message,
     ) : CartEvent
 
     data class UpdateCount(
