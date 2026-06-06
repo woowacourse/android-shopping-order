a05111d360d013839693c16c9c72549a5c05d4bf
first-woosun <dbdntjs620@gmail.com>
2026-06-06 14:44:25 +0900

    feat: Message 객체 정의

--- app/src/main/java/woowacourse/shopping/ui/shopping/ShoppingEvent.kt
+++ app/src/main/java/woowacourse/shopping/ui/shopping/ShoppingEvent.kt
@@ -1,10 +1,41 @@
 package woowacourse.shopping.ui.shopping
 
+import android.content.Context
+import woowacourse.shopping.R
 import woowacourse.shopping.domain.PurchaseProduct
 
 sealed interface ShoppingEvent {
+    sealed interface Message {
+        fun toDisplayString(context: Context): String
+
+        object CartAdded : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_cart_added)
+        }
+
+        object QuantityUpdated : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_quantity_updated)
+        }
+
+        object RecentProductsLoadFailed : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_recent_products_load_failed)
+        }
+
+        data class NetworkError(val code: Int) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_network_error)}$code"
+        }
+
+        data class ExceptionError(val message: String?) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_generic_error)}${message}"
+        }
+    }
+
     data class ShowSnackBar(
-        val message: String,
+        val message: Message,
     ) : ShoppingEvent
 
     data class NavigateToProductDetail(
