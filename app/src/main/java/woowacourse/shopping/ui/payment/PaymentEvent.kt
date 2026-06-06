75ed83e479635b8efcb389d0929d395c1ed7b788
first-woosun <dbdntjs620@gmail.com>
2026-06-06 15:36:37 +0900

    feat: PaymentEvent Message 객체 정의

--- app/src/main/java/woowacourse/shopping/ui/payment/PaymentEvent.kt
+++ app/src/main/java/woowacourse/shopping/ui/payment/PaymentEvent.kt
@@ -1,8 +1,30 @@
 package woowacourse.shopping.ui.payment
 
+import android.content.Context
+import woowacourse.shopping.R
+
 sealed interface PaymentEvent {
+    sealed interface Message {
+        fun toDisplayString(context: Context): String
+
+        object OrderSuccess : Message {
+            override fun toDisplayString(context: Context): String =
+                context.getString(R.string.msg_order_success)
+        }
+
+        data class NetworkError(val code: Int) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_network_error)}$code"
+        }
+
+        data class ExceptionError(val message: String?) : Message {
+            override fun toDisplayString(context: Context): String =
+                "${context.getString(R.string.msg_generic_error)}${message}"
+        }
+    }
+
     data class SnackbarEvent(
-        val message: String,
+        val message: Message,
     ) : PaymentEvent
 
     object Order : PaymentEvent
